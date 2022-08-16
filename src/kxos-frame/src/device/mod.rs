182dee0d0c1c2be354713bc432bc44581b28acda
sdww0 <1315953661@qq.com>
2022-08-15 19:41:25 -0700

    boot and irq

--- src/kxos-frame/src/device/mod.rs
+++ src/kxos-frame/src/device/mod.rs
@@ -1,7 +1,15 @@
 //! Device-related APIs.
 
+pub mod framebuffer;
 mod io_port;
 mod irq;
 
+use bootloader::BootInfo;
+
 pub use self::io_port::IoPort;
-pub use self::irq::{IrqCallbackHandle, IrqLine};
+pub use self::irq::{InterruptInformation, IrqCallbackHandle, IrqLine};
+
+pub fn init(boot_info: &'static mut BootInfo) {
+    framebuffer::init(boot_info.framebuffer.as_mut().unwrap());
+    irq::init();
+}
