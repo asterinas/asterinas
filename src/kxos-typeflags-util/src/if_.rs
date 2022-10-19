dd1e21b97c1824c686a944e009e1179053313170
Jianfeng Jiang <jiangjianfeng.jjf@antgroup.com>
2022-10-19 11:34:47 +0800

    add procedural macro typeflags

---
+++ src/kxos-typeflags-util/src/if_.rs
@@ -0,0 +1,29 @@
+//! Type Level If
+
+use crate::bool::{False, True};
+
+pub trait If<B1, B2> {
+    type Output;
+}
+
+impl<B1, B2> If<B1, B2> for True {
+    type Output = B1;
+}
+
+impl<B1, B2> If<B1, B2> for False {
+    type Output = B2;
+}
+
+pub type IfOp<Cond, B1, B2> = <Cond as If<B1, B2>>::Output;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::*;
+
+    #[test]
+    fn test() {
+        assert_type_same!(IfOp<True, u32, ()>, u32);
+        assert_type_same!(IfOp<False, (), usize>, usize);
+    }
+}
