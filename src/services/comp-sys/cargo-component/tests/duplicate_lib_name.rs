b5201daf121bca4e8b4b0f2b38f06289bb4533c5
Jianfeng Jiang <jiangjianfeng.jjf@antgroup.com>
2023-03-08 15:42:21 +0800

    refactor cargo-component tests

--- src/services/comp-sys/cargo-component/tests/duplicate_lib_name.rs
+++ src/services/comp-sys/cargo-component/tests/duplicate_lib_name.rs
@@ -2,22 +2,10 @@
 
 #![feature(once_cell)]
 
-use std::path::PathBuf;
-use test_utils::{cargo_clean, cargo_component};
 mod test_utils;
 
 #[test]
 fn duplicate_lib_name() {
-    let root_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let target_dir = root_dir.join("target").join("duplicate_lib_name_test");
-    let cwd = root_dir.join("tests").join("duplicate_lib_name_test");
-    let output = cargo_clean(&cwd, &target_dir);
-    assert!(output.status.success());
-
-    let output = cargo_component(&cwd, &target_dir);
-    let stderr = String::from_utf8_lossy(&output.stderr);
-    println!("stderr: {stderr}");
-
-    assert!(!output.status.success());
+    let stderr = run_cargo_component_cmd!();
     assert!(stderr.contains("duplicate library names"));
 }
--- src/services/comp-sys/cargo-component/tests/trait_method.rs
+++ src/services/comp-sys/cargo-component/tests/trait_method.rs
@@ -2,27 +2,13 @@
 
 #![feature(once_cell)]
 
-use std::path::PathBuf;
-use test_utils::{cargo_clean, cargo_component, clean_after_test};
 mod test_utils;
 
 #[test]
 fn trait_method() {
-    let root_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let target_dir = root_dir.join("target").join("trait_method_test");
-    let cwd = root_dir.join("tests").join("trait_method_test");
-    let output = cargo_clean(&cwd, &target_dir);
-    assert!(output.status.success());
-
-    let output = cargo_component(&cwd, &target_dir);
-    let stderr = String::from_utf8_lossy(&output.stderr);
-    println!("stderr: {stderr}");
-
-    assert!(output.status.success());
+    let stderr = run_cargo_component_cmd!();
     assert!(stderr.contains("access controlled entry point is disallowed"));
     assert!(stderr.contains("access foo::Foo::method in bar"));
     assert!(stderr.contains("access foo::FooTrait::trait_associate_fn in bar"));
     assert!(stderr.contains("access foo::FooTrait::trait_method in bar"));
-
-    clean_after_test(&cwd);
 }
