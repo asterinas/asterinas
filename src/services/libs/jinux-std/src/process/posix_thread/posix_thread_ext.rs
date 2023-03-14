45427da50e023100fefae4c5edb2cb297f95568a
Jianfeng Jiang <jiangjianfeng.jjf@antgroup.com>
2023-03-14 19:41:08 +0800

    support returning Err when creating posix thread

--- src/services/libs/jinux-std/src/process/posix_thread/posix_thread_ext.rs
+++ src/services/libs/jinux-std/src/process/posix_thread/posix_thread_ext.rs
@@ -5,7 +5,7 @@ use crate::{
     prelude::*,
     process::{program_loader::load_program_to_root_vmar, Process},
     rights::Full,
-    thread::{allocate_tid, Thread},
+    thread::{Thread, Tid},
     vm::vmar::Vmar,
 };
 
@@ -13,25 +13,27 @@ use super::{builder::PosixThreadBuilder, name::ThreadName, PosixThread};
 pub trait PosixThreadExt {
     fn as_posix_thread(&self) -> Option<&PosixThread>;
     fn new_posix_thread_from_executable(
+        tid: Tid,
         root_vmar: &Vmar<Full>,
         fs_resolver: &FsResolver,
         executable_path: &str,
         process: Weak<Process>,
         argv: Vec<CString>,
         envp: Vec<CString>,
-    ) -> Arc<Self>;
+    ) -> Result<Arc<Self>>;
 }
 
 impl PosixThreadExt for Thread {
     /// This function should only be called when launch shell()
     fn new_posix_thread_from_executable(
+        tid: Tid,
         root_vmar: &Vmar<Full>,
         fs_resolver: &FsResolver,
         executable_path: &str,
         process: Weak<Process>,
         argv: Vec<CString>,
         envp: Vec<CString>,
-    ) -> Arc<Self> {
+    ) -> Result<Arc<Self>> {
         let elf_load_info = load_program_to_root_vmar(
             root_vmar,
             executable_path.to_string(),
@@ -39,20 +41,18 @@ impl PosixThreadExt for Thread {
             envp,
             fs_resolver,
             1,
-        )
-        .unwrap();
+        )?;
 
         let vm_space = root_vmar.vm_space().clone();
         let mut cpu_ctx = CpuContext::default();
         cpu_ctx.set_rip(elf_load_info.entry_point() as _);
         cpu_ctx.set_rsp(elf_load_info.user_stack_top() as _);
         let user_space = Arc::new(UserSpace::new(vm_space, cpu_ctx));
-        let thread_name = Some(ThreadName::new_from_executable_path(executable_path).unwrap());
-        let tid = allocate_tid();
+        let thread_name = Some(ThreadName::new_from_executable_path(executable_path)?);
         let thread_builder = PosixThreadBuilder::new(tid, user_space)
             .thread_name(thread_name)
             .process(process);
-        thread_builder.build()
+        Ok(thread_builder.build())
     }
 
     fn as_posix_thread(&self) -> Option<&PosixThread> {
