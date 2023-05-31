ec1efb4b79655f4f44ca744a41ce53227d7df55a
Jianfeng Jiang <jiangjianfeng.jjf@antgroup.com>
2023-05-31 10:48:43 +0800

    Add network syscalls

---
+++ services/libs/jinux-std/src/syscall/accept.rs
@@ -0,0 +1,24 @@
+use crate::util::write_socket_addr_to_user;
+use crate::{fs::file_table::FileDescripter, prelude::*};
+use crate::{get_socket_without_holding_filetable_lock, log_syscall_entry};
+
+use super::SyscallReturn;
+use super::SYS_ACCEPT;
+
+pub fn sys_accept(
+    sockfd: FileDescripter,
+    sockaddr_ptr: Vaddr,
+    addr_len: u32,
+) -> Result<SyscallReturn> {
+    log_syscall_entry!(SYS_ACCEPT);
+    debug!("sockfd = {sockfd}, sockaddr_ptr = 0x{sockaddr_ptr:x}, addr_len = {addr_len}");
+    let current = current!();
+    get_socket_without_holding_filetable_lock!(socket, current, sockfd);
+    let (connected_socket, socket_addr) = socket.accept()?;
+    write_socket_addr_to_user(&socket_addr, sockaddr_ptr, addr_len as usize)?;
+    let fd = {
+        let mut file_table = current.file_table().lock();
+        file_table.insert(connected_socket)
+    };
+    Ok(SyscallReturn::Return(fd as _))
+}
