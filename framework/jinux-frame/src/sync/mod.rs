ba4121cd6a102efaafd7893e73554fe03742c6c9
Chuandong Li <lichuandong.lcd@antgroup.com>
2023-07-02 18:18:35 +0800

    Add spin lock without disabling local irq

--- framework/jinux-frame/src/sync/mod.rs
+++ framework/jinux-frame/src/sync/mod.rs
@@ -7,5 +7,5 @@ mod wait;
 pub use self::atomic_bits::AtomicBits;
 pub use self::mutex::{Mutex, MutexGuard};
 pub use self::rcu::{pass_quiescent_state, OwnerPtr, Rcu, RcuReadGuard, RcuReclaimer};
-pub use self::spin::{SpinLock, SpinLockGuard};
+pub use self::spin::{SpinLock, SpinLockGuard, SpinLockIrqDisabledGuard};
 pub use self::wait::WaitQueue;
