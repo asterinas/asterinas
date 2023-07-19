d407d85e70503bd9805b0e0c0f8ec4cade7a1452
Zhang Junyang <zhangjunyang.zjy@antgroup.com>
2023-07-19 15:38:47 +0800

    Adapt Docker environment and minor fixes

--- build/grub/scripts/build-grub-image.sh
+++ build/grub/scripts/build-grub-image.sh
@@ -1,16 +1,17 @@
 #! /bin/sh
+set -eux
 
 # Cargo passes the path to the built executable as the first argument.
 KERNEL=$1
 
 # Copy the needed files into an ISO image.
 mkdir -p target/iso_root
-cp $KERNEL target/iso_root/jinux
+cp $KERNEL target/iso_root/boot/jinux
 mkdir -p target/iso_root/boot/grub
 cp build/grub/conf/grub.cfg target/iso_root/boot/grub
 
 # Copy ramdisk
-cp regression/build/ramdisk.cpio.gz target/iso_root
+cp regression/build/ramdisk.cpio.gz target/iso_root/boot
 
 # Make boot device .iso image
 grub-mkrescue -o $KERNEL.iso target/iso_root
