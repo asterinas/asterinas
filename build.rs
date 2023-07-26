bb03be3cbd77e61b9aa8939651ec46eb1552b517
Zhang Junyang <zhangjunyang.zjy@antgroup.com>
2023-07-26 16:11:16 +0800

    Refactor the build package to absorb the script

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    let linker_script_path = "build/grub/conf/linker.ld";
+    let linker_script_path = "framework/jinux-frame/src/arch/x86/boot/linker.ld";
     println!("cargo:rerun-if-changed={}", linker_script_path);
     println!("cargo:rustc-link-arg=-T{}", linker_script_path);
     println!("cargo:rerun-if-env-changed=CARGO_PKG_NAME");
