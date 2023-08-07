12fc074f5638c2f9058ff1f2ef89928e2bad1081
Hsy-Intel <siyuan.hui@intel.com>
2023-08-07 10:56:03 +0800

    Add tdx-guest crate

---
+++ framework/libs/tdx-guest/src/asm/tdvmcall.asm
@@ -0,0 +1,96 @@
+.section .text
+
+# Mask used to control which part of the guest TD GPR and XMM
+# state is exposed to the VMM. A bit value of 1 indicates the
+# corresponding register is passed to VMM. Refer to TDX Module
+# ABI specification section TDG.VP.VMCALL for detail.
+# Here we expose R10 - R15 to VMM in td_vm_call()
+.equ TDVMCALL_EXPOSE_REGS_MASK, 0xfc00
+
+# TDG.VP.VMCALL leaf number
+.equ TDVMCALL, 0
+
+# Arguments offsets in TdVmcallArgs struct
+.equ VMCALL_ARG_R10, 0x0
+.equ VMCALL_ARG_R11, 0x8
+.equ VMCALL_ARG_R12, 0x10
+.equ VMCALL_ARG_R13, 0x18
+.equ VMCALL_ARG_R14, 0x20
+.equ VMCALL_ARG_R15, 0x28
+
+# asm_td_vmcall -> u64 (
+#   args: *mut TdVmcallArgs,
+# )
+.global asm_td_vmcall
+asm_td_vmcall:
+        endbr64
+        # Save the registers accroding to MS x64 calling convention
+        push rbp
+        mov rbp, rsp
+        push r15
+        push r14
+        push r13
+        push r12
+        push rbx
+        push rsi
+        push rdi
+
+        # Use RDI to save RCX value
+        mov rdi, rcx
+
+        # Test if input pointer is valid
+        test rdi, rdi
+        jz vmcall_exit
+
+        # Copy the input operands from memory to registers
+        mov r10, [rdi + VMCALL_ARG_R10]
+        mov r11, [rdi + VMCALL_ARG_R11]
+        mov r12, [rdi + VMCALL_ARG_R12]
+        mov r13, [rdi + VMCALL_ARG_R13]
+        mov r14, [rdi + VMCALL_ARG_R14]
+        mov r15, [rdi + VMCALL_ARG_R15]
+
+        # Set TDCALL leaf number
+        mov rax, TDVMCALL
+
+        # Set exposed register mask
+        mov ecx, TDVMCALL_EXPOSE_REGS_MASK
+
+        # TDCALL
+       .byte 0x66,0x0f,0x01,0xcc
+
+        # RAX should always be zero for TDVMCALL, panic if it is not.
+        test rax, rax
+        jnz vmcall_panic
+
+        # Copy the output operands from registers to the struct
+        mov [rdi + VMCALL_ARG_R10], r10
+        mov [rdi + VMCALL_ARG_R11], r11
+        mov [rdi + VMCALL_ARG_R12], r12
+        mov [rdi + VMCALL_ARG_R13], r13
+        mov [rdi + VMCALL_ARG_R14], r14
+        mov [rdi + VMCALL_ARG_R15], r15
+
+        mov rax, r10
+
+vmcall_exit:
+        # Clean the registers that are exposed to VMM to
+        # protect against speculative attack, others will
+        # be restored to the values saved in stack
+        xor r10, r10
+        xor r11, r11
+
+        # Pop out saved registers from stack
+        pop rdi
+        pop rsi
+        pop rbx
+        pop r12
+        pop r13
+        pop r14
+        pop r15
+        pop rbp
+
+        ret
+
+vmcall_panic:
+        ud2
---
+++ framework/libs/tdx-guest/src/lib.rs
@@ -0,0 +1,81 @@
+#![no_std]
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+extern crate alloc;
+
+pub mod asm;
+pub mod tdcall;
+pub mod tdvmcall;
+
+pub use self::tdcall::{deadloop, tdg_vp_veinfo_get, TdxVirtualExceptionType};
+pub use self::tdvmcall::print;
+
+use alloc::string::{String, ToString};
+use raw_cpuid::{native_cpuid::cpuid_count, CpuIdResult};
+use tdcall::{tdg_vp_vmcall, InitError, TdgVeInfo, TdgVpInfo};
+
+const TDX_CPUID_LEAF_ID: u64 = 0x21;
+
+pub fn tdx_early_init() -> Result<TdgVpInfo, InitError> {
+    match is_tdx_guest() {
+        Ok(_) => Ok(tdcall::tdg_vp_info()?),
+        Err(err) => Err(err),
+    }
+}
+
+fn is_tdx_guest() -> Result<(), InitError> {
+    let cpuid_leaf = cpuid_count(0, 0).eax as u64;
+    if cpuid_leaf < TDX_CPUID_LEAF_ID {
+        return Err(InitError::TdxCpuLeafIdError);
+    }
+    let cpuid_result: CpuIdResult = cpuid_count(TDX_CPUID_LEAF_ID as u32, 0);
+    if convert_ascii(cpuid_result.ebx) == "Inte"
+        && convert_ascii(cpuid_result.edx) == "lTDX"
+        && convert_ascii(cpuid_result.ecx) == "    "
+    {
+        Ok(())
+    } else {
+        Err(InitError::TdxVendorIdError)
+    }
+}
+
+fn convert_ascii(reg: u32) -> String {
+    let bytes = [
+        (reg & 0xFF) as u8,
+        ((reg >> 8) & 0xFF) as u8,
+        ((reg >> 16) & 0xFF) as u8,
+        ((reg >> 24) & 0xFF) as u8,
+    ];
+    String::from_utf8_lossy(&bytes).to_string()
+}
+
+pub trait TdxTrapFrame {
+    fn rax(&self) -> usize;
+    fn set_rax(&mut self, rax: usize);
+    fn rbx(&self) -> usize;
+    fn set_rbx(&mut self, rbx: usize);
+    fn rcx(&self) -> usize;
+    fn set_rcx(&mut self, rcx: usize);
+    fn rdx(&self) -> usize;
+    fn set_rdx(&mut self, rdx: usize);
+    fn rsi(&self) -> usize;
+    fn set_rsi(&mut self, rsi: usize);
+    fn rdi(&self) -> usize;
+    fn set_rdi(&mut self, rdi: usize);
+    fn rip(&self) -> usize;
+    fn set_rip(&mut self, rip: usize);
+}
+
+pub fn virtual_exception_handler(trapframe: &mut impl TdxTrapFrame, ve_info: &TdgVeInfo) {
+    match ve_info.exit_reason.into() {
+        TdxVirtualExceptionType::Hlt
+        | TdxVirtualExceptionType::Io
+        | TdxVirtualExceptionType::MsrRead
+        | TdxVirtualExceptionType::MsrWrite
+        | TdxVirtualExceptionType::CpuId => tdg_vp_vmcall(trapframe, ve_info),
+        TdxVirtualExceptionType::Other => panic!("Unknown TDX vitrual exception type"),
+        _ => return,
+    };
+    trapframe.set_rip(trapframe.rip() + ve_info.exit_instruction_length as usize);
+}
