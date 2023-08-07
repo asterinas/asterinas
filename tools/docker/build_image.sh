c90c8df3a51465d4af01cc67ba71be6f0e2aef23
Jianfeng Jiang <jiangjianfeng.jjf@antgroup.com>
2023-08-07 11:35:01 +0800

    Update rust version in docker image

--- tools/docker/build_image.sh
+++ tools/docker/build_image.sh
@@ -8,7 +8,12 @@ VERSION=$( grep -m1 -o '[0-9]\+\.[0-9]\+\.[0-9]\+' ${CARGO_TOML_PATH} | sed 's/[
 IMAGE_NAME=jinuxdev/jinux:${VERSION}
 DOCKER_FILE=${SCRIPT_DIR}/Dockerfile.ubuntu22.04
 ARCH=linux/amd64
+RUST_TOOLCHAIN_PATH=${SCRIPT_DIR}/../../rust-toolchain.toml
+JINUX_RUST_VERSION=$( grep -m1 -o 'nightly-[0-9]\+-[0-9]\+-[0-9]\+' ${RUST_TOOLCHAIN_PATH} )
 
 # Build docker
 cd ${SCRIPT_DIR}
-docker buildx build -f ${DOCKER_FILE} --platform ${ARCH} -t ${IMAGE_NAME} .
+docker buildx build -f ${DOCKER_FILE} \
+    --build-arg JINUX_RUST_VERSION=${JINUX_RUST_VERSION} \
+    --platform ${ARCH} \
+    -t ${IMAGE_NAME} .
