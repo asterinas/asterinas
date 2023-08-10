be243745319a122faf660ba6eb0997bc9291104a
Yuke Peng <pengyuke.pyk@antgroup.com>
2023-08-10 16:23:02 +0800

    Remove jinux-pci and hide PCI ports

--- framework/jinux-frame/src/arch/x86/pci.rs
+++ framework/jinux-frame/src/arch/x86/pci.rs
@@ -1,7 +1,6 @@
 //! PCI bus io port
 
-use super::io_port::IoPort;
-use super::io_port::{ReadWriteAccess, WriteOnlyAccess};
+use super::device::io_port::{IoPort, ReadWriteAccess, WriteOnlyAccess};
 
 pub static PCI_ADDRESS_PORT: IoPort<u32, WriteOnlyAccess> = unsafe { IoPort::new(0x0CF8) };
 pub static PCI_DATA_PORT: IoPort<u32, ReadWriteAccess> = unsafe { IoPort::new(0x0CFC) };
