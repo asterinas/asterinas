de9c218f09ea9faa42245a1f5549fc826ed80f0a
Hsy-Intel <siyuan.hui@intel.com>
2023-08-28 11:13:12 +0800

    implement virtual_exception_handler in Jinux

--- framework/jinux-frame/src/arch/x86/cpu.rs
+++ framework/jinux-frame/src/arch/x86/cpu.rs
@@ -8,7 +8,11 @@ use trapframe::{GeneralRegs, UserContext as RawUserContext};
 
 use log::debug;
 #[cfg(feature = "intel_tdx")]
-use tdx_guest::*;
+use tdx_guest::{serial_println, TdxVirtualExceptionType};
+#[cfg(feature = "intel_tdx")]
+use tdx_guest::tdcall;
+#[cfg(feature = "intel_tdx")]
+use tdx_guest::tdvmcall;
 use x86_64::registers::rflags::RFlags;
 
 use crate::trap::call_irq_callback_functions;
@@ -43,52 +47,72 @@ pub struct TrapInformation {
 }
 
 #[cfg(feature = "intel_tdx")]
-struct VeGeneralRegs<'a>(&'a mut GeneralRegs);
+pub fn virtual_exception_handler(trapframe: &mut GeneralRegs, ve_info: &tdcall::TdgVeInfo) {
+    match ve_info.exit_reason.into() {
+        TdxVirtualExceptionType::Hlt => {
+            serial_println!("Ready to halt");
+            tdvmcall::hlt();
+        }
+        TdxVirtualExceptionType::Io => {
+            if !handle_ve_io(trapframe, ve_info) {
+                serial_println!("Handle tdx ioexit errors, ready to halt");
+                tdvmcall::hlt();
+            }
+        }
+        TdxVirtualExceptionType::MsrRead => {
+            let msr = tdvmcall::rdmsr(trapframe.rcx as u32).unwrap();
+            trapframe.rax = (msr as u32 & u32::MAX) as usize;
+            trapframe.rdx = ((msr >> 32) as u32 & u32::MAX) as usize;
+        }
+        TdxVirtualExceptionType::MsrWrite => {
+            let data = trapframe.rax as u64 | ((trapframe.rdx as u64) << 32);
+            tdvmcall::wrmsr(trapframe.rcx as u32, data).unwrap();
+        }
+        TdxVirtualExceptionType::CpuId => {
+            let cpuid_info = tdvmcall::cpuid(trapframe.rax as u32, trapframe.rcx as u32).unwrap();
+            let mask = 0xFFFF_FFFF_0000_0000_usize;
+            trapframe.rax = (trapframe.rax & mask) | cpuid_info.eax;
+            trapframe.rbx = (trapframe.rbx & mask) | cpuid_info.ebx;
+            trapframe.rcx = (trapframe.rcx & mask) | cpuid_info.ecx;
+            trapframe.rdx = (trapframe.rdx & mask) | cpuid_info.edx;
+        }
+        TdxVirtualExceptionType::Other => panic!("Unknown TDX vitrual exception type"),
+        _ => return,
+    }
+    trapframe.rip = trapframe.rip + ve_info.exit_instruction_length as usize;
+}
 
 #[cfg(feature = "intel_tdx")]
-impl TdxTrapFrame for VeGeneralRegs<'_> {
-    fn rax(&self) -> usize {
-        self.0.rax
-    }
-    fn set_rax(&mut self, rax: usize) {
-        self.0.rax = rax;
-    }
-    fn rbx(&self) -> usize {
-        self.0.rbx
-    }
-    fn set_rbx(&mut self, rbx: usize) {
-        self.0.rbx = rbx;
-    }
-    fn rcx(&self) -> usize {
-        self.0.rcx
-    }
-    fn set_rcx(&mut self, rcx: usize) {
-        self.0.rcx = rcx;
-    }
-    fn rdx(&self) -> usize {
-        self.0.rdx
-    }
-    fn set_rdx(&mut self, rdx: usize) {
-        self.0.rdx = rdx;
-    }
-    fn rsi(&self) -> usize {
-        self.0.rsi
-    }
-    fn set_rsi(&mut self, rsi: usize) {
-        self.0.rsi = rsi;
-    }
-    fn rdi(&self) -> usize {
-        self.0.rdi
-    }
-    fn set_rdi(&mut self, rdi: usize) {
-        self.0.rdi = rdi;
-    }
-    fn rip(&self) -> usize {
-        self.0.rip
-    }
-    fn set_rip(&mut self, rip: usize) {
-        self.0.rip = rip;
-    }
+pub fn handle_ve_io(trapframe: &mut GeneralRegs, ve_info: &tdcall::TdgVeInfo) -> bool {
+    let size = match ve_info.exit_qualification & 0x3 {
+        0 => 1,
+        1 => 2,
+        3 => 4,
+        _ => panic!("Invalid size value"),
+    };
+    let direction = if (ve_info.exit_qualification >> 3) & 0x1 == 0 {
+        tdvmcall::Direction::Out
+    } else {
+        tdvmcall::Direction::In
+    };
+    let string = (ve_info.exit_qualification >> 4) & 0x1 == 1;
+    let repeat = (ve_info.exit_qualification >> 5) & 0x1 == 1;
+    let operand = if (ve_info.exit_qualification >> 6) & 0x1 == 0 {
+        tdvmcall::Operand::Dx
+    } else {
+        tdvmcall::Operand::Immediate
+    };
+    let port = (ve_info.exit_qualification >> 16) as u16;
+
+    match direction {
+        tdvmcall::Direction::In => {
+            trapframe.rax = tdvmcall::io_read(size, port).unwrap() as usize;
+        }
+        tdvmcall::Direction::Out => {
+            tdvmcall::io_write(size, port, trapframe.rax as u32).unwrap();
+        }
+    };
+    true
 }
 
 impl UserContext {
@@ -128,9 +152,8 @@ impl UserContextApiInternal for UserContext {
                     #[cfg(feature = "intel_tdx")]
                     if *exception == VIRTUALIZATION_EXCEPTION {
                         let ve_info =
-                            tdg_vp_veinfo_get().expect("#VE handler: fail to get VE info\n");
-                        let mut ve_f = VeGeneralRegs(self.general_regs_mut());
-                        virtual_exception_handler(&mut ve_f, &ve_info);
+                            tdcall::get_veinfo().expect("#VE handler: fail to get VE info\n");
+                        virtual_exception_handler(self.general_regs_mut(), &ve_info);
                         continue;
                     }
                     if exception.typ == CpuExceptionType::FaultOrTrap
