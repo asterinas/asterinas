35ca8da436d7c017863c40386e9118fbc62fadc5
Hsy-Intel <siyuan.hui@intel.com>
2023-09-15 10:01:51 +0800

    Handle other CPU exceptions & use trait method

--- framework/jinux-frame/src/arch/x86/tdx_guest.rs
+++ framework/jinux-frame/src/arch/x86/tdx_guest.rs
@@ -5,45 +5,24 @@ use tdx_guest::{
 };
 use trapframe::{GeneralRegs, TrapFrame};
 
-pub struct TdxTrapFrame {
-    rax: usize,
-    rbx: usize,
-    rcx: usize,
-    rdx: usize,
-    rsi: usize,
-    rdi: usize,
-    rip: usize,
+pub trait TdxTrapFrame {
+    fn rax(&self) -> usize;
+    fn set_rax(&mut self, rax: usize);
+    fn rbx(&self) -> usize;
+    fn set_rbx(&mut self, rbx: usize);
+    fn rcx(&self) -> usize;
+    fn set_rcx(&mut self, rcx: usize);
+    fn rdx(&self) -> usize;
+    fn set_rdx(&mut self, rdx: usize);
+    fn rsi(&self) -> usize;
+    fn set_rsi(&mut self, rsi: usize);
+    fn rdi(&self) -> usize;
+    fn set_rdi(&mut self, rdi: usize);
+    fn rip(&self) -> usize;
+    fn set_rip(&mut self, rip: usize);
 }
 
-impl From<TrapFrame> for TdxTrapFrame {
-    fn from(tf: TrapFrame) -> Self {
-        Self {
-            rax: tf.rax,
-            rbx: tf.rbx,
-            rcx: tf.rcx,
-            rdx: tf.rdx,
-            rsi: tf.rsi,
-            rdi: tf.rdi,
-            rip: tf.rip,
-        }
-    }
-}
-
-impl From<GeneralRegs> for TdxTrapFrame {
-    fn from(gr: GeneralRegs) -> Self {
-        Self {
-            rax: gr.rax,
-            rbx: gr.rbx,
-            rcx: gr.rcx,
-            rdx: gr.rdx,
-            rsi: gr.rsi,
-            rdi: gr.rdi,
-            rip: gr.rip,
-        }
-    }
-}
-
-pub fn handle_virtual_exception(trapframe: &mut TdxTrapFrame, ve_info: &TdgVeInfo) {
+pub fn handle_virtual_exception(trapframe: &mut dyn TdxTrapFrame, ve_info: &TdgVeInfo) {
     match ve_info.exit_reason.into() {
         TdxVirtualExceptionType::Hlt => {
             serial_println!("Ready to halt");
@@ -56,29 +35,29 @@ pub fn handle_virtual_exception(trapframe: &mut TdxTrapFrame, ve_info: &TdgVeInf
             }
         }
         TdxVirtualExceptionType::MsrRead => {
-            let msr = unsafe { rdmsr(trapframe.rcx as u32).unwrap() };
-            trapframe.rax = (msr as u32 & u32::MAX) as usize;
-            trapframe.rdx = ((msr >> 32) as u32 & u32::MAX) as usize;
+            let msr = unsafe { rdmsr(trapframe.rcx() as u32).unwrap() };
+            trapframe.set_rax((msr as u32 & u32::MAX) as usize);
+            trapframe.set_rdx(((msr >> 32) as u32 & u32::MAX) as usize);
         }
         TdxVirtualExceptionType::MsrWrite => {
-            let data = trapframe.rax as u64 | ((trapframe.rdx as u64) << 32);
-            unsafe { wrmsr(trapframe.rcx as u32, data).unwrap() };
+            let data = trapframe.rax() as u64 | ((trapframe.rdx() as u64) << 32);
+            unsafe { wrmsr(trapframe.rcx() as u32, data).unwrap() };
         }
         TdxVirtualExceptionType::CpuId => {
-            let cpuid_info = cpuid(trapframe.rax as u32, trapframe.rcx as u32).unwrap();
+            let cpuid_info = cpuid(trapframe.rax() as u32, trapframe.rcx() as u32).unwrap();
             let mask = 0xFFFF_FFFF_0000_0000_usize;
-            trapframe.rax = (trapframe.rax & mask) | cpuid_info.eax;
-            trapframe.rbx = (trapframe.rbx & mask) | cpuid_info.ebx;
-            trapframe.rcx = (trapframe.rcx & mask) | cpuid_info.ecx;
-            trapframe.rdx = (trapframe.rdx & mask) | cpuid_info.edx;
+            trapframe.set_rax((trapframe.rax() & mask) | cpuid_info.eax);
+            trapframe.set_rbx((trapframe.rbx() & mask) | cpuid_info.ebx);
+            trapframe.set_rcx((trapframe.rcx() & mask) | cpuid_info.ecx);
+            trapframe.set_rdx((trapframe.rdx() & mask) | cpuid_info.edx);
         }
         TdxVirtualExceptionType::Other => panic!("Unknown TDX vitrual exception type"),
         _ => return,
     }
-    trapframe.rip += ve_info.exit_instruction_length as usize;
+    trapframe.set_rip(trapframe.rip() + ve_info.exit_instruction_length as usize);
 }
 
-fn handle_io(trapframe: &mut TdxTrapFrame, ve_info: &tdcall::TdgVeInfo) -> bool {
+fn handle_io(trapframe: &mut dyn TdxTrapFrame, ve_info: &tdcall::TdgVeInfo) -> bool {
     let size = match ve_info.exit_qualification & 0x3 {
         0 => IoSize::Size1,
         1 => IoSize::Size2,
@@ -99,10 +78,10 @@ fn handle_io(trapframe: &mut TdxTrapFrame, ve_info: &tdcall::TdgVeInfo) -> bool 
 
     match direction {
         tdvmcall::Direction::In => {
-            trapframe.rax = tdvmcall::io_read(size, port).unwrap() as usize;
+            trapframe.set_rax(tdvmcall::io_read(size, port).unwrap() as usize);
         }
         tdvmcall::Direction::Out => {
-            tdvmcall::io_write(size, port, trapframe.rax as u32).unwrap();
+            tdvmcall::io_write(size, port, trapframe.rax() as u32).unwrap();
         }
     };
     true
