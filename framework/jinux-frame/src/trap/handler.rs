f2b2c20967803a8d0bcd261409b8a7c4506e9d48
Hsy-Intel <siyuan.hui@intel.com>
2023-09-15 10:01:51 +0800

    Handle other CPU exceptions & use trait method

--- framework/jinux-frame/src/trap/handler.rs
+++ framework/jinux-frame/src/trap/handler.rs
@@ -1,10 +1,59 @@
 use crate::{arch::irq::IRQ_LIST, cpu::CpuException};
 
 #[cfg(feature = "intel_tdx")]
-use crate::arch::tdx_guest::handle_virtual_exception;
+use crate::arch::tdx_guest::{handle_virtual_exception, TdxTrapFrame};
 #[cfg(feature = "intel_tdx")]
 use tdx_guest::tdcall;
 use trapframe::TrapFrame;
+
+#[cfg(feature = "intel_tdx")]
+struct VeTrapFrame<'a>(&'a mut TrapFrame);
+
+#[cfg(feature = "intel_tdx")]
+impl TdxTrapFrame for VeTrapFrame<'_> {
+    fn rax(&self) -> usize {
+        self.0.rax
+    }
+    fn set_rax(&mut self, rax: usize) {
+        self.0.rax = rax;
+    }
+    fn rbx(&self) -> usize {
+        self.0.rbx
+    }
+    fn set_rbx(&mut self, rbx: usize) {
+        self.0.rbx = rbx;
+    }
+    fn rcx(&self) -> usize {
+        self.0.rcx
+    }
+    fn set_rcx(&mut self, rcx: usize) {
+        self.0.rcx = rcx;
+    }
+    fn rdx(&self) -> usize {
+        self.0.rdx
+    }
+    fn set_rdx(&mut self, rdx: usize) {
+        self.0.rdx = rdx;
+    }
+    fn rsi(&self) -> usize {
+        self.0.rsi
+    }
+    fn set_rsi(&mut self, rsi: usize) {
+        self.0.rsi = rsi;
+    }
+    fn rdi(&self) -> usize {
+        self.0.rdi
+    }
+    fn set_rdi(&mut self, rdi: usize) {
+        self.0.rdi = rdi;
+    }
+    fn rip(&self) -> usize {
+        self.0.rip
+    }
+    fn set_rip(&mut self, rip: usize) {
+        self.0.rip = rip;
+    }
+}
 
 /// Only from kernel
 #[no_mangle]
@@ -13,9 +62,10 @@ extern "sysv64" fn trap_handler(f: &mut TrapFrame) {
         #[cfg(feature = "intel_tdx")]
         if f.trap_num as u16 == 20 {
             let ve_info = tdcall::get_veinfo().expect("#VE handler: fail to get VE info\n");
-            handle_virtual_exception(&mut (*f).into(), &ve_info);
+            let mut ve_f = VeTrapFrame(f);
+            handle_virtual_exception(&mut ve_f, &ve_info);
+            return;
         }
-        #[cfg(not(feature = "intel_tdx"))]
         panic!("cannot handle kernel cpu fault now, information:{:#x?}", f);
     } else {
         call_irq_callback_functions(f);
