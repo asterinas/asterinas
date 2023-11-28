ceaba95fa0e3254afb0f1c30904189eb25a12f58
Chuandong Li <lichuandong.lcd@antgroup.com>
2023-11-29 01:05:00 +0800

    Support coherent dma mappings

--- framework/jinux-frame/src/arch/x86/iommu/mod.rs
+++ framework/jinux-frame/src/arch/x86/iommu/mod.rs
@@ -3,16 +3,17 @@ mod fault;
 mod remapping;
 mod second_stage;
 
-use crate::{sync::Mutex, vm::VmFrame};
 use log::info;
 use spin::Once;
 
 use crate::{
     arch::iommu::{context_table::RootTable, second_stage::PageTableEntry},
     bus::pci::PciDeviceLocation,
+    sync::Mutex,
     vm::{
-        page_table::{PageTableConfig, PageTableError},
-        PageTable, Vaddr,
+        dma::Daddr,
+        page_table::{DeviceMode, PageTableConfig, PageTableError},
+        Paddr, PageTable,
     },
 };
 
@@ -22,27 +23,18 @@ pub enum IommuError {
     ModificationError(PageTableError),
 }
 
-// FIXME: Perform map operations by obtaining ownership of a VmFrame.
 ///
 /// # Safety
 ///
 /// Mapping an incorrect address may lead to a kernel data leak.
-pub(crate) unsafe fn map(vaddr: Vaddr, frame: &VmFrame) -> Result<(), IommuError> {
+pub(crate) unsafe fn map(daddr: Daddr, paddr: Paddr) -> Result<(), IommuError> {
     let Some(table) = PAGE_TABLE.get() else {
         return Err(IommuError::NoIommu);
     };
     // The page table of all devices is the same. So we can use any device ID.
     table
         .lock()
-        .map(
-            PciDeviceLocation {
-                bus: 0,
-                device: 0,
-                function: 0,
-            },
-            vaddr,
-            frame,
-        )
+        .map(PciDeviceLocation::zero(), daddr, paddr)
         .map_err(|err| match err {
             context_table::ContextTableError::InvalidDeviceId => unreachable!(),
             context_table::ContextTableError::ModificationError(err) => {
@@ -51,21 +43,14 @@ pub(crate) unsafe fn map(vaddr: Vaddr, frame: &VmFrame) -> Result<(), IommuError
         })
 }
 
-pub(crate) fn unmap(vaddr: Vaddr) -> Result<(), IommuError> {
+pub(crate) fn unmap(daddr: Daddr) -> Result<(), IommuError> {
     let Some(table) = PAGE_TABLE.get() else {
         return Err(IommuError::NoIommu);
     };
     // The page table of all devices is the same. So we can use any device ID.
     table
         .lock()
-        .unmap(
-            PciDeviceLocation {
-                bus: 0,
-                device: 0,
-                function: 0,
-            },
-            vaddr,
-        )
+        .unmap(PciDeviceLocation::zero(), daddr)
         .map_err(|err| match err {
             context_table::ContextTableError::InvalidDeviceId => unreachable!(),
             context_table::ContextTableError::ModificationError(err) => {
@@ -77,9 +62,10 @@ pub(crate) fn unmap(vaddr: Vaddr) -> Result<(), IommuError> {
 pub(crate) fn init() -> Result<(), IommuError> {
     let mut root_table = RootTable::new();
     // For all PCI Device, use the same page table.
-    let page_table: PageTable<PageTableEntry> = PageTable::new(PageTableConfig {
-        address_width: crate::vm::page_table::AddressWidth::Level3,
-    });
+    let page_table: PageTable<PageTableEntry, DeviceMode> =
+        PageTable::<PageTableEntry, DeviceMode>::new(PageTableConfig {
+            address_width: crate::vm::page_table::AddressWidth::Level3,
+        });
     for table in PciDeviceLocation::all() {
         root_table.specify_device_page_table(table, page_table.clone())
     }
@@ -87,6 +73,10 @@ pub(crate) fn init() -> Result<(), IommuError> {
     PAGE_TABLE.call_once(|| Mutex::new(root_table));
     info!("IOMMU enabled");
     Ok(())
+}
+
+pub(crate) fn has_iommu() -> bool {
+    PAGE_TABLE.get().is_some()
 }
 
 static PAGE_TABLE: Once<Mutex<RootTable>> = Once::new();
