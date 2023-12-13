ef2d5e69edbf348b569cd9f9e3bc00e29d37df43
Jianfeng Jiang <jiangjianfeng.jjf@antgroup.com>
2023-12-13 07:36:28 +0000

    [BUG] Only write time to user if addr is not zero

--- services/libs/jinux-std/src/syscall/time.rs
+++ services/libs/jinux-std/src/syscall/time.rs
@@ -3,14 +3,20 @@ use crate::prelude::*;
 use crate::time::SystemTime;
 use crate::util::write_val_to_user;
 
-use super::SyscallReturn;
-use super::SYS_TIME;
+use super::{SyscallReturn, SYS_TIME};
 
 pub fn sys_time(tloc: Vaddr) -> Result<SyscallReturn> {
     log_syscall_entry!(SYS_TIME);
     debug!("tloc = 0x{tloc:x}");
-    let now = SystemTime::now();
-    let now_as_secs = now.duration_since(&SystemTime::UNIX_EPOCH)?.as_secs();
-    write_val_to_user(tloc, &now_as_secs)?;
+
+    let now_as_secs = {
+        let now = SystemTime::now();
+        now.duration_since(&SystemTime::UNIX_EPOCH)?.as_secs()
+    };
+
+    if tloc != 0 {
+        write_val_to_user(tloc, &now_as_secs)?;
+    }
+
     Ok(SyscallReturn::Return(0))
 }
