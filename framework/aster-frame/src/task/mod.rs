77c27e9751b6954293f56d51077eeae314fdab92
fgh1999 <fgh@stu.pku.edu.cn>
2024-01-10 15:31:32 +0000

    Refactor PreemptInfo and preempt disabling.
    Fix #478.
    
    The global PROCESSOR are now guarded by SpinLock.

--- framework/aster-frame/src/task/mod.rs
+++ framework/aster-frame/src/task/mod.rs
@@ -1,12 +1,22 @@
 //! Tasks are the unit of code execution.
 
+mod nice;
+mod preempt;
 mod priority;
 mod processor;
 mod scheduler;
 #[allow(clippy::module_inception)]
 mod task;
 
+pub use self::nice::Nice;
+pub use self::preempt::{in_atomic, preemptible, DisablePreemptGuard};
 pub use self::priority::Priority;
-pub use self::processor::{current_task, disable_preempt, preempt, schedule, DisablePreemptGuard};
-pub use self::scheduler::{add_task, set_scheduler, Scheduler};
-pub use self::task::{Task, TaskAdapter, TaskOptions, TaskStatus};
+pub use self::processor::{
+    current_task, init as init_processor, schedule, switch_to, try_preempt, yield_now,
+};
+pub use self::scheduler::{
+    add_task, init as init_scheduler, remove_task, set_scheduler, Scheduler,
+};
+pub use self::task::{
+    NeedResched, ReadPriority, Task, TaskAdapter, TaskOptions, TaskStatus, WakeUp,
+};
