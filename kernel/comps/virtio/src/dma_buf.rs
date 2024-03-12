deba8783f4ceb40a44d3f6e5a792eb1ca8a77e2d
Jianfeng Jiang <jiangjianfeng.jjf@antgroup.com>
2024-03-12 11:09:15 +0000

    Add DmaBuf for DMA-capable memory areas

---
+++ kernel/comps/virtio/src/dma_buf.rs
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MPL-2.0
+
+use aster_frame::{
+    config::PAGE_SIZE,
+    vm::{Daddr, DmaCoherent, DmaStream, HasDaddr},
+};
+
+#[allow(clippy::len_without_is_empty)]
+pub trait DmaBuf {
+    fn addr(&self) -> Daddr;
+    fn len(&self) -> usize;
+}
+
+impl DmaBuf for DmaStream {
+    fn addr(&self) -> Daddr {
+        self.daddr()
+    }
+
+    fn len(&self) -> usize {
+        self.nbytes()
+    }
+}
+
+impl DmaBuf for DmaCoherent {
+    fn addr(&self) -> Daddr {
+        self.daddr()
+    }
+
+    fn len(&self) -> usize {
+        self.nframes() * PAGE_SIZE
+    }
+}
+
+impl DmaBuf for (Daddr, usize) {
+    fn addr(&self) -> Daddr {
+        self.0
+    }
+
+    fn len(&self) -> usize {
+        self.1
+    }
+}
