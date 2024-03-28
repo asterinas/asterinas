33f093dc5a7d2e5192d7b7d623f3733f6dec5f11
Chen Chengjun <chenchengjun.ccj@antgroup.com>
2024-03-28 17:34:50 +0800

    Re-implement the alarm syscall

--- kernel/aster-nix/src/syscall/alarm.rs
+++ kernel/aster-nix/src/syscall/alarm.rs
@@ -3,17 +3,14 @@
 use core::time::Duration;
 
 use super::{SyscallReturn, SYS_ALARM};
-use crate::{log_syscall_entry, prelude::*, process::posix_thread::PosixThreadExt};
+use crate::{log_syscall_entry, prelude::*};
 
 pub fn sys_alarm(seconds: u32) -> Result<SyscallReturn> {
     log_syscall_entry!(SYS_ALARM);
     debug!("seconds = {}", seconds);
 
-    let current_thread = current_thread!();
-    let mut real_timer = {
-        let posix_thread = current_thread.as_posix_thread().unwrap();
-        posix_thread.real_timer().lock()
-    };
+    let current = current!();
+    let real_timer = current.real_timer();
 
     let remaining_secs = real_timer.remain().as_secs();
 
@@ -23,7 +20,7 @@ pub fn sys_alarm(seconds: u32) -> Result<SyscallReturn> {
         return Ok(SyscallReturn::Return(remaining_secs as _));
     }
 
-    real_timer.set(Duration::from_secs(seconds as u64))?;
+    real_timer.set_timer(Duration::from_secs(seconds as u64));
 
     Ok(SyscallReturn::Return(remaining_secs as _))
 }
