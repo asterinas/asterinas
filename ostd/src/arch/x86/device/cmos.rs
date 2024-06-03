ae5395465600c176ea1723566b50701b8783be20
Yuke Peng <pykfirst@gmail.com>
2024-06-03 16:11:06 +0800

    Add documentation to x86/device

--- ostd/src/arch/x86/device/cmos.rs
+++ ostd/src/arch/x86/device/cmos.rs
@@ -1,5 +1,12 @@
 // SPDX-License-Identifier: MPL-2.0
 
+//! Provides CMOS I/O port access.
+//!
+//! "CMOS" is a tiny bit of very low power static memory that lives on the same chip as the Real-Time Clock (RTC).
+//!
+//! Reference: <https://wiki.osdev.org/CMOS>
+//!
+
 #![allow(unused_variables)]
 
 use acpi::{fadt::Fadt, sdt::Signature};
@@ -8,10 +15,14 @@ use x86_64::instructions::port::{ReadOnlyAccess, WriteOnlyAccess};
 use super::io_port::IoPort;
 use crate::arch::x86::kernel::acpi::ACPI_TABLES;
 
+/// CMOS address I/O port
 pub static CMOS_ADDRESS: IoPort<u8, WriteOnlyAccess> = unsafe { IoPort::new(0x70) };
+
+/// CMOS data I/O port
 pub static CMOS_DATA: IoPort<u8, ReadOnlyAccess> = unsafe { IoPort::new(0x71) };
 
-pub fn get_century_register() -> Option<u8> {
+/// Gets the century register location. This function is used in RTC(Real Time Clock) module initialization.
+pub fn century_register() -> Option<u8> {
     if !ACPI_TABLES.is_completed() {
         return None;
     }
