fd5fe26faeefee517bcfc3fe7e281cfe1c09b29b
Yuke Peng <pykfirst@gmail.com>
2024-08-02 21:27:50 +0800

    Add file read bandwidth test

---
+++ test/benchmark/lmbench-file-rd-bandwidth/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# SPDX-License-Identifier: MPL-2.0
+
+set -e
+
+echo "*** Running the LMbench file read bandwidth test ***"
+
+dd if=/dev/zero of=/ext2/test_file bs=1M count=512
+/benchmark/bin/lmbench/bw_file_rd -P 1 512m io_only /ext2/test_file
