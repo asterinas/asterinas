923ca16d32337f76205aa0bfe456cca15ba659af
Marsman1996 <lqliuyuwei@outlook.com>
2024-08-08 11:02:38 +0800

    Make code more readable

--- osdk/src/commands/new/mod.rs
+++ osdk/src/commands/new/mod.rs
@@ -163,13 +163,14 @@ fn get_manifest_path<'a>(cargo_metadata: &'a serde_json::Value, crate_name: &str
 fn get_src_path<'a>(cargo_metadata: &'a serde_json::Value, crate_name: &str) -> &'a str {
     let metadata = get_package_metadata(cargo_metadata, crate_name);
     let targets = metadata.get("targets").unwrap().as_array().unwrap();
-    if targets.len() == 1 {
-        let target = &targets[0];
-        let src_path = target.get("src_path").unwrap();
-        return src_path.as_str().unwrap();
-    }
+    assert!(
+        targets.len() == 1,
+        "there must be one and only one target generated"
+    );
 
-    panic!("there is no target generated");
+    let target = &targets[0];
+    let src_path = target.get("src_path").unwrap();
+    return src_path.as_str().unwrap();
 }
 
 fn get_workspace_root(cargo_metadata: &serde_json::Value) -> &str {
