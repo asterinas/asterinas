9e2956b4a3600ffa5be0b1da52af006455d66f12
Yuke Peng <pykfirst@gmail.com>
2024-08-16 12:42:33 +0800

    Add lmbench mmap bandwidth test

---
+++ test/benchmark/lmbench-mmap-bandwidth/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# SPDX-License-Identifier: MPL-2.0
+
+set -e
+
+echo "*** Running the LMbench mmap bandwidth test ***"
+
+dd if=/dev/zero of=/ext2/test_file bs=1M count=256
+/benchmark/bin/lmbench/bw_mmap_rd 256m mmap_only /ext2/test_file
