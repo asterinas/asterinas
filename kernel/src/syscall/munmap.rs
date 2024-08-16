aa77747f94c4b1cb1237ba52414642827a6efc25
Jianfeng Jiang <jiangjianfeng.jjf@antgroup.com>
2024-08-16 07:38:56 +0000

    Make mprotect&munmap return fast when len is zero

--- kernel/src/syscall/munmap.rs
+++ kernel/src/syscall/munmap.rs
@@ -7,6 +7,14 @@ use crate::prelude::*;
 
 pub fn sys_munmap(addr: Vaddr, len: usize, ctx: &Context) -> Result<SyscallReturn> {
     debug!("addr = 0x{:x}, len = {}", addr, len);
+
+    if addr % PAGE_SIZE != 0 {
+        return_errno_with_message!(Errno::EINVAL, "munmap addr must be page-aligned");
+    }
+    if len == 0 {
+        return_errno_with_message!(Errno::EINVAL, "munmap len cannot be zero");
+    }
+
     let root_vmar = ctx.process.root_vmar();
     let len = len.align_up(PAGE_SIZE);
     debug!("unmap range = 0x{:x} - 0x{:x}", addr, addr + len);
