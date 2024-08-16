359c7131c863127a200c1542103d78c91aa656d6
Yuke Peng <pykfirst@gmail.com>
2024-08-16 12:43:44 +0800

    Add lmbench signal protection test

---
+++ test/benchmark/lmbench-signal-prot/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# SPDX-License-Identifier: MPL-2.0
+
+set -e
+
+echo "*** Running the LMbench protection fault latency test ***"
+
+dd if=/dev/zero of=/ext2/test_file bs=1M count=256
+/benchmark/bin/lmbench/lat_sig prot /ext2/test_file
