090b5a1c1ddfb3546f40e5022d4568e76f60afe8
Yuke Peng <pykfirst@gmail.com>
2024-08-25 17:04:25 +0800

    Dsiable mitigations for CPU vulnerabilities in Linux

--- test/benchmark/bench_linux_and_aster.sh
+++ test/benchmark/bench_linux_and_aster.sh
@@ -67,7 +67,7 @@ run_benchmark() {
         -initrd ${BENCHMARK_DIR}/../build/initramfs.cpio.gz \
         -drive if=none,format=raw,id=x0,file=${BENCHMARK_DIR}/../build/ext2.img \
         -device virtio-blk-pci,bus=pcie.0,addr=0x6,drive=x0,serial=vext2,disable-legacy=on,disable-modern=off,queue-size=64,num-queues=1,config-wce=off,request-merging=off,write-cache=off,backend_defaults=off,discard=off,event_idx=off,indirect_desc=off,ioeventfd=off,queue_reset=off \
-        -append 'console=ttyS0 rdinit=/benchmark/benchmark_entrypoint.sh' \
+        -append 'console=ttyS0 rdinit=/benchmark/benchmark_entrypoint.sh mitigations=off' \
         -nographic \
         2>&1 | tee ${linux_output}" 
 
