34352dc0d94a482fd0f2297adcbe870e7efe66c0
Yuke Peng <pykfirst@gmail.com>
2024-08-26 17:36:15 +0800

    Add lmbench TCP connect latency test

---
+++ test/benchmark/lmbench-tcp-connect-latency/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# SPDX-License-Identifier: MPL-2.0
+
+set -e
+
+echo "*** Running lmbench TCP connection latency test***"
+
+/benchmark/bin/lmbench/lat_connect -s
+/benchmark/bin/lmbench/lat_connect 127.0.0.1
+/benchmark/bin/lmbench/lat_connect -S 127.0.0.1
