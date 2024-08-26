e011570748bd66abf3ba993b6989cbccb88072cc
Yuke Peng <pykfirst@gmail.com>
2024-08-26 17:37:30 +0800

    Add lmbench localhost UDP latency test

---
+++ test/benchmark/lmbench-udp-latency-localhost/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# SPDX-License-Identifier: MPL-2.0
+
+set -e
+
+echo "*** Running lmbench UDP latency test ***"
+
+/benchmark/bin/lmbench/lat_udp -s
+/benchmark/bin/lmbench/lat_udp -P 1 127.0.0.1
+/benchmark/bin/lmbench/lat_udp -S 127.0.0.1
