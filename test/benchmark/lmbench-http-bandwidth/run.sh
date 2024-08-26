9fec6ce95a94aa24031e03b626b01b0880729de1
Yuke Peng <pykfirst@gmail.com>
2024-08-26 17:35:03 +0800

    Add lmbench HTTP latency test

---
+++ test/benchmark/lmbench-http-bandwidth/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# SPDX-License-Identifier: MPL-2.0
+
+set -e
+
+echo "*** Running lmbench HTTP latency test ***"
+
+dd if=/dev/zero of=test_file bs=1M count=64
+echo "test_file" > file_list
+/benchmark/bin/lmbench/lmhttp & 
+/benchmark/bin/lmbench/lat_http 127.0.0.1 < file_list
+/benchmark/bin/lmbench/lat_http -S 127.0.0.1
