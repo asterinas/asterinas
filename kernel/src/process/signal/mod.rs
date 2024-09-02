054c64542ba10604bd7ee2c6cf0351cfcaea5498
Ruihan Li <lrh2000@pku.edu.cn>
2024-09-02 22:01:19 +0800

    Use `PollAdaptor` to signal `SIGIO`

--- kernel/src/process/signal/mod.rs
+++ kernel/src/process/signal/mod.rs
@@ -20,7 +20,7 @@ use c_types::{siginfo_t, ucontext_t};
 pub use events::{SigEvents, SigEventsFilter};
 use ostd::{cpu::UserContext, user::UserContextApi};
 pub use pause::{with_signal_blocked, Pause};
-pub use poll::{PollHandle, Pollable, Pollee, Poller};
+pub use poll::{PollAdaptor, PollHandle, Pollable, Pollee, Poller};
 use sig_action::{SigAction, SigActionFlags, SigDefaultAction};
 use sig_mask::SigMask;
 use sig_num::SigNum;
