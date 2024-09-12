66452208108a99c014ce779e6502757ddf85d4ef
Chen Chengjun <chenchengjun.ccj@antgroup.com>
2024-09-12 20:03:44 +0800

    Adjust the test parameters in lmbench mmap/pagefault tests

--- test/benchmark/lmbench-mmap-latency/run.sh
+++ test/benchmark/lmbench-mmap-latency/run.sh
@@ -6,5 +6,5 @@ set -e
 
 echo "*** Running the LMbench mmap latency test ***"
 
-dd if=/dev/zero of=/ext2/test_file bs=1M count=256
-/benchmark/bin/lmbench/lat_mmap 256m /ext2/test_file
+dd if=/dev/zero of=/ext2/test_file bs=1M count=4
+/benchmark/bin/lmbench/lat_mmap 4m /ext2/test_file
