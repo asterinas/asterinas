6304a3cc8813a4c09f95f36c43f443cbaa3efc15
Yuke Peng <pykfirst@gmail.com>
2024-09-25 14:05:27 +0800

    Stabilize some benchmark results

--- test/benchmark/lmbench/ramfs_create_delete_files_10k_ops/run.sh
+++ test/benchmark/lmbench/ramfs_create_delete_files_10k_ops/run.sh
@@ -6,4 +6,4 @@ set -e
 
 echo "*** Running the LMbench file system create/delete test (Ramfs) ***"
 
-/benchmark/bin/lmbench/lat_fs -s 10k -P 1
+/benchmark/bin/lmbench/lat_fs -s 10k -P 1 -W 30 -N 300
--- test/benchmark/lmbench/vfs_read_pagecache_bw/run.sh
+++ test/benchmark/lmbench/vfs_read_pagecache_bw/run.sh
@@ -7,4 +7,4 @@ set -e
 echo "*** Running the LMbench file read bandwidth test ***"
 
 dd if=/dev/zero of=/ext2/test_file bs=1M count=512
-/benchmark/bin/lmbench/bw_file_rd -P 1 512m io_only /ext2/test_file
+/benchmark/bin/lmbench/bw_file_rd -P 1 -W 30 -N 300 512m io_only /ext2/test_file
