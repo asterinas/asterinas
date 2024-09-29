1495d1bb6b08b8e77e7774c87045601b270d48ec
Ruihan Li <lrh2000@pku.edu.cn>
2024-09-29 09:21:28 +0800

    Benchmark TCP localhost with 64K messages

---
+++ test/benchmark/lmbench/tcp_loopback_bw_64k/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# SPDX-License-Identifier: MPL-2.0
+
+set -e
+
+echo "*** Running lmbench TCP bandwidth test ***"
+
+/benchmark/bin/lmbench/bw_tcp -s
+/benchmark/bin/lmbench/bw_tcp -m 65536 -P 1 127.0.0.1
+/benchmark/bin/lmbench/bw_tcp -S 127.0.0.1
