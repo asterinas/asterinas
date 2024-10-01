a212e5e7a8411e9fa741fa5a5c89fcfd2e4d2c91
Yuke Peng <pykfirst@gmail.com>
2024-10-01 13:42:35 +0800

    [OSTD] Refactor pci bus access to support multiple architecture

--- ostd/src/arch/x86/pci.rs
+++ ostd/src/arch/x86/pci.rs
@@ -1,8 +1,26 @@
 // SPDX-License-Identifier: MPL-2.0
 
-//! PCI bus io port
+//! PCI bus access
 
 use super::device::io_port::{IoPort, ReadWriteAccess, WriteOnlyAccess};
+use crate::{bus::pci::PciDeviceLocation, prelude::*};
 
-pub static PCI_ADDRESS_PORT: IoPort<u32, WriteOnlyAccess> = unsafe { IoPort::new(0x0CF8) };
-pub static PCI_DATA_PORT: IoPort<u32, ReadWriteAccess> = unsafe { IoPort::new(0x0CFC) };
+static PCI_ADDRESS_PORT: IoPort<u32, WriteOnlyAccess> = unsafe { IoPort::new(0x0CF8) };
+static PCI_DATA_PORT: IoPort<u32, ReadWriteAccess> = unsafe { IoPort::new(0x0CFC) };
+
+const BIT32_ALIGN_MASK: u32 = 0xFFFC;
+
+pub fn write32(location: &PciDeviceLocation, offset: u32, value: u32) -> Result<()> {
+    PCI_ADDRESS_PORT.write(location.encode_as_x86_port_value() | (offset & BIT32_ALIGN_MASK));
+    PCI_DATA_PORT.write(value.to_le());
+    Ok(())
+}
+
+pub fn read32(location: &PciDeviceLocation, offset: u32) -> Result<u32> {
+    PCI_ADDRESS_PORT.write(location.encode_as_x86_port_value() | (offset & BIT32_ALIGN_MASK));
+    Ok(PCI_DATA_PORT.read().to_le())
+}
+
+pub fn has_pci_bus() -> bool {
+    true
+}
