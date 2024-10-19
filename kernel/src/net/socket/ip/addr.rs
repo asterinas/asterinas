9707b46c7fc20150971dc53437ee6f2a9cdee951
Ruihan Li <lrh2000@pku.edu.cn>
2024-10-19 17:38:30 +0800

    Bump smoltcp version

--- kernel/src/net/socket/ip/addr.rs
+++ kernel/src/net/socket/ip/addr.rs
@@ -9,7 +9,7 @@ impl TryFrom<SocketAddr> for IpEndpoint {
 
     fn try_from(value: SocketAddr) -> Result<Self> {
         match value {
-            SocketAddr::IPv4(addr, port) => Ok(IpEndpoint::new(addr.into_address(), port)),
+            SocketAddr::IPv4(addr, port) => Ok(IpEndpoint::new(addr.into(), port)),
             _ => return_errno_with_message!(
                 Errno::EAFNOSUPPORT,
                 "the address is in an unsupported address family"
