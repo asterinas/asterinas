9e31876ee53d6104d622103ab96fa4ccf6130daf
Yuke Peng <pykfirst@gmail.com>
2024-10-28 16:47:26 +0800

    Implement IoPortDispatcher

--- ostd/src/lib.rs
+++ ostd/src/lib.rs
@@ -34,6 +34,7 @@ pub mod bus;
 pub mod collections;
 pub mod console;
 pub mod cpu;
+pub mod device;
 mod error;
 pub mod io_mem;
 pub mod logger;
@@ -86,6 +87,8 @@ unsafe fn init() {
     mm::page::allocator::init();
     mm::kspace::init_kernel_page_table(mm::init_page_meta());
     mm::dma::init();
+
+    device::dispatcher::init();
 
     arch::init_on_bsp();
 
