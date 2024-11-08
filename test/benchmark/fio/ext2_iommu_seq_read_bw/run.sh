d93676ac7ab116b796bc71faf5552e5fd6ac5a02
Shaowei Song <songshaowei.ssw@antgroup.com>
2024-11-08 05:28:08 +0000

    Add several FIO tests to the benchmark CI

---
+++ test/benchmark/fio/ext2_iommu_seq_read_bw/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# SPDX-License-Identifier: MPL-2.0
+
+set -e
+
+echo "*** Running the FIO sequential read test (Ext2) ***"
+
+/benchmark/bin/fio -rw=read -filename=/ext2/fio-test -name=seqread \
+-size=1G -bs=1M \
+-ioengine=sync -direct=1 -numjobs=1 -fsync_on_close=1 \
+-time_based=1 -runtime=60
