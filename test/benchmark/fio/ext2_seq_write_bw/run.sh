c7404f8139cb8b64e674f2adcf4952d502e1a4f4
Shaowei Song <songshaowei.ssw@antgroup.com>
2024-11-08 05:28:08 +0000

    Add several FIO tests to the benchmark CI

---
+++ test/benchmark/fio/ext2_seq_write_bw/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# SPDX-License-Identifier: MPL-2.0
+
+set -e
+
+echo "*** Running the FIO sequential write test (Ext2) ***"
+
+/benchmark/bin/fio -rw=write -filename=/ext2/fio-test -name=seqwrite \
+-size=1G -bs=1M \
+-ioengine=sync -direct=1 -numjobs=1 -fsync_on_close=1 \
+-time_based=1 -runtime=60
