b88675c4e9edcd4a667740e1428a4e1f768f964e
Shaowei Song <songshaowei.ssw@antgroup.com>
2024-11-20 05:23:11 +0000

    Stabilize the benchmark results of FIO

--- test/benchmark/fio/ext2_iommu_seq_write_bw/run.sh
+++ test/benchmark/fio/ext2_iommu_seq_write_bw/run.sh
@@ -9,4 +9,4 @@ echo "*** Running the FIO sequential write test (Ext2) ***"
 /benchmark/bin/fio -rw=write -filename=/ext2/fio-test -name=seqwrite \
 -size=1G -bs=1M \
 -ioengine=sync -direct=1 -numjobs=1 -fsync_on_close=1 \
--time_based=1 -runtime=60
+-time_based=1 -ramp_time=60 -runtime=100
--- test/benchmark/fio/ext2_seq_read_bw/run.sh
+++ test/benchmark/fio/ext2_seq_read_bw/run.sh
@@ -9,4 +9,4 @@ echo "*** Running the FIO sequential read test (Ext2) ***"
 /benchmark/bin/fio -rw=read -filename=/ext2/fio-test -name=seqread \
 -size=1G -bs=1M \
 -ioengine=sync -direct=1 -numjobs=1 -fsync_on_close=1 \
--time_based=1 -runtime=60
+-time_based=1 -ramp_time=60 -runtime=100
