28edc57dd02036b224882d13a922a5db97ee3bb9
Ruihan Li <lrh2000@pku.edu.cn>
2024-12-01 23:11:49 +0800

    Fix the nested bottom half

--- ostd/src/trap/handler.rs
+++ ostd/src/trap/handler.rs
@@ -34,35 +34,53 @@ fn process_top_half(trap_frame: &TrapFrame, irq_number: usize) {
 }
 
 fn process_bottom_half() {
+    let Some(handler) = BOTTOM_HALF_HANDLER.get() else {
+        return;
+    };
+
     // We need to disable preemption when processing bottom half since
     // the interrupt is enabled in this context.
-    let _preempt_guard = disable_preempt();
+    // This needs to be done before enabling the local interrupts to
+    // avoid race conditions.
+    let preempt_guard = disable_preempt();
+    crate::arch::irq::enable_local();
 
-    if let Some(handler) = BOTTOM_HALF_HANDLER.get() {
-        handler()
-    }
+    handler();
+
+    crate::arch::irq::disable_local();
+    drop(preempt_guard);
 }
 
 pub(crate) fn call_irq_callback_functions(trap_frame: &TrapFrame, irq_number: usize) {
-    // For x86 CPUs, interrupts are not re-entrant. Local interrupts will be disabled when
-    // an interrupt handler is called (Unless interrupts are re-enabled in an interrupt handler).
+    // We do not provide support for reentrant interrupt handlers. Otherwise, it's very hard to
+    // guarantee the absence of stack overflows.
     //
-    // FIXME: For arch that supports re-entrant interrupts, we may need to record nested level here.
-    IN_INTERRUPT_CONTEXT.store(true);
+    // As a concrete example, Linux once supported them in its early days, but has dropped support
+    // for this very reason. See
+    // <https://github.com/torvalds/linux/commit/d8bf368d0631d4bc2612d8bf2e4e8e74e620d0cc>.
+    //
+    // Nevertheless, we still need a counter to track the nested level because interrupts are
+    // enabled while the bottom half is being processing. The counter cannot exceed two because the
+    // bottom half cannot be reentrant for the same reason.
+    INTERRUPT_NESTED_LEVEL.add_assign(1);
 
     process_top_half(trap_frame, irq_number);
     crate::arch::interrupts_ack(irq_number);
-    crate::arch::irq::enable_local();
-    process_bottom_half();
 
-    IN_INTERRUPT_CONTEXT.store(false);
+    if INTERRUPT_NESTED_LEVEL.load() == 1 {
+        process_bottom_half();
+    }
+
+    INTERRUPT_NESTED_LEVEL.sub_assign(1);
 }
 
 cpu_local_cell! {
-    static IN_INTERRUPT_CONTEXT: bool = false;
+    static INTERRUPT_NESTED_LEVEL: u8 = 0;
 }
 
 /// Returns whether we are in the interrupt context.
+///
+/// Note that both the top half and the bottom half is processed in the interrupt context.
 pub fn in_interrupt_context() -> bool {
-    IN_INTERRUPT_CONTEXT.load()
+    INTERRUPT_NESTED_LEVEL.load() != 0
 }
