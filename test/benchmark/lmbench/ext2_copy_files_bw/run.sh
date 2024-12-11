9f6eb1e7efa6ecf20d7f5bd2bcfdb1fd3130e716
Yuke Peng <pykfirst@gmail.com>
2024-12-11 09:50:41 +0800

    Make `ext2_copy_files_bw` script consistent with description

--- test/benchmark/lmbench/ext2_copy_files_bw/run.sh
+++ test/benchmark/lmbench/ext2_copy_files_bw/run.sh
@@ -6,4 +6,5 @@ set -e
 
 echo "*** Running the LMbench lmdd test ***"
 
-echo -n "lmdd result: " & /benchmark/bin/lmbench/lmdd if=/dev/zero of=/ext2/test_file bs=1M count=512
+dd if=/dev/zero of=/ext2/zero_file bs=1M count=512
+echo -n "lmdd result: " & /benchmark/bin/lmbench/lmdd if=/ext2/zero_file of=/ext2/test_file
