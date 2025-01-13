bd84a0f39a9bc9a7a50bb885b52ab67ec17fcdf9
_Walnutz_ <2124934587@qq.com>
2025-01-13 18:59:11 +0800

    init

---
+++ kernel/comps/virtio/src/device/crypto/device.rs
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MPL-2.0
+
+use alloc::{boxed::Box, collections::linked_list::LinkedList};
+use log::debug;
+use ostd::sync::SpinLock;
+use crate::{
+    device::{crypto::config::CryptoFeatures, VirtioDeviceError},
+    queue::VirtQueue,
+    transport::{ConfigManager, VirtioTransport},
+};
+
+use super::config::VirtioCryptoConfig;
+
+pub struct CryptoDevice{
+    config_manager: ConfigManager<VirtioCryptoConfig>,
+    data_queues: SpinLock<LinkedList<VirtQueue>>,
+    control_queue: SpinLock<VirtQueue>,
+}
+
+impl CryptoDevice {
+    pub fn negotiate_features(features: u64) -> u64 {
+        features
+    }
+
+    pub fn init(mut transport: Box<dyn VirtioTransport>) -> Result<(), VirtioDeviceError> {
+        let config_manager = VirtioCryptoConfig::new_manager(transport.as_ref());
+        ostd::early_println!("virtio_crypto_config = {:?}", config_manager.read_config());
+        transport.finish_init();
+        
+        drop(transport);
+        Ok(())
+    }
+}
---
+++ kernel/comps/virtio/src/device/crypto/mod.rs
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MPL-2.0
+
+pub mod device;
+pub mod config;
+
+pub static DEVICE_NAME: &str = "Virtio-Crypto";
