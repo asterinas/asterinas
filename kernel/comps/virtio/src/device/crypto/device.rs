6ad00543927b72a753a8ddbdf1b2e6b540b4e92b
_Walnutz_ <2124934587@qq.com>
2025-01-13 20:49:53 +0800

    init

--- kernel/comps/virtio/src/device/crypto/device.rs
+++ kernel/comps/virtio/src/device/crypto/device.rs
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use alloc::{boxed::Box, collections::linked_list::LinkedList, string::ToString, sync::Arc, fmt::Debug};
+use alloc::{boxed::Box, string::ToString, sync::Arc, fmt::Debug};
 use aster_crypto::AnyCryptoDevice;
-use log::debug;
+use log::{debug, warn};
 use ostd::sync::SpinLock;
 use crate::{
-    device::VirtioDeviceError,
+    device::{crypto::config::CryptoFeatures, VirtioDeviceError},
     queue::VirtQueue,
     transport::{ConfigManager, VirtioTransport},
 };
@@ -15,7 +15,7 @@ use super::config::VirtioCryptoConfig;
 pub struct CryptoDevice{
     transport: SpinLock<Box<dyn VirtioTransport>>,
     config_manager: ConfigManager<VirtioCryptoConfig>,
-    data_queues: SpinLock<LinkedList<VirtQueue>>,
+    data_queue: SpinLock<VirtQueue>,
     control_queue: SpinLock<VirtQueue>,
 }
 
@@ -24,8 +24,20 @@ impl AnyCryptoDevice for CryptoDevice{
 }
 
 impl CryptoDevice {
-    pub fn negotiate_features(features: u64) -> u64 {
-        features
+    pub fn negotiate_features(device_features: u64) -> u64 {
+        let device_features = CryptoFeatures::from_bits_truncate(device_features);
+        let supported_features = CryptoFeatures::support_features();
+        let crypto_features = device_features & supported_features;
+
+        if crypto_features != device_features {
+            warn!(
+                "Virtio crypto contains unsupported device features: {:?}",
+                device_features.difference(supported_features)
+            );
+        }
+
+        debug!("{:?}", crypto_features);
+        crypto_features.bits()
     }
 
     pub fn init(mut transport: Box<dyn VirtioTransport>) -> Result<(), VirtioDeviceError> {
@@ -33,16 +45,18 @@ impl CryptoDevice {
         let config = config_manager.read_config();
         debug!("virtio_crypto_config = {:?}", config);
 
-        let ctrl_queue_idx = (config.max_dataqueues - 1) as u16;
-        let data_queues: SpinLock<LinkedList<VirtQueue>> = SpinLock::new(LinkedList::new());
+        let max_queue_num = config.max_dataqueues as u16;
+        let data_queue : SpinLock<VirtQueue>  = 
+            SpinLock::new(VirtQueue::new(0, max_queue_num, transport.as_mut()).unwrap());
+            
 
         let control_queue: SpinLock<VirtQueue>  = 
-            SpinLock::new(VirtQueue::new(ctrl_queue_idx, 2, transport.as_mut()).unwrap());
+            SpinLock::new(VirtQueue::new(max_queue_num, 1, transport.as_mut()).unwrap());
         
         let device = Arc::new(Self{
             config_manager,
             control_queue,
-            data_queues,
+            data_queue,
             transport: SpinLock::new(transport)
         });
         
@@ -50,7 +64,7 @@ impl CryptoDevice {
         transport.finish_init();
         drop(transport);
 
-    aster_crypto::register_device(super::DEVICE_NAME.to_string(), device);
+        aster_crypto::register_device(super::DEVICE_NAME.to_string(), device);
 
         Ok(())
     }
@@ -61,7 +75,7 @@ impl Debug for CryptoDevice {
         f.debug_struct("CryptoDevice")
             .field("config", &self.config_manager.read_config())
             .field("transport", &self.transport)
-            .field("data_queues", &self.data_queues)
+            .field("data_queue", &self.data_queue)
             .field("control_queue", &self.control_queue)
             .finish()
     }
