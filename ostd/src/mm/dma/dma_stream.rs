8c767bedce733f846acb27dc36d5ddc08381d8d3
Fabing Li <lifabing.lfb@antgroup.com>
2025-01-15 08:07:47 +0000

    Fix clone implementation for DmaStreamSlice<Dma>

--- ostd/src/mm/dma/dma_stream.rs
+++ ostd/src/mm/dma/dma_stream.rs
@@ -354,7 +354,7 @@ impl<Dma: AsRef<DmaStream>> HasPaddr for DmaStreamSlice<Dma> {
     }
 }
 
-impl Clone for DmaStreamSlice<DmaStream> {
+impl<Dma: AsRef<DmaStream> + Clone> Clone for DmaStreamSlice<Dma> {
     fn clone(&self) -> Self {
         Self {
             stream: self.stream.clone(),
