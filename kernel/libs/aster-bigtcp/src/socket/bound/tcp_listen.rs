41b6222cb7c01a9b46f71f83895dfbf925cd5cdb
jiangjianfeng <jiangjianfeng.jjf@antgroup.com>
2025-02-20 07:00:53 +0000

    Move network polling code to bottom half

--- kernel/libs/aster-bigtcp/src/socket/bound/tcp_listen.rs
+++ kernel/libs/aster-bigtcp/src/socket/bound/tcp_listen.rs
@@ -2,7 +2,8 @@
 
 use alloc::{boxed::Box, collections::btree_map::BTreeMap, sync::Arc, vec::Vec};
 
-use ostd::sync::{LocalIrqDisabled, SpinLock};
+use aster_softirq::BottomHalfDisabled;
+use ostd::sync::SpinLock;
 use smoltcp::{
     iface::Context,
     socket::PollAt,
@@ -37,7 +38,7 @@ pub struct TcpBacklog<E: Ext> {
 
 /// States needed by [`TcpListenerBg`].
 pub struct TcpListenerInner<E: Ext> {
-    pub(super) backlog: SpinLock<TcpBacklog<E>, LocalIrqDisabled>,
+    pub(super) backlog: SpinLock<TcpBacklog<E>, BottomHalfDisabled>,
     listener_key: ListenerKey,
 }
 
