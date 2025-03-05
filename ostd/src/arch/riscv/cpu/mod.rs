847bc6af4f58104b36c7803194e1be7838f27095
Zhang Junyang <junyang@stu.pku.edu.cn>
2025-03-05 11:31:56 +0800

    Halt the idle CPUs

--- ostd/src/arch/riscv/cpu/mod.rs
+++ ostd/src/arch/riscv/cpu/mod.rs
@@ -4,3 +4,17 @@
 
 pub mod context;
 pub mod local;
+
+/// Halts the CPU.
+///
+/// This function halts the CPU until the next interrupt is received. By
+/// halting, the CPU might consume less power. Internally it is implemented
+/// using the `wfi` instruction.
+///
+/// Since the function sleeps the CPU, it should not be used within an atomic
+/// mode ([`crate::task::atomic_mode`]).
+#[track_caller]
+pub fn sleep_for_interrupt() {
+    crate::task::atomic_mode::might_sleep();
+    riscv::asm::wfi();
+}
