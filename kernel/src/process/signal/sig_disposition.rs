7e96810e1ab8514acde21de92ee7c588a4b96b12
jiangjianfeng <jiangjianfeng.jjf@antgroup.com>
2025-03-14 06:52:23 +0000

    Remove trampoline code in handling user kernel

--- kernel/src/process/signal/sig_disposition.rs
+++ kernel/src/process/signal/sig_disposition.rs
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MPL-2.0
 
 use super::{constants::*, sig_action::SigAction, sig_num::SigNum};
+use crate::{prelude::*, process::signal::sig_action::SigActionFlags};
 
 #[derive(Copy, Clone)]
 pub struct SigDispositions {
@@ -26,9 +27,10 @@ impl SigDispositions {
         self.map[idx]
     }
 
-    pub fn set(&mut self, num: SigNum, sa: SigAction) -> SigAction {
+    pub fn set(&mut self, num: SigNum, sa: SigAction) -> Result<SigAction> {
+        check_sigaction(&sa)?;
         let idx = Self::num_to_idx(num);
-        core::mem::replace(&mut self.map[idx], sa)
+        Ok(core::mem::replace(&mut self.map[idx], sa))
     }
 
     pub fn set_default(&mut self, num: SigNum) {
@@ -50,5 +52,38 @@ impl SigDispositions {
 
     fn num_to_idx(num: SigNum) -> usize {
         (num.as_u8() - MIN_STD_SIG_NUM) as usize
+    }
+}
+
+fn check_sigaction(sig_action: &SigAction) -> Result<()> {
+    // Here we only check if the SA_RESTORER flag is set and restorer_addr is not 0.
+    // Note: Linux checks the SA_RESTORER flag when initializing the signal stack,
+    // whereas we have moved this check forward to prevent this action from being set.
+    // This may result in some differences from the behavior of Linux.
+
+    let SigAction::User {
+        flags,
+        restorer_addr,
+        ..
+    } = sig_action
+    else {
+        return Ok(());
+    };
+
+    if flags.contains(SigActionFlags::SA_RESTORER) && *restorer_addr != 0 {
+        return Ok(());
+    }
+
+    cfg_if::cfg_if! {
+        if #[cfg(target_arch = "x86_64")] {
+            // On x86-64, `SA_RESTORER` is mandatory and cannot be omitted.
+            // Ref: <https://elixir.bootlin.com/linux/v6.13/source/arch/x86/kernel/signal_64.c#L172>
+            return_errno_with_message!(Errno::EINVAL, "x86-64 should always use SA_RESTORER");
+        } else {
+            // FIXME: The support for user-provided signal handlers
+            // without `SA_RESTORER` is arch-dependent.
+            // Other archs may need to handle scenarios where `SA_RESTORER` is omitted.
+            return_errno_with_message!(Errno::EINVAL, "TODO: properly deal with SA_RESTORER");
+        }
     }
 }
