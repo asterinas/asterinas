e5be154ca8178445029cb9f9dd890412725d6a7d
Zhang Junyang <junyang@stu.pku.edu.cn>
2025-03-20 19:27:06 +0800

    Make per-cpu counter a macro

--- osdk/deps/frame-allocator/src/lib.rs
+++ osdk/deps/frame-allocator/src/lib.rs
@@ -31,22 +31,28 @@ extern crate alloc;
 use core::alloc::Layout;
 
 use ostd::{
+    cpu::PinCurrentCpu,
     mm::{frame::GlobalFrameAllocator, Paddr},
     trap,
 };
 
 mod cache;
 mod chunk;
-mod per_cpu_counter;
 mod pools;
 mod set;
+mod smp_counter;
 
 #[cfg(ktest)]
 mod test;
 
+fast_smp_counter! {
+    /// The total size of free memory.
+    pub static TOTAL_FREE_SIZE: usize;
+}
+
 /// Loads the total size (in bytes) of free memory in the allocator.
 pub fn load_total_free_size() -> usize {
-    per_cpu_counter::read_total_free_size()
+    TOTAL_FREE_SIZE.get()
 }
 
 /// The global frame allocator provided by OSDK.
@@ -61,20 +67,20 @@ impl GlobalFrameAllocator for FrameAllocator {
         let guard = trap::disable_local();
         let res = cache::alloc(&guard, layout);
         if res.is_some() {
-            per_cpu_counter::sub_free_size(&guard, layout.size());
+            TOTAL_FREE_SIZE.sub(guard.current_cpu(), layout.size());
         }
         res
     }
 
     fn dealloc(&self, addr: Paddr, size: usize) {
         let guard = trap::disable_local();
-        per_cpu_counter::add_free_size(&guard, size);
+        TOTAL_FREE_SIZE.add(guard.current_cpu(), size);
         cache::dealloc(&guard, addr, size);
     }
 
     fn add_free_memory(&self, addr: Paddr, size: usize) {
         let guard = trap::disable_local();
-        per_cpu_counter::add_free_size(&guard, size);
+        TOTAL_FREE_SIZE.add(guard.current_cpu(), size);
         pools::add_free_memory(&guard, addr, size);
     }
 }
