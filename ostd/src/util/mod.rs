6950ef2f93bee0460897a99a84c718f6709b9c0c
Ruihan Li <lrh2000@pku.edu.cn>
2025-04-11 10:11:08 +0800

    Add support for `Rcu` to store an `Either`

--- ostd/src/util/mod.rs
+++ ostd/src/util/mod.rs
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MPL-2.0
 
+//! Utility types and methods.
+
+mod either;
 mod macros;
-pub mod marker;
-pub mod ops;
-pub mod range_alloc;
+pub(crate) mod marker;
+pub(crate) mod ops;
+pub(crate) mod range_alloc;
+
+pub use either::Either;
