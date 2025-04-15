d6e40933b8b8cfe91a6b529631b79dbeb53ac99c
Ruihan Li <lrh2000@pku.edu.cn>
2025-04-15 22:22:07 +0800

    Move the log lock to a better location

--- ostd/src/console.rs
+++ ostd/src/console.rs
@@ -2,16 +2,34 @@
 
 //! Console output.
 
-use core::fmt::Arguments;
+use core::fmt::{self, Arguments, Write};
 
-use crate::if_tdx_enabled;
+use crate::{
+    if_tdx_enabled,
+    sync::{LocalIrqDisabled, SpinLock},
+};
+
+struct Stdout;
+
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for &c in s.as_bytes() {
+            crate::arch::serial::send(c);
+        }
+        Ok(())
+    }
+}
+
+static STDOUT: SpinLock<Stdout, LocalIrqDisabled> = SpinLock::new(Stdout);
 
 /// Prints formatted arguments to the console.
 pub fn early_print(args: Arguments) {
     if_tdx_enabled!({
+        // Hold the lock to prevent the logs from interleaving.
+        let _guard = STDOUT.lock();
         tdx_guest::print(args);
     } else {
-        crate::arch::serial::print(args);
+        STDOUT.lock().write_fmt(args).unwrap();
     });
 }
 
