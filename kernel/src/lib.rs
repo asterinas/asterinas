9a6622ef8a057725f5c2e8b5966e68ecb3f5d022
Ruihan Li <lrh2000@pku.edu.cn>
2025-04-16 21:01:40 +0800

    Use simpler APIs to simplify the code

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -36,7 +36,7 @@ use kcmdline::KCmdlineArg;
 use ostd::{
     arch::qemu::{exit_qemu, QemuExitCode},
     boot::boot_info,
-    cpu::{CpuId, CpuSet, PinCurrentCpu},
+    cpu::{CpuId, CpuSet},
 };
 use process::Process;
 use sched::SchedPolicy;
@@ -107,22 +107,20 @@ pub fn init() {
 
 fn ap_init() {
     fn ap_idle_thread() {
-        let preempt_guard = ostd::task::disable_preempt();
-        let cpu_id = preempt_guard.current_cpu();
-        drop(preempt_guard);
-        log::info!("Kernel idle thread for CPU #{} started.", cpu_id.as_usize());
+        log::info!(
+            "Kernel idle thread for CPU #{} started.",
+            // No races because `ap_idle_thread` runs on a certain AP.
+            ostd::cpu::current_cpu_racy().as_usize(),
+        );
 
         loop {
             ostd::task::halt_for_preempt();
         }
     }
 
-    let preempt_guard = ostd::task::disable_preempt();
-    let cpu_id = preempt_guard.current_cpu();
-    drop(preempt_guard);
-
     ThreadOptions::new(ap_idle_thread)
-        .cpu_affinity(cpu_id.into())
+        // No races because `ap_init` runs on a certain AP.
+        .cpu_affinity(ostd::cpu::current_cpu_racy().into())
         .sched_policy(SchedPolicy::Idle)
         .spawn();
 }
