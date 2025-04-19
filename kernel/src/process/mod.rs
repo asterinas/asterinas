42d63e7b9d7a50c9233604cb43cd1934cb331765
Ruihan Li <lrh2000@pku.edu.cn>
2025-04-19 18:24:08 +0800

    Add `enqueue_signal_async`

--- kernel/src/process/mod.rs
+++ kernel/src/process/mod.rs
@@ -23,8 +23,8 @@ pub use clone::{clone_child, CloneArgs, CloneFlags};
 pub use credentials::{Credentials, Gid, Uid};
 pub use kill::{kill, kill_all, kill_group, tgkill};
 pub use process::{
-    spawn_init_process, ExitCode, JobControl, Pgid, Pid, Process, ProcessGroup, Session, Sid,
-    Terminal,
+    enqueue_signal_async, spawn_init_process, ExitCode, JobControl, Pgid, Pid, Process,
+    ProcessGroup, Session, Sid, Terminal,
 };
 pub use process_filter::ProcessFilter;
 pub use process_vm::{
