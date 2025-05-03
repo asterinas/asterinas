038faa3b28b73321d2f90b340c9bd3f267883cf6
Ruihan Li <lrh2000@pku.edu.cn>
2025-05-03 22:58:25 +0800

    Use one lock for `LineDiscipline`

--- kernel/src/device/tty/mod.rs
+++ kernel/src/device/tty/mod.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use ostd::early_print;
+use ostd::{early_print, sync::LocalIrqDisabled};
 use spin::Once;
 
 use self::{driver::TtyDriver, line_discipline::LineDiscipline};
@@ -15,7 +15,7 @@ use crate::{
     prelude::*,
     process::{
         broadcast_signal_async,
-        signal::{PollHandle, Pollable},
+        signal::{PollHandle, Pollable, Pollee},
         JobControl, Terminal,
     },
 };
@@ -36,13 +36,16 @@ pub(super) fn init() {
     driver::init();
 }
 
+const IO_CAPACITY: usize = 4096;
+
 pub struct Tty {
     /// tty_name
     #[expect(unused)]
     name: CString,
     /// line discipline
-    ldisc: LineDiscipline,
+    ldisc: SpinLock<LineDiscipline, LocalIrqDisabled>,
     job_control: JobControl,
+    pollee: Pollee,
     /// driver
     driver: SpinLock<Weak<TtyDriver>>,
     weak_self: Weak<Self>,
@@ -52,8 +55,9 @@ impl Tty {
     pub fn new(name: CString) -> Arc<Self> {
         Arc::new_cyclic(move |weak_ref| Tty {
             name,
-            ldisc: LineDiscipline::new(),
+            ldisc: SpinLock::new(LineDiscipline::new()),
             job_control: JobControl::new(),
+            pollee: Pollee::new(),
             driver: SpinLock::new(Weak::new()),
             weak_self: weak_ref.clone(),
         })
@@ -66,7 +70,7 @@ impl Tty {
     pub fn push_char(&self, ch: u8) {
         // FIXME: Use `early_print` to avoid calling virtio-console.
         // This is only a workaround
-        self.ldisc.push_char(
+        self.ldisc.lock().push_char(
             ch,
             |signum| {
                 if let Some(foreground) = self.job_control.foreground() {
@@ -74,69 +78,90 @@ impl Tty {
                 }
             },
             |content| early_print!("{}", content),
-        )
+        );
+        self.pollee.notify(IoEvents::IN);
+    }
+
+    fn check_io_events(&self) -> IoEvents {
+        if self.ldisc.lock().buffer_len() != 0 {
+            IoEvents::IN | IoEvents::OUT
+        } else {
+            IoEvents::OUT
+        }
     }
 }
 
 impl Pollable for Tty {
     fn poll(&self, mask: IoEvents, poller: Option<&mut PollHandle>) -> IoEvents {
-        self.ldisc.poll(mask, poller)
+        self.pollee
+            .poll_with(mask, poller, || self.check_io_events())
     }
 }
 
 impl FileIo for Tty {
     fn read(&self, writer: &mut VmWriter) -> Result<usize> {
-        let mut buf = vec![0; writer.avail()];
         self.job_control.wait_until_in_foreground()?;
-        let read_len = self.ldisc.read(buf.as_mut_slice())?;
-        writer.write_fallible(&mut buf.as_slice().into())?;
+
+        // TODO: Add support for non-blocking mode and timeout
+        let mut buf = vec![0u8; writer.avail().min(IO_CAPACITY)];
+        let read_len =
+            self.wait_events(IoEvents::IN, None, || self.ldisc.lock().try_read(&mut buf))?;
+        self.pollee.invalidate();
+
+        // TODO: Confirm what we should do if `write_fallible` fails in the middle.
+        writer.write_fallible(&mut buf[..read_len].into())?;
         Ok(read_len)
     }
 
     fn write(&self, reader: &mut VmReader) -> Result<usize> {
-        let buf = reader.collect()?;
-        if let Ok(content) = alloc::str::from_utf8(&buf) {
+        let mut buf = vec![0u8; reader.remain().min(IO_CAPACITY)];
+        let write_len = reader.read_fallible(&mut buf.as_mut_slice().into())?;
+
+        if let Ok(content) = alloc::str::from_utf8(&buf[..write_len]) {
             print!("{content}");
         } else {
             println!("Not utf-8 content: {:?}", buf);
         }
-        Ok(buf.len())
+        Ok(write_len)
     }
 
     fn ioctl(&self, cmd: IoctlCmd, arg: usize) -> Result<i32> {
         match cmd {
             IoctlCmd::TCGETS => {
-                // Get terminal attributes
-                let termios = self.ldisc.termios();
-                trace!("get termios = {:?}", termios);
+                let termios = *self.ldisc.lock().termios();
+
                 current_userspace!().write_val(arg, &termios)?;
             }
             IoctlCmd::TCSETS => {
-                // Set terminal attributes
                 let termios = current_userspace!().read_val(arg)?;
-                debug!("set termios = {:?}", termios);
-                self.ldisc.set_termios(termios);
+
+                self.ldisc.lock().set_termios(termios);
             }
             IoctlCmd::TCSETSW => {
                 let termios = current_userspace!().read_val(arg)?;
-                debug!("set termios = {:?}", termios);
-                self.ldisc.set_termios(termios);
-                // TODO: drain output buffer
+
+                self.ldisc.lock().set_termios(termios);
+                // TODO: Drain the output buffer
             }
             IoctlCmd::TCSETSF => {
                 let termios = current_userspace!().read_val(arg)?;
-                debug!("set termios = {:?}", termios);
-                self.ldisc.set_termios(termios);
-                self.ldisc.drain_input();
-                // TODO: drain output buffer
+
+                let mut ldisc = self.ldisc.lock();
+                ldisc.set_termios(termios);
+                ldisc.drain_input();
+                // TODO: Drain the output buffer
+
+                self.pollee.invalidate();
             }
             IoctlCmd::TIOCGWINSZ => {
-                let winsize = self.ldisc.window_size();
+                let winsize = self.ldisc.lock().window_size();
+
                 current_userspace!().write_val(arg, &winsize)?;
             }
             IoctlCmd::TIOCSWINSZ => {
                 let winsize = current_userspace!().read_val(arg)?;
-                self.ldisc.set_window_size(winsize);
+
+                self.ldisc.lock().set_window_size(winsize);
             }
             _ => (self.weak_self.upgrade().unwrap() as Arc<dyn Terminal>)
                 .job_ioctl(cmd, arg, false)?,
