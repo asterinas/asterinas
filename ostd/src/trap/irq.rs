ef811009580260ae71e1fca462cea8c1f06876d2
Zhang Junyang <junyang@stu.pku.edu.cn>
2025-05-22 20:55:57 +0800

    Add `InAtomicMode: Debug`

--- ostd/src/trap/irq.rs
+++ ostd/src/trap/irq.rs
@@ -139,6 +139,7 @@ pub fn disable_local() -> DisabledLocalIrqGuard {
 /// A guard for disabled local IRQs.
 #[clippy::has_significant_drop]
 #[must_use]
+#[derive(Debug)]
 pub struct DisabledLocalIrqGuard {
     was_enabled: bool,
 }
