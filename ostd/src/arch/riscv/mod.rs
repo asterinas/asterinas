11d6920232ee813c68b631d9023e739acb4a1f3b
Zejun Zhao <jelly.zhao.42@gmail.com>
2025-05-28 15:35:00 +0800

    Add RISC-V PLIC support

--- ostd/src/arch/riscv/mod.rs
+++ ostd/src/arch/riscv/mod.rs
@@ -9,7 +9,7 @@ pub(crate) mod cpu;
 pub mod device;
 mod io;
 pub(crate) mod iommu;
-pub(crate) mod irq;
+pub mod irq;
 pub(crate) mod mm;
 pub(crate) mod pci;
 pub mod qemu;
@@ -20,7 +20,10 @@ pub mod trap;
 
 use core::sync::atomic::Ordering;
 
-use crate::arch::timer::TIMER_IRQ_NUM;
+use crate::{
+    arch::{irq::IRQ_CHIP, timer::TIMER_IRQ_NUM},
+    cpu::CpuId,
+};
 
 #[cfg(feature = "cvm_guest")]
 pub(crate) fn init_cvm_guest() {
@@ -40,6 +43,12 @@ pub(crate) unsafe fn late_init_on_bsp() {
     unsafe { trap::init() };
 
     let io_mem_builder = io::construct_io_mem_allocator_builder();
+
+    // SAFETY:
+    // 1. This function is called once and only once in the boot context.
+    // 2. No other functions from the `irq` module have been called before this.
+    // 3. The kernel page table is already activated.
+    unsafe { irq::init(&io_mem_builder) };
 
     // SAFETY:
     // 1. The caller ensures that the function is only called once in the
@@ -72,7 +81,11 @@ pub(crate) fn interrupts_ack(irq_number: usize) {
         return;
     }
 
-    unimplemented!()
+    IRQ_CHIP
+        .get()
+        .unwrap()
+        .lock()
+        .complete_interrupt(CpuId::current_racy().as_usize() as u32, irq_number as u32);
 }
 
 /// Return the frequency of TSC. The unit is Hz.
