baf595eeb98c421b5e85e8fac286aedee2178841
Qingsong Chen <changxian.cqs@antgroup.com>
2025-06-11 08:40:04 +0000

    Install nix packages in docker image

---
+++ test/nix/lmbench.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import <nixpkgs> { }, }:
+with pkgs;
+stdenv.mkDerivation {
+  pname = "lmbench";
+  version = "0.1.0";
+  src = fetchFromGitHub {
+    owner = "asterinas";
+    repo = "lmbench";
+    rev = "25a43f544af396b81c7a378c83d33f2cbab10fcc";
+    hash = "sha256-HGhBNuR5rrSAsk6c2bD0YuVV+5w7itCNVVxFRD522Rw";
+  };
+
+  dontPatchShebangs = true;
+  makeFlags = [ "CC=${stdenv.cc.targetPrefix}cc" ];
+  patchPhase = ''
+    substituteInPlace src/Makefile \
+      --replace-fail "/bin/rm" "rm" \
+      --replace-fail "AR=ar" ""
+  '';
+  buildInputs = [ libtirpc ];
+  preBuild = ''
+    makeFlagsArray+=(CPPFLAGS="-std=gnu89 -I${libtirpc.dev}/include/tirpc -Wno-error=format-security")
+  '';
+  installPhase = ''
+    mkdir -p $out/bin
+    mv bin/x86_64-linux-gnu/* $out/bin/
+  '';
+}
