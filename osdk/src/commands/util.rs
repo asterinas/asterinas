ea877b92c91ee7716c309178df1a0fbafc210567
Zhang Junyang <junyang@stu.pku.edu.cn>
2025-06-13 11:19:00 +0800

    Make OSDK errors clear if commands don't exist

--- osdk/src/commands/util.rs
+++ osdk/src/commands/util.rs
@@ -2,7 +2,7 @@
 
 use std::process::Command;
 
-use crate::util::get_kernel_crate;
+use crate::util::{get_kernel_crate, new_command_checked_exists};
 
 pub const COMMON_CARGO_ARGS: &[&str] = &[
     "-Zbuild-std=core,alloc,compiler_builtins",
@@ -12,7 +12,7 @@ pub const COMMON_CARGO_ARGS: &[&str] = &[
 pub const DEFAULT_TARGET_RELPATH: &str = "osdk";
 
 pub fn cargo() -> Command {
-    Command::new("cargo")
+    new_command_checked_exists("cargo")
 }
 
 pub fn profile_name_adapter(profile: &str) -> &str {
