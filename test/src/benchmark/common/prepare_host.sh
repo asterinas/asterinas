7fad653992e7d9cbbe4c572f7ba339a779ef8025
Hsy-Intel <siyuan.hui@intel.com>
2025-07-07 22:19:39 +0800

    Use 6.16.0 Linux kernel for benchmark tests & Add Linux TDX benchmark tests

--- test/src/benchmark/common/prepare_host.sh
+++ test/src/benchmark/common/prepare_host.sh
@@ -13,18 +13,17 @@ ASTER_OUTPUT="${BENCHMARK_ROOT}/aster_output.txt"
 # Dependencies for Linux
 LINUX_DEPENDENCIES_DIR="/opt/linux_binary_cache"
 LINUX_KERNEL="${LINUX_DEPENDENCIES_DIR}/vmlinuz"
-LINUX_KERNEL_VERSION="5.15.0-105"
+LINUX_KERNEL_VERSION="6.16.0"
 LINUX_MODULES_DIR="${BENCHMARK_ROOT}/../build/initramfs/lib/modules/${LINUX_KERNEL_VERSION}/kernel"
 WGET_SCRIPT="${BENCHMARK_ROOT}/../../../tools/atomic_wget.sh"
 
 # Prepare Linux kernel and modules
 prepare_libs() {
-    # Download the Linux kernel and modules
     mkdir -p "${LINUX_DEPENDENCIES_DIR}"
 
     # Array of files to download and their URLs
     declare -A files=(
-        ["${LINUX_KERNEL}"]="https://raw.githubusercontent.com/asterinas/linux_binary_cache/14598b6/vmlinuz-${LINUX_KERNEL_VERSION}"
+        ["${LINUX_KERNEL}"]="https://raw.githubusercontent.com/asterinas/linux_binary_cache/24db4ff/vmlinuz-${LINUX_KERNEL_VERSION}"
     )
 
     # Download files if they don't exist
