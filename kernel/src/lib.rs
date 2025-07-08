aba02d3e55410457949e5c48075bdbd9669d8aff
王英泰 <2253457010@qq.com>
2025-07-08 18:53:18 +0800

    Finish the arch section for LoongArch in kernel

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -53,6 +53,9 @@ pub mod arch;
 #[cfg(target_arch = "riscv64")]
 #[path = "arch/riscv/mod.rs"]
 pub mod arch;
+#[cfg(target_arch = "loongarch64")]
+#[path = "arch/loongarch/mod.rs"]
+pub mod arch;
 pub mod context;
 pub mod cpu;
 pub mod device;
