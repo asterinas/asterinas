4736004cff1fcc30540040cac300824a822ffef0
Qingsong Chen <changxian.cqs@antgroup.com>
2025-07-08 06:28:58 +0000

    Fix lmbench by adding delay to ensure lmhttp starts

--- test/src/benchmark/lmbench/tcp_loopback_http_bw/run.sh
+++ test/src/benchmark/lmbench/tcp_loopback_http_bw/run.sh
@@ -8,6 +8,7 @@ echo "*** Running lmbench HTTP latency test ***"
 
 dd if=/dev/zero of=test_file bs=1M count=64
 echo "test_file" > file_list
-/benchmark/bin/lmbench/lmhttp & 
+/benchmark/bin/lmbench/lmhttp &
+sleep 1
 /benchmark/bin/lmbench/lat_http 127.0.0.1 < file_list
 /benchmark/bin/lmbench/lat_http -S 127.0.0.1
