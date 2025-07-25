bf0b1dea4d6a7c4cabc9873e39ad14d5df3a508e
Yuke Peng <pykfirst@gmail.com>
2025-07-25 15:39:06 +0800

    Disable IoPort read/write in IoBar

--- kernel/comps/pci/src/cfg_space.rs
+++ kernel/comps/pci/src/cfg_space.rs
@@ -380,13 +380,13 @@ impl IoBar {
         if self.size < size_of::<T>() as u32 || offset > self.size - size_of::<T>() as u32 {
             return Err(Error::InvalidArgs);
         }
-        // SAFETY: The range of ports accessed is within the scope managed by the IoBar and
-        // an out-of-bounds check is performed.
-        unsafe { Ok(T::read_from_port((self.base + offset) as u16)) }
+
+        // TODO: Implement the read operation based on `IoPortAllocator`.
+        Err(Error::IoError)
     }
 
     /// Writes to port
-    pub fn write<T: PortWrite>(&self, offset: u32, value: T) -> Result<()> {
+    pub fn write<T: PortWrite>(&self, offset: u32, _value: T) -> Result<()> {
         // Check alignment
         if (self.base + offset) % size_of::<T>() as u32 != 0 {
             return Err(Error::InvalidArgs);
@@ -395,10 +395,9 @@ impl IoBar {
         if size_of::<T>() as u32 > self.size || offset > self.size - size_of::<T>() as u32 {
             return Err(Error::InvalidArgs);
         }
-        // SAFETY: The range of ports accessed is within the scope managed by the IoBar and
-        // an out-of-bounds check is performed.
-        unsafe { T::write_to_port((self.base + offset) as u16, value) }
-        Ok(())
+
+        // TODO: Implement the write operation based on `IoPortAllocator`.
+        Err(Error::IoError)
     }
 
     /// Creates an I/O port BAR structure.
