89b404bce67362154fe725360dc58d11d1c7b431
jiangjianfeng <jiangjianfeng.jjf@antgroup.com>
2025-08-04 09:37:45 +0000

    Add syscall unshare

--- kernel/src/process/mod.rs
+++ kernel/src/process/mod.rs
@@ -19,6 +19,7 @@ mod status;
 pub mod sync;
 mod task_set;
 mod term_status;
+mod unshare;
 mod user_ns;
 mod wait;
 
@@ -36,6 +37,7 @@ pub use process_vm::{renew_vm_and_map, MAX_LEN_STRING_ARG, MAX_NR_STRING_ARGS};
 pub use program_loader::{check_executable_file, ProgramToLoad};
 pub use rlimit::ResourceType;
 pub use term_status::TermStatus;
+pub use unshare::ContextUnshareApi;
 pub use user_ns::UserNamespace;
 pub use wait::{do_wait, WaitOptions, WaitStatus};
 
