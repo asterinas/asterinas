bd11458ce50febafab4efc757572ac74e4b60023
Zejun Zhao <jelly.zhao.42@gmail.com>
2025-08-07 01:47:52 +0800

    Refactor RISC-V exception-related code

--- ostd/src/arch/riscv/cpu/context.rs
+++ ostd/src/arch/riscv/cpu/context.rs
@@ -4,7 +4,7 @@
 
 use core::{fmt::Debug, sync::atomic::Ordering};
 
-use riscv::register::scause::{Exception, Interrupt, Trap};
+use riscv::register::scause::Exception;
 
 use crate::{
     arch::{
@@ -13,17 +13,17 @@ use crate::{
         TIMER_IRQ_NUM,
     },
     cpu::CpuId,
+    task::scheduler,
     trap::call_irq_callback_functions,
     user::{ReturnReason, UserContextApi, UserContextApiInternal},
 };
 
 /// Userspace CPU context, including general-purpose registers and exception information.
-#[derive(Clone, Debug)]
+#[derive(Clone, Default, Debug)]
 #[repr(C)]
 pub struct UserContext {
     user_context: RawUserContext,
-    trap: Trap,
-    cpu_exception_info: Option<CpuExceptionInfo>,
+    exception: Option<CpuException>,
 }
 
 /// General-purpose registers.
@@ -64,45 +64,87 @@ pub struct GeneralRegs {
     pub t6: usize,
 }
 
-/// CPU exception information.
-//
-// TODO: Refactor the struct into an enum (similar to x86's `CpuException`).
-#[expect(missing_docs)]
+/// RISC-V CPU exceptions
+///
+/// Every enum variant corresponds to one exception defined by the RISC-V
+/// architecture. Variants that naturally carry an error code (in stval
+/// register) expose it through their associated data fields.
 #[derive(Clone, Copy, Debug)]
-#[repr(C)]
-pub struct CpuExceptionInfo {
-    /// The type of the exception.
-    pub code: Exception,
-    /// The error code associated with the exception.
-    pub page_fault_addr: usize,
-    pub error_code: usize, // TODO
+pub enum CpuException {
+    /// Instruction address misalignment exception.
+    InstructionMisaligned,
+    /// Instruction access fault exception.
+    InstructionFault,
+    /// Illegal instruction exception.
+    IllegalInstruction(Instruction),
+    /// Breakpoint exception.
+    Breakpoint,
+    /// Load address misalignment exception.
+    LoadMisaligned(FaultAddress),
+    /// Load access fault exception.
+    LoadFault(FaultAddress),
+    /// Store address misalignment exception.
+    StoreMisaligned(FaultAddress),
+    /// Store access fault exception.
+    StoreFault(FaultAddress),
+    /// Environment call from user mode.
+    UserEnvCall,
+    /// Environment call from supervisor mode.
+    SupervisorEnvCall,
+    /// Environment call from machine mode.
+    MachineEnvCall,
+    /// Instruction page fault exception.
+    InstructionPageFault(FaultAddress),
+    /// Load page fault exception.
+    LoadPageFault(FaultAddress),
+    /// Store page fault exception.
+    StorePageFault(FaultAddress),
+    /// Unknown.
+    Unknown,
 }
 
-impl Default for UserContext {
-    fn default() -> Self {
-        UserContext {
-            user_context: RawUserContext::default(),
-            trap: Trap::Exception(Exception::Unknown),
-            cpu_exception_info: None,
+impl From<Exception> for CpuException {
+    fn from(value: Exception) -> Self {
+        use Exception::*;
+
+        let stval = riscv::register::stval::read();
+        match value {
+            InstructionMisaligned => Self::InstructionMisaligned,
+            InstructionFault => Self::InstructionFault,
+            IllegalInstruction => Self::IllegalInstruction({
+                if stval & 0x3 == 0x3 {
+                    Instruction::Normal(stval as u32)
+                } else {
+                    Instruction::Compressed(stval as u16)
+                }
+            }),
+            Breakpoint => Self::Breakpoint,
+            LoadMisaligned => Self::LoadMisaligned(FaultAddress(stval)),
+            LoadFault => Self::LoadFault(FaultAddress(stval)),
+            StoreMisaligned => Self::StoreMisaligned(FaultAddress(stval)),
+            StoreFault => Self::StoreFault(FaultAddress(stval)),
+            UserEnvCall => Self::UserEnvCall,
+            SupervisorEnvCall => Self::SupervisorEnvCall,
+            InstructionPageFault => Self::InstructionPageFault(FaultAddress(stval)),
+            LoadPageFault => Self::LoadPageFault(FaultAddress(stval)),
+            StorePageFault => Self::StorePageFault(FaultAddress(stval)),
+            Unknown => Self::Unknown,
         }
     }
 }
 
-impl Default for CpuExceptionInfo {
-    fn default() -> Self {
-        CpuExceptionInfo {
-            code: Exception::Unknown,
-            page_fault_addr: 0,
-            error_code: 0,
-        }
-    }
-}
+#[derive(Clone, Copy, Debug)]
+/// Data address of data access exceptions.
+pub struct FaultAddress(pub usize);
 
-impl CpuExceptionInfo {
-    /// Get corresponding CPU exception
-    pub fn cpu_exception(&self) -> CpuException {
-        self.code
-    }
+#[derive(Clone, Copy, Debug)]
+/// Illegal instruction that caused exception.
+pub enum Instruction {
+    /// Normal 4-byte instruction.
+    Normal(u32),
+    /// Compressed 2-byte instruction. Used only when compressed (C) extension
+    /// is enabled.
+    Compressed(u16),
 }
 
 impl UserContext {
@@ -117,8 +159,8 @@ impl UserContext {
     }
 
     /// Returns the trap information.
-    pub fn take_exception(&mut self) -> Option<CpuExceptionInfo> {
-        self.cpu_exception_info.take()
+    pub fn take_exception(&mut self) -> Option<CpuException> {
+        self.exception.take()
     }
 
     /// Sets the thread-local storage pointer.
@@ -143,16 +185,23 @@ impl UserContextApiInternal for UserContext {
     where
         F: FnMut() -> bool,
     {
-        let return_reason = loop {
+        use riscv::register::scause::{Interrupt::*, Trap::*};
+
+        // Enables userspace traps.
+        self.user_context.sstatus |= 1 << 5;
+        loop {
+            scheduler::might_preempt();
             self.user_context.run();
-            match riscv::register::scause::read().cause() {
-                Trap::Interrupt(Interrupt::SupervisorTimer) => {
+
+            let scause = riscv::register::scause::read();
+            match scause.cause() {
+                Interrupt(SupervisorTimer) => {
                     call_irq_callback_functions(
                         &self.as_trap_frame(),
                         TIMER_IRQ_NUM.load(Ordering::Relaxed) as usize,
                     );
                 }
-                Trap::Interrupt(Interrupt::SupervisorExternal) => {
+                Interrupt(SupervisorExternal) => {
                     let current_cpu = CpuId::current_racy().as_usize() as u32;
                     loop {
                         let irq_chip = IRQ_CHIP.get().unwrap().lock();
@@ -168,30 +217,44 @@ impl UserContextApiInternal for UserContext {
                         }
                     }
                 }
-                Trap::Interrupt(_) => todo!(),
-                Trap::Exception(Exception::UserEnvCall) => {
-                    self.user_context.sepc += 4;
-                    break ReturnReason::UserSyscall;
+                Interrupt(SupervisorSoft) => todo!(),
+                Interrupt(Unknown) => {
+                    panic!(
+                        "Cannot handle unknown supervisor interrupt, scause: {:#x}, trapframe: {:?}.",
+                        scause.bits(),
+                        self.as_trap_frame()
+                    );
                 }
-                Trap::Exception(e) => {
-                    let stval = riscv::register::stval::read();
-                    log::trace!("Exception, scause: {e:?}, stval: {stval:#x?}");
-                    self.cpu_exception_info = Some(CpuExceptionInfo {
-                        code: e,
-                        page_fault_addr: stval,
-                        error_code: 0,
-                    });
-                    break ReturnReason::UserException;
+                Exception(e) => {
+                    use CpuException::*;
+
+                    let exception = e.into();
+                    crate::arch::irq::enable_local();
+                    match exception {
+                        UserEnvCall => {
+                            self.user_context.sepc += 4;
+                            return ReturnReason::UserSyscall;
+                        }
+                        Unknown => {
+                            panic!(
+                                "Cannot handle unknown exception, scause: {:#x}, trapframe: {:?}.",
+                                scause.bits(),
+                                self.as_trap_frame()
+                            );
+                        }
+                        _ => {
+                            self.exception = Some(exception);
+                            return ReturnReason::UserException;
+                        }
+                    }
                 }
             }
+            crate::arch::irq::enable_local();
 
             if has_kernel_event() {
                 break ReturnReason::KernelEvent;
             }
-        };
-
-        crate::arch::irq::enable_local();
-        return_reason
+        }
     }
 
     fn as_trap_frame(&self) -> TrapFrame {
@@ -282,9 +345,6 @@ cpu_context_impl_getter_setter!(
     [t5, set_t5],
     [t6, set_t6]
 );
-
-/// CPU exception.
-pub type CpuException = Exception;
 
 /// The FPU context of user task.
 ///
--- ostd/src/arch/riscv/trap/mod.rs
+++ ostd/src/arch/riscv/trap/mod.rs
@@ -7,13 +7,16 @@ mod trap;
 
 use core::sync::atomic::Ordering;
 
-use riscv::register::scause::Interrupt;
 use spin::Once;
 pub(super) use trap::RawUserContext;
 pub use trap::TrapFrame;
 
-use super::{cpu::context::CpuExceptionInfo, timer::TIMER_IRQ_NUM};
-use crate::{arch::irq::IRQ_CHIP, cpu::CpuId, cpu_local_cell, trap::call_irq_callback_functions};
+use super::{
+    cpu::context::CpuException,
+    irq::{disable_local, enable_local, IRQ_CHIP},
+    timer::TIMER_IRQ_NUM,
+};
+use crate::{cpu::CpuId, cpu_local_cell, trap::call_irq_callback_functions};
 
 cpu_local_cell! {
     static IS_KERNEL_INTERRUPTED: bool = false;
@@ -36,16 +39,31 @@ pub fn is_kernel_interrupted() -> bool {
 /// Handle traps (only from kernel).
 #[no_mangle]
 extern "C" fn trap_handler(f: &mut TrapFrame) {
-    use riscv::register::scause::Trap;
+    fn enable_local_if(cond: bool) {
+        if cond {
+            enable_local();
+        }
+    }
 
-    match riscv::register::scause::read().cause() {
-        Trap::Interrupt(interrupt) => {
+    fn disable_local_if(cond: bool) {
+        if cond {
+            disable_local();
+        }
+    }
+
+    use riscv::register::scause::Trap::*;
+
+    let scause = riscv::register::scause::read();
+    match scause.cause() {
+        Interrupt(interrupt) => {
+            use riscv::register::scause::Interrupt::*;
+
             IS_KERNEL_INTERRUPTED.store(true);
             match interrupt {
-                Interrupt::SupervisorTimer => {
+                SupervisorTimer => {
                     call_irq_callback_functions(f, TIMER_IRQ_NUM.load(Ordering::Relaxed) as usize);
                 }
-                Interrupt::SupervisorExternal => {
+                SupervisorExternal => {
                     let current_cpu = CpuId::current_racy().as_usize() as u32;
                     loop {
                         let irq_chip = IRQ_CHIP.get().unwrap().lock();
@@ -58,32 +76,52 @@ extern "C" fn trap_handler(f: &mut TrapFrame) {
                         }
                     }
                 }
-                Interrupt::SupervisorSoft => todo!(),
-                _ => {
+                SupervisorSoft => todo!(),
+                Unknown => {
                     panic!(
-                        "cannot handle unknown supervisor interrupt: {interrupt:?}. trapframe: {f:#x?}.",
+                        "Cannot handle unknown supervisor interrupt, scause: {:#x}, trapframe: {:#x?}.",
+                        scause.bits(), f
                     );
                 }
             }
             IS_KERNEL_INTERRUPTED.store(false);
         }
-        Trap::Exception(e) => {
-            let stval = riscv::register::stval::read();
-            panic!(
-                "Cannot handle kernel cpu exception: {e:?}. stval: {stval:#x}, trapframe: {f:#x?}.",
-            );
+        Exception(e) => {
+            use CpuException::*;
+
+            let exception = e.into();
+            // The IRQ state before trapping. We need to ensure that the IRQ state
+            // during exception handling is consistent with the state before the trap.
+            let was_irq_enabled = riscv::register::sstatus::read().spie();
+            enable_local_if(was_irq_enabled);
+            match exception {
+                Unknown => {
+                    panic!(
+                        "Cannot handle unknown exception, scause: {:#x}, trapframe: {:#x?}.",
+                        scause.bits(),
+                        f
+                    );
+                }
+                _ => {
+                    panic!(
+                        "Cannot handle kernel exception, exception: {:?}, trapframe: {:#x?}.",
+                        exception, f
+                    );
+                }
+            };
+            disable_local_if(was_irq_enabled);
         }
     }
 }
 
 #[expect(clippy::type_complexity)]
-static USER_PAGE_FAULT_HANDLER: Once<fn(&CpuExceptionInfo) -> core::result::Result<(), ()>> =
+static USER_PAGE_FAULT_HANDLER: Once<fn(&CpuException) -> core::result::Result<(), ()>> =
     Once::new();
 
 /// Injects a custom handler for page faults that occur in the kernel and
 /// are caused by user-space address.
 pub fn inject_user_page_fault_handler(
-    handler: fn(info: &CpuExceptionInfo) -> core::result::Result<(), ()>,
+    handler: fn(info: &CpuException) -> core::result::Result<(), ()>,
 ) {
     USER_PAGE_FAULT_HANDLER.call_once(|| handler);
 }
