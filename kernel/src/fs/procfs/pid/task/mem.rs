6c32411f7557d776bd88788935708d9dde4ff800
Wang Siyuan <wsy@stu.pku.edu.cn>
2025-08-10 12:55:26 +0000

    Support `/proc/[pid]/mem` and `/proc/[pid]/task/[tid]/mem`

---
+++ kernel/src/fs/procfs/pid/task/mem.rs
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MPL-2.0
+
+use crate::{
+    fs::{
+        procfs::template::{FileOps, ProcFileBuilder},
+        utils::{mkmod, Inode},
+    },
+    prelude::*,
+    Process,
+};
+
+/// Represents the inode at either `/proc/[pid]/mem` or `/proc/[pid]/task/[tid]/mem`.
+pub struct MemFileOps(Arc<Process>);
+
+impl MemFileOps {
+    pub fn new_inode(process_ref: Arc<Process>, parent: Weak<dyn Inode>) -> Arc<dyn Inode> {
+        ProcFileBuilder::new(Self(process_ref), mkmod!(u+rw))
+            .parent(parent)
+            // Reference: <https://github.com/torvalds/linux/blob/0ff41df1cb268fc69e703a08a57ee14ae967d0ca/fs/proc/base.c#L3341>
+            .build()
+            .unwrap()
+    }
+}
+
+impl FileOps for MemFileOps {
+    fn data(&self) -> Result<Vec<u8>> {
+        unreachable!()
+    }
+
+    fn read_at(&self, offset: usize, writer: &mut VmWriter) -> Result<usize> {
+        let vmar_guard = self.0.lock_root_vmar();
+        let vmar = vmar_guard
+            .as_ref()
+            .ok_or_else(|| Error::with_message(Errno::ESRCH, "the process has exited"))?;
+
+        vmar.read_maybe_remote(offset, writer)
+    }
+
+    fn write_at(&self, offset: usize, reader: &mut VmReader) -> Result<usize> {
+        let vmar_guard = self.0.lock_root_vmar();
+        let vmar = vmar_guard
+            .as_ref()
+            .ok_or_else(|| Error::with_message(Errno::ESRCH, "the process has exited"))?;
+
+        vmar.write_maybe_remote(offset, reader)
+    }
+}
