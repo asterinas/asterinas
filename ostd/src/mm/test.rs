c2c17df8a52533072b4e172872ac4c2833839140
Zhe Tang <tangzh6101@gmail.com>
2025-08-13 16:08:25 +0000

    Add OSTD support for mapping `IoMem` in userspace
    ostd patch

--- ostd/src/mm/test.rs
+++ ostd/src/mm/test.rs
@@ -10,7 +10,7 @@ use crate::{
         io::{VmIo, VmIoFill, VmReader, VmWriter},
         io_util::HasVmReaderWriter,
         tlb::TlbFlushOp,
-        vm_space::get_activated_vm_space,
+        vm_space::{get_activated_vm_space, VmQueriedItem},
         CachePolicy, FallibleVmRead, FallibleVmWrite, FrameAllocOptions, PageFlags, PageProperty,
         UFrame, VmSpace,
     },
@@ -538,7 +538,13 @@ mod vmspace {
             assert_eq!(cursor.virt_addr(), range.start);
             assert_eq!(
                 cursor.query().unwrap(),
-                (range.clone(), Some((frame.clone(), prop)))
+                (
+                    range.clone(),
+                    Some(VmQueriedItem::MappedRam {
+                        frame: frame.clone(),
+                        prop
+                    })
+                )
             );
         }
 
@@ -579,7 +585,13 @@ mod vmspace {
                 .expect("Failed to create cursor");
             assert_eq!(
                 cursor.query().unwrap(),
-                (range.clone(), Some((frame.clone(), prop)))
+                (
+                    range.clone(),
+                    Some(VmQueriedItem::MappedRam {
+                        frame: frame.clone(),
+                        prop
+                    })
+                )
             );
         }
 
@@ -596,7 +608,13 @@ mod vmspace {
                 .expect("Failed to create cursor");
             assert_eq!(
                 cursor.query().unwrap(),
-                (range.clone(), Some((frame.clone(), prop)))
+                (
+                    range.clone(),
+                    Some(VmQueriedItem::MappedRam {
+                        frame: frame.clone(),
+                        prop
+                    })
+                )
             );
         }
 
@@ -687,7 +705,13 @@ mod vmspace {
                 .expect("Failed to create cursor");
             assert_eq!(
                 cursor.next().unwrap(),
-                (range.clone(), Some((frame.clone(), prop)))
+                (
+                    range.clone(),
+                    Some(VmQueriedItem::MappedRam {
+                        frame: frame.clone(),
+                        prop
+                    })
+                )
             );
         }
 
@@ -709,10 +733,10 @@ mod vmspace {
                 cursor.next().unwrap(),
                 (
                     range.clone(),
-                    Some((
-                        frame.clone(),
-                        PageProperty::new_user(PageFlags::R, CachePolicy::Writeback)
-                    ))
+                    Some(VmQueriedItem::MappedRam {
+                        frame: frame.clone(),
+                        prop: PageProperty::new_user(PageFlags::R, CachePolicy::Writeback)
+                    })
                 )
             );
         }
@@ -800,15 +824,14 @@ mod vmspace {
             .cursor(&preempt_guard, &range)
             .expect("Failed to create cursor");
         assert!(cursor.jump(range.start).is_ok());
-        let item = cursor.next();
         assert_eq!(
-            item.unwrap(),
+            cursor.next().unwrap(),
             (
                 range.clone(),
-                Some((
-                    frame.clone(),
-                    PageProperty::new_user(PageFlags::R, CachePolicy::Writeback)
-                ))
+                Some(VmQueriedItem::MappedRam {
+                    frame,
+                    prop: PageProperty::new_user(PageFlags::R, CachePolicy::Writeback)
+                })
             )
         );
 
@@ -844,10 +867,10 @@ mod vmspace {
             cursor.next().unwrap(),
             (
                 range.clone(),
-                Some((
-                    frame.clone(),
-                    PageProperty::new_user(PageFlags::R, CachePolicy::Writeback)
-                ))
+                Some(VmQueriedItem::MappedRam {
+                    frame: frame.clone(),
+                    prop: PageProperty::new_user(PageFlags::R, CachePolicy::Writeback)
+                })
             )
         );
     }
