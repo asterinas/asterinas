4a49558ddcd7b8ee58182042b3f9f8d075d764df
Hsy-Intel <siyuan.hui@intel.com>
2025-08-15 16:48:39 +0800

    Update Docker image references to remove '-tdx' suffix for consistency

--- osdk/tools/docker/run_container.sh
+++ osdk/tools/docker/run_container.sh
@@ -7,11 +7,6 @@ set -e
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ASTER_ROOT_DIR=${SCRIPT_DIR}/../..
 VERSION=$( cat ${ASTER_ROOT_DIR}/VERSION )
-
-if [ "$1" = "intel-tdx" ]; then
-    IMAGE_NAME="asterinas/osdk:${VERSION}-tdx"
-else
-    IMAGE_NAME="asterinas/osdk:${VERSION}"
-fi
+IMAGE_NAME="asterinas/osdk:${VERSION}"
 
 docker run -it -v ${ASTER_ROOT_DIR}:/root/asterinas ${IMAGE_NAME}
--- tools/docker/run_dev_container.sh
+++ tools/docker/run_dev_container.sh
@@ -8,11 +8,6 @@ SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ASTER_SRC_DIR=${SCRIPT_DIR}/../..
 CARGO_TOML_PATH=${SCRIPT_DIR}/../../Cargo.toml
 VERSION=$( cat ${ASTER_SRC_DIR}/VERSION )
-
-if [ "$1" = "intel-tdx" ]; then
-    IMAGE_NAME="asterinas/asterinas:${VERSION}-tdx"
-else
-    IMAGE_NAME="asterinas/asterinas:${VERSION}"
-fi
+IMAGE_NAME="asterinas/asterinas:${VERSION}"
 
 docker run -it --privileged --network=host --device=/dev/kvm --device=/dev/vhost-net -v ${ASTER_SRC_DIR}:/root/asterinas ${IMAGE_NAME}
