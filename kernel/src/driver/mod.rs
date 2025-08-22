5b2b8f9bfd50467eec2759488adc66dd0fd2b110
Cautreoxit <huk@stu.pku.edu.cn>
2025-08-22 16:53:11 +0800

    Migrate i8042 keyboard to input subsystem API

--- kernel/src/driver/mod.rs
+++ kernel/src/driver/mod.rs
@@ -6,10 +6,15 @@ use aster_framebuffer::{CONSOLE_NAME, FRAMEBUFFER_CONSOLE};
 use log::info;
 
 pub fn init() {
-    // print all the input device to make sure input crate will compile
     for device in aster_input::all_devices() {
-        info!("Found Input device, name:{}", device.name());
+        info!("Found an input device, name:{}", device.name());
     }
+
+    // FIXME: Currently, we have to do this manually to ensure the crates containing the input
+    // devices are linked and their `#[init_component]` hooks can run to register the devices with
+    // the input core. We should find a way to avoid this in the future.
+    #[expect(unused_imports)]
+    use aster_keyboard::*;
 
     if let Some(console) = FRAMEBUFFER_CONSOLE.get() {
         aster_console::register_device(CONSOLE_NAME.to_string(), console.clone());
