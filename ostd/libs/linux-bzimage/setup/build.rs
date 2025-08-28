f27f141cad5bd9dfa2910f5c74406356d9b81e0c
Zejun Zhao <jelly.zhao.42@gmail.com>
2025-08-29 00:02:34 +0800

    Use rust-analyzer for riscv64 and loongarch64 target

--- ostd/libs/linux-bzimage/setup/build.rs
+++ ostd/libs/linux-bzimage/setup/build.rs
@@ -7,23 +7,27 @@ fn main() {
     let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
 
     let target_arch = std::env::var("TARGET").unwrap();
-    let lds = source_dir.join("src/x86/linker.ld");
-    let def = match target_arch.as_str() {
-        "x86_64-unknown-none" => "-DCFG_TARGET_ARCH_X86_64=1",
-        "x86_64-i386_pm-none" => "-DCFG_TARGET_ARCH_X86_64=0",
-        other => panic!("unsupported target: {}", other),
-    };
+    if target_arch.starts_with("x86_64") {
+        let lds = source_dir.join("src/x86/linker.ld");
+        let def = match target_arch.as_str() {
+            "x86_64-unknown-none" => "-DCFG_TARGET_ARCH_X86_64=1",
+            "x86_64-i386_pm-none" => "-DCFG_TARGET_ARCH_X86_64=0",
+            other => panic!("unsupported x86_64 target: {}", other),
+        };
 
-    let out_lds = out_dir.join("linker.ld");
-    let status = Command::new("cpp")
-        .arg("-o")
-        .arg(&out_lds)
-        .arg(def)
-        .arg(&lds)
-        .status()
-        .expect("failed to run the preprocessor");
-    assert!(status.success(), "the preprocessor exits with failure");
+        let out_lds = out_dir.join("linker.ld");
+        let status = Command::new("cpp")
+            .arg("-o")
+            .arg(&out_lds)
+            .arg(def)
+            .arg(&lds)
+            .status()
+            .expect("failed to run the preprocessor");
+        assert!(status.success(), "the preprocessor exits with failure");
 
-    println!("cargo:rerun-if-changed={}", lds.display());
-    println!("cargo:rustc-link-arg=-T{}", out_lds.display());
+        println!("cargo:rerun-if-changed={}", lds.display());
+        println!("cargo:rustc-link-arg=-T{}", out_lds.display());
+    } else {
+        println!("cargo:warning=Unsupported target: {}", target_arch);
+    }
 }
