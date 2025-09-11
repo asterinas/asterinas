802ecfd64fdd67edfe82ddca22f89f9e7911dd71
Chen Chengjun <chenchengjun.ccj@antgroup.com>
2025-09-11 08:34:54 +0000

    Add ReadString trait for VmReader

--- kernel/src/util/mod.rs
+++ kernel/src/util/mod.rs
@@ -5,7 +5,9 @@ pub mod net;
 mod padded;
 pub mod per_cpu_counter;
 pub mod random;
+mod read_string;
 pub mod ring_buffer;
 
 pub use iovec::{MultiRead, MultiWrite, VmReaderArray, VmWriterArray};
 pub use padded::padded;
+pub use read_string::ReadString;
