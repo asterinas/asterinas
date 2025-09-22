f1d949d05bc49f30c87879273bb7c5c07c4c347e
Chen Chengjun <chenchengjun.ccj@antgroup.com>
2025-09-22 07:05:24 +0000

    Add /proc/[pid]/cgroup

---
+++ kernel/src/fs/procfs/pid/task/cgroup.rs
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MPL-2.0
+
+use alloc::format;
+
+use aster_systree::SysObj;
+
+use crate::{
+    fs::{
+        cgroupfs::CgroupSystem,
+        procfs::template::{FileOps, ProcFileBuilder},
+        utils::{mkmod, Inode},
+    },
+    prelude::*,
+    Process,
+};
+
+/// Represents the inode at `/proc/[pid]/cgroup`.
+pub struct CgroupOps(Arc<Process>);
+
+impl CgroupOps {
+    pub fn new_inode(process_ref: Arc<Process>, parent: Weak<dyn Inode>) -> Arc<dyn Inode> {
+        ProcFileBuilder::new(Self(process_ref), mkmod!(a+r))
+            .parent(parent)
+            .build()
+            .unwrap()
+    }
+}
+
+impl FileOps for CgroupOps {
+    fn data(&self) -> Result<Vec<u8>> {
+        let path = self
+            .0
+            .cgroup()
+            .get()
+            .map(|cgroup| cgroup.path())
+            .unwrap_or_else(|| CgroupSystem::singleton().path());
+
+        let data = format!("{}{}\n", "0::", path);
+        Ok(data.into_bytes())
+    }
+}
