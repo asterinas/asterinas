79f9d0fdeed8f0232040112b55d96a2379003779
Zejun Zhao <jelly.zhao.42@gmail.com>
2025-09-27 21:49:33 +0800

    Make writable pages readable on RISC-V platforms

--- kernel/src/syscall/mprotect.rs
+++ kernel/src/syscall/mprotect.rs
@@ -34,9 +34,12 @@ pub fn sys_mprotect(addr: Vaddr, len: usize, perms: u64, ctx: &Context) -> Resul
     ))?;
     let range = addr..end;
 
-    // On x86, `PROT_WRITE` implies `PROT_READ`.
-    // <https://man7.org/linux/man-pages/man2/mprotect.2.html>
-    #[cfg(target_arch = "x86_64")]
+    // On x86_64 and riscv64, `PROT_WRITE` implies `PROT_READ`.
+    // Reference:
+    // <https://man7.org/linux/man-pages/man2/mprotect.2.html>,
+    // Section 5.11.3 from <https://www.intel.com/content/dam/www/public/us/en/documents/manuals/64-ia-32-architectures-software-developer-vol-3a-part-1-manual.pdf>,
+    // <https://riscv.github.io/riscv-isa-manual/snapshot/privileged/#translation>.
+    #[cfg(any(target_arch = "x86_64", target_arch = "riscv64"))]
     let vm_perms = if !vm_perms.contains(VmPerms::READ) && vm_perms.contains(VmPerms::WRITE) {
         vm_perms | VmPerms::READ
     } else {
