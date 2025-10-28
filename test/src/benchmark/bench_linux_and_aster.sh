63efd4891b36e49c466c8a5137820e4dcb878143
Hsy-Intel <siyuan.hui@intel.com>
2025-10-29 06:33:45 +0800

    Fix Linux TDX network performance by unifying virtio-net-pci parameters

--- test/src/benchmark/bench_linux_and_aster.sh
+++ test/src/benchmark/bench_linux_and_aster.sh
@@ -133,7 +133,6 @@ run_benchmark() {
         asterinas_cmd_arr+=(INTEL_TDX=1)
     fi
 
-    # TODO: `guest_uso4=off,guest_uso6=off,host_uso=off` is not supported by the QEMU of TDX development image.
     local linux_cmd_arr=(
         qemu-system-x86_64
         --no-reboot
@@ -145,6 +144,7 @@ run_benchmark() {
         -initrd "${BENCHMARK_ROOT}/../../build/initramfs.cpio.gz"
         -drive "if=none,format=raw,id=x0,file=${BENCHMARK_ROOT}/../../build/ext2.img"
         -device "virtio-blk-pci,bus=pcie.0,addr=0x6,drive=x0,serial=vext2,disable-legacy=on,disable-modern=off,queue-size=64,num-queues=1,request-merging=off,backend_defaults=off,discard=off,write-zeroes=off,event_idx=off,indirect_desc=off,queue_reset=off"
+        -device "virtio-net-pci,netdev=net01,disable-legacy=on,disable-modern=off,csum=off,guest_csum=off,ctrl_guest_offloads=off,guest_tso4=off,guest_tso6=off,guest_ecn=off,guest_ufo=off,host_tso4=off,host_tso6=off,host_ecn=off,mrg_rxbuf=off,ctrl_vq=off,ctrl_rx=off,ctrl_vlan=off,ctrl_rx_extra=off,guest_announce=off,ctrl_mac_addr=off,host_ufo=off,guest_uso4=off,guest_uso6=off,host_uso=off"
         -append "console=ttyS0 rdinit=/benchmark/common/bench_runner.sh ${benchmark} linux mitigations=off hugepages=0 transparent_hugepage=never quiet"
         -netdev "tap,id=net01,script=${BENCHMARK_ROOT}/../../../tools/net/qemu-ifup.sh,downscript=${BENCHMARK_ROOT}/../../../tools/net/qemu-ifdown.sh,vhost=on"
         -nographic
@@ -152,7 +152,6 @@ run_benchmark() {
     if [[ "$platform" != "tdx" ]]; then
         linux_cmd_arr+=(
             -cpu Icelake-Server,-pcid,+x2apic
-            -device "virtio-net-pci,netdev=net01,disable-legacy=on,disable-modern=off,csum=off,guest_csum=off,ctrl_guest_offloads=off,guest_tso4=off,guest_tso6=off,guest_ecn=off,guest_ufo=off,host_tso4=off,host_tso6=off,host_ecn=off,host_ufo=off,mrg_rxbuf=off,ctrl_vq=off,ctrl_rx=off,ctrl_vlan=off,ctrl_rx_extra=off,guest_announce=off,ctrl_mac_addr=off,host_ufo=off,guest_uso4=off,guest_uso6=off,host_uso=off"
         )
     else
         linux_cmd_arr+=(
@@ -162,7 +161,6 @@ run_benchmark() {
             -nodefaults
             -serial stdio
             -object '{ "qom-type": "tdx-guest", "id": "tdx0", "sept-ve-disable": true, "quote-generation-socket": { "type": "vsock", "cid": "2", "port": "4050" } }'
-            -device "virtio-net-pci,netdev=net01,disable-legacy=on,disable-modern=off,csum=off,guest_csum=off,ctrl_guest_offloads=off,guest_tso4=off,guest_tso6=off,guest_ecn=off,guest_ufo=off,host_tso4=off,host_tso6=off,host_ecn=off,host_ufo=off,mrg_rxbuf=off,ctrl_vq=off,ctrl_rx=off,ctrl_vlan=off,ctrl_rx_extra=off,guest_announce=off,ctrl_mac_addr=off,host_ufo=off"
         )
     fi
 
