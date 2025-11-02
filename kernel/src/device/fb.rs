c548194c3c57bf12cf799bb2ec537a6c30a89905
Zhe Tang <tangzh@stu.pku.edu.cn>
2025-11-02 02:29:14 +0000

    Support `read` and `write` for the EFI framebuffer

--- kernel/src/device/fb.rs
+++ kernel/src/device/fb.rs
@@ -80,9 +80,56 @@ impl FileIo for Fb {
 }
 
 impl FileIo for FbHandle {
-    fn read(&self, writer: &mut VmWriter, _status_flags: StatusFlags) -> Result<usize> {}
+    fn read(&self, writer: &mut VmWriter, _status_flags: StatusFlags) -> Result<usize> {
+        if !writer.has_avail() {
+            return Ok(0);
+        }
 
-    fn write(&self, reader: &mut VmReader, _status_flags: StatusFlags) -> Result<usize> {}
+        let mut reader = self.framebuffer.io_mem().reader();
+        let mut offset = self.offset.lock();
+
+        if *offset >= reader.remain() {
+            return Ok(0);
+        }
+        reader.skip(*offset);
+
+        let mut reader = reader.to_fallible();
+        let len = match reader.read_fallible(writer) {
+            Ok(len) => len,
+            Err((err, 0)) => return Err(err.into()),
+            Err((_err, len)) => len,
+        };
+        *offset += len;
+
+        Ok(len)
+    }
+
+    fn write(&self, reader: &mut VmReader, _status_flags: StatusFlags) -> Result<usize> {
+        if !reader.has_remain() {
+            return Ok(0);
+        }
+
+        let mut writer = self.framebuffer.io_mem().writer();
+        let mut offset = self.offset.lock();
+
+        if *offset >= writer.avail() {
+            return_errno_with_message!(
+                Errno::ENOSPC,
+                "the write offset is beyond the framebuffer size"
+            );
+        }
+        writer.skip(*offset);
+
+        let mut writer = writer.to_fallible();
+        let len = match writer.write_fallible(reader) {
+            Ok(len) => len,
+            Err((err, 0)) => return Err(err.into()),
+            Err((_err, len)) => len,
+        };
+
+        *offset += len;
+        Ok(len)
+    }
 
     fn mappable(&self) -> Result<Mappable> {
         let iomem = self.framebuffer.io_mem();
