3dda868ffa344c3dd52fc99f1d6195b798f2da9b
Wang Siyuan <wsy@stu.pku.edu.cn>
2025-11-07 06:41:12 +0000

    Move the executable `PathOrInode` to `ProcessVm`

--- kernel/src/process/signal/mod.rs
+++ kernel/src/process/signal/mod.rs
@@ -117,8 +117,8 @@ pub fn handle_pending_signal(
             match sig_default_action {
                 SigDefaultAction::Core | SigDefaultAction::Term => {
                     warn!(
-                        "{:?}: terminating on signal {}",
-                        ctx.process.executable_file().display_name(),
+                        "PID {}: terminating on signal {}",
+                        ctx.process.pid(),
                         sig_num.sig_name()
                     );
                     // We should exit current here, since we cannot restore a valid status from trap now.
