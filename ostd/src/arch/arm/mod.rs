0d367275cce9e375f70fca2d8dc4a56e8a1c30ec
Ruihan Li <lrh2000@pku.edu.cn>
2025-11-11 23:13:42 +0800

    Add AArch64 context switching support

--- ostd/src/arch/arm/mod.rs
+++ ostd/src/arch/arm/mod.rs
@@ -60,6 +60,26 @@ pub(crate) unsafe fn init_on_ap() {
     unimplemented!()
 }
 
+/// Returns the frequency of TSC. The unit is Hz.
+pub fn tsc_freq() -> u64 {
+    use core::arch::asm;
+
+    let cntfrq;
+    // SAFETY: It is safe to read a time-related counter.
+    unsafe { asm!("mrs {}, cntfrq_el0", out(reg) cntfrq) };
+    cntfrq
+}
+
+/// Reads the current value of the processor's time-stamp counter (TSC).
+pub fn read_tsc() -> u64 {
+    use core::arch::asm;
+
+    let cntvct;
+    // SAFETY: It is safe to read a time-related counter.
+    unsafe { asm!("mrs {}, cntvct_el0", out(reg) cntvct) };
+    cntvct
+}
+
 /// Reads a hardware generated 64-bit random value.
 ///
 /// Returns `None` if no random value was generated.
