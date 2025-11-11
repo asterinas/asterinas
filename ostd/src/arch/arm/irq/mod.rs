5676c1cb90f743027d749bc31306cd521b51213e
Ruihan Li <lrh2000@pku.edu.cn>
2025-11-11 23:13:42 +0800

    Add AArch64 context switching support

--- ostd/src/arch/arm/irq/mod.rs
+++ ostd/src/arch/arm/irq/mod.rs
@@ -7,6 +7,7 @@ mod ops;
 mod remapping;
 
 pub(crate) use ipi::{HwCpuId, send_ipi};
+pub(super) use ops::PSTATE_I;
 pub(crate) use ops::{
     disable_local, disable_local_and_halt, enable_local, enable_local_and_halt, is_local_enabled,
 };
