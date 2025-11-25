30efc6502a29ace6932e103f1255cf249b36c06c
Zhang Junyang <junyang@stu.pku.edu.cn>
2025-11-25 20:04:23 +0800

    Add a set of APIs for auxiliary PT metadata

--- kernel/src/vm/vmar/vmar_impls/mod.rs
+++ kernel/src/vm/vmar/vmar_impls/mod.rs
@@ -27,10 +27,8 @@ use crate::{
     process::{Process, ProcessVm, ResourceType},
 };
 
-/// The VMAR (used to be Virtual Memory Address Region, but now an orphan
-/// initialism).
-///
-/// A VMAR is the address space of a process.
+/// Virtual Memory Address Regions (VMARs) are a type of capability that manages
+/// user address spaces.
 pub struct Vmar {
     /// VMAR inner
     inner: RwMutex<VmarInner>,
@@ -46,7 +44,7 @@ impl Vmar {
     /// Creates a new VMAR.
     pub fn new(process_vm: ProcessVm) -> Arc<Self> {
         let inner = VmarInner::new();
-        let vm_space = VmSpace::new();
+        let vm_space = VmSpace::<()>::new();
         let rss_counters = array::from_fn(|_| PerCpuCounter::new());
         Arc::new(Vmar {
             inner: RwMutex::new(inner),
