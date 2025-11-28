ee1d36f16464d7b69ec3c5cb3d381dd2def87aab
Ruihan Li <lrh2000@pku.edu.cn>
2025-11-28 10:49:08 +0800

    Simplify code and resolve lints

--- kernel/src/syscall/reboot.rs
+++ kernel/src/syscall/reboot.rs
@@ -4,14 +4,14 @@ use super::SyscallReturn;
 use crate::{prelude::*, process::credentials::capabilities::CapSet};
 
 // Linux reboot magic constants.
-const LINUX_REBOOT_MAGIC1: i32 = 0xfee1dead_u32 as i32;
-const LINUX_REBOOT_MAGIC2: i32 = 0x28121969;
-const LINUX_REBOOT_MAGIC2A: i32 = 0x05121996;
-const LINUX_REBOOT_MAGIC2B: i32 = 0x16041998;
-const LINUX_REBOOT_MAGIC2C: i32 = 0x20112000;
+const LINUX_REBOOT_MAGIC1: u32 = 0xfee1dead;
+const LINUX_REBOOT_MAGIC2: u32 = 0x28121969;
+const LINUX_REBOOT_MAGIC2A: u32 = 0x05121996;
+const LINUX_REBOOT_MAGIC2B: u32 = 0x16041998;
+const LINUX_REBOOT_MAGIC2C: u32 = 0x20112000;
 
 /// Linux reboot commands.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, TryFromInt)]
 #[repr(u32)]
 enum RebootCmd {
     Restart = 0x01234567,
@@ -20,42 +20,28 @@ enum RebootCmd {
     // TODO: Add more reboot sub-commands.
 }
 
-impl TryFrom<i32> for RebootCmd {
-    type Error = Error;
-
-    fn try_from(value: i32) -> Result<Self> {
-        match value {
-            0x01234567 => Ok(Self::Restart),
-            0xcdef0123 => Ok(Self::Halt),
-            0x4321fedc => Ok(Self::PowerOff),
-            _ => return_errno_with_message!(Errno::EINVAL, "invalid reboot command"),
-        }
-    }
-}
-
 pub fn sys_reboot(
-    magic1: i32,
-    magic2: i32,
-    op: i32,
+    magic1: u32,
+    magic2: u32,
+    op: u32,
     _arg: Vaddr,
     ctx: &Context,
 ) -> Result<SyscallReturn> {
     debug!(
-        "Reboot syscall invoked with magic1: {:#x}, magic2: {:#x}, op: {:#x}",
+        "[sys_reboot]: magic1 = {:#x}, magic2 = {:#x}, op = {:#x}",
         magic1, magic2, op
     );
 
     // Verify magic numbers
     if magic1 != LINUX_REBOOT_MAGIC1 {
-        return_errno_with_message!(Errno::EINVAL, "invalid magic1");
+        return_errno_with_message!(Errno::EINVAL, "the reboot magic is invalid");
     }
-
     if magic2 != LINUX_REBOOT_MAGIC2
         && magic2 != LINUX_REBOOT_MAGIC2A
         && magic2 != LINUX_REBOOT_MAGIC2B
         && magic2 != LINUX_REBOOT_MAGIC2C
     {
-        return_errno_with_message!(Errno::EINVAL, "invalid magic2");
+        return_errno_with_message!(Errno::EINVAL, "the reboot magic is invalid");
     }
 
     if !ctx
@@ -64,7 +50,7 @@ pub fn sys_reboot(
         .effective_capset()
         .contains(CapSet::SYS_BOOT)
     {
-        return_errno_with_message!(Errno::EPERM, "insufficient capabilities for reboot");
+        return_errno_with_message!(Errno::EPERM, "reboot without SYS_BOOT is not allowed");
     }
 
     let cmd = RebootCmd::try_from(op)?;
@@ -72,17 +58,11 @@ pub fn sys_reboot(
     match cmd {
         RebootCmd::Restart => {
             // TODO: Implement restart functionality.
-            return_errno_with_message!(Errno::ENOSYS, "restart not implemented");
+            return_errno_with_message!(Errno::ENOSYS, "restart is not implemented yet");
         }
         RebootCmd::Halt | RebootCmd::PowerOff => {
-            poweroff();
+            // TODO: Perform any necessary cleanup before powering off.
+            ostd::arch::cpu::poweroff::poweroff();
         }
     }
-}
-
-fn poweroff() -> ! {
-    use ostd::arch::cpu::poweroff::poweroff;
-
-    // TODO: Perform any necessary cleanup before powering off.
-    poweroff()
 }
