15446386afc3dd8b72b770334adda22e8356f935
Ruihan Li <lrh2000@pku.edu.cn>
2025-11-28 19:22:55 +0800

    Don't panic in the panic handler

--- ostd/src/arch/loongarch/irq/mod.rs
+++ ostd/src/arch/loongarch/irq/mod.rs
@@ -8,7 +8,9 @@ mod ops;
 mod remapping;
 
 pub(crate) use ipi::{send_ipi, HwCpuId};
-pub(crate) use ops::{disable_local, enable_local, enable_local_and_halt, is_local_enabled};
+pub(crate) use ops::{
+    disable_local, disable_local_and_halt, enable_local, enable_local_and_halt, is_local_enabled,
+};
 pub(crate) use remapping::IrqRemapping;
 
 pub(crate) const IRQ_NUM_MIN: u8 = 0;
