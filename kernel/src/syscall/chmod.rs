8152695a17ed9bcec64972a736996057a5af03be
vvsv <vvsv_7169@163.com>
2025-12-02 10:03:57 +0000

    Support syscall fchmodat2

--- kernel/src/syscall/chmod.rs
+++ kernel/src/syscall/chmod.rs
@@ -24,32 +24,62 @@ pub fn sys_fchmod(fd: FileDesc, mode: u16, ctx: &Context) -> Result<SyscallRetur
 }
 
 pub fn sys_chmod(path_ptr: Vaddr, mode: u16, ctx: &Context) -> Result<SyscallReturn> {
-    self::sys_fchmodat(AT_FDCWD, path_ptr, mode, ctx)
+    do_fchmodat(AT_FDCWD, path_ptr, mode, ChmodFlags::empty(), ctx)
 }
 
-// Glibc handles the `flags` argument, so we just ignore it.
 pub fn sys_fchmodat(
     dirfd: FileDesc,
     path_ptr: Vaddr,
     mode: u16,
-    /* flags: u32, */
+    ctx: &Context,
+) -> Result<SyscallReturn> {
+    do_fchmodat(dirfd, path_ptr, mode, ChmodFlags::empty(), ctx)
+}
+
+pub fn sys_fchmodat2(
+    dirfd: FileDesc,
+    path_ptr: Vaddr,
+    mode: u16,
+    flags: u32,
+    ctx: &Context,
+) -> Result<SyscallReturn> {
+    let flags = ChmodFlags::from_bits(flags)
+        .ok_or_else(|| Error::with_message(Errno::EINVAL, "invalid chmod flags"))?;
+
+    do_fchmodat(dirfd, path_ptr, mode, flags, ctx)
+}
+
+fn do_fchmodat(
+    dirfd: FileDesc,
+    path_ptr: Vaddr,
+    mode: u16,
+    flags: ChmodFlags,
     ctx: &Context,
 ) -> Result<SyscallReturn> {
     let path_name = ctx.user_space().read_cstring(path_ptr, PATH_MAX)?;
+
     debug!(
-        "dirfd = {}, path_name = {:?}, mode = 0o{:o}",
-        dirfd, path_name, mode,
+        "dirfd = {}, path_name = {:?}, mode = 0o{:o}, flags = {:?}",
+        dirfd, path_name, mode, flags,
     );
 
     let path_or_inode = {
         let path_name = path_name.to_string_lossy();
-        let fs_path = FsPath::from_fd_and_path(dirfd, &path_name)?;
-        ctx.thread_local
-            .borrow_fs()
-            .resolver()
-            .read()
-            .lookup_inode(&fs_path)?
+        let fs_path = if flags.contains(ChmodFlags::AT_EMPTY_PATH) && path_name.is_empty() {
+            FsPath::from_fd(dirfd)?
+        } else {
+            FsPath::from_fd_and_path(dirfd, &path_name)?
+        };
+
+        let fs_ref = ctx.thread_local.borrow_fs();
+        let fs = fs_ref.resolver().read();
+        if flags.contains(ChmodFlags::AT_SYMLINK_NOFOLLOW) {
+            fs.lookup_inode_no_follow(&fs_path)?
+        } else {
+            fs.lookup_inode(&fs_path)?
+        }
     };
+
     path_or_inode
         .inode()
         .set_mode(InodeMode::from_bits_truncate(mode))?;
@@ -57,4 +87,11 @@ pub fn sys_fchmodat(
         fs::notify::on_attr_change(&path);
     }
     Ok(SyscallReturn::Return(0))
+}
+
+bitflags::bitflags! {
+    struct ChmodFlags: u32 {
+        const AT_EMPTY_PATH = 1 << 12;
+        const AT_SYMLINK_NOFOLLOW = 1 << 8;
+    }
 }
