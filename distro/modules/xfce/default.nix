1beace3922e36a5e29091e8f5ee337bc38cf7e24
Wei Zhang <ruoyuan.zw@antgroup.com>
2025-12-03 00:14:05 +0000

    Add xfce nix overlay
    Signed-off-by: Wei Zhang <ruoyuan.zw@antgroup.com>

---
+++ distro/modules/xfce/default.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, ... }:
+let
+  startXfce = pkgs.writeScriptBin "start_xfce" (builtins.readFile ./start_xfce.sh);
+in
+{
+  environment.systemPackages =
+    (lib.optionals (config.services.xserver.enable &&
+      config.services.xserver.desktopManager.xfce.enable)
+      [ startXfce ])
+    ++ (lib.optionals config.services.xserver.enable
+      [ pkgs.xorg.xf86videofbdev ]);
+
+  systemd.services."xfce-desktop" = lib.mkIf
+    (config.services.xserver.enable && config.services.xserver.desktopManager.xfce.enable)
+    {
+      description = "XFCE Desktop Environment";
+      after = [ "multi-user.target" ];
+      wantedBy = [ "graphical.target" ];
+      serviceConfig = {
+        Environment = "DISPLAY=:0";
+        ExecStart = "${startXfce}/bin/start_xfce";
+        StandardOutput = "tty";
+        StandardError = "tty";
+        KillMode = "process";
+        Delegate = "yes";
+        Restart = "no";
+        Type = "simple";
+      };
+    };
+}
