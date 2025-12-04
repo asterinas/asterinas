99fefb7adf1bfeda7e24991fdff0a0ae0843d387
Ruihan Li <lrh2000@pku.edu.cn>
2025-12-04 11:50:06 +0800

    Implement new ioctl infrastructure

---
+++ kernel/src/util/ioctl/sealed.rs
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MPL-2.0
+
+use int_to_c_enum::TryFromInt;
+
+/// An ioctl command that follows Linux-style encoding.
+///
+/// Layout (from LSB to MSB):
+/// - bits 0..=7   : command number (nr)
+/// - bits 8..=15  : type / magic
+/// - bits 16..=29 : size (in bytes)
+/// - bits 30..=31 : direction
+///
+/// Reference: <https://elixir.bootlin.com/linux/v6.18/source/include/uapi/asm-generic/ioctl.h#L69-L73>
+#[derive(Clone, Copy, Debug)]
+pub(super) struct IoctlCmd(u32);
+
+/// The direction of data transfer for an ioctl command.
+///
+/// The meaning is from the *user-space* perspective,
+/// following Linux's `_IOC_*` macros:
+///  - `None`  -> `_IOC_NONE`
+///  - `Write` -> `_IOC_WRITE` (user to kernel)
+///  - `Read`  -> `_IOC_READ`  (kernel to user)
+///  - `ReadWrite` -> `_IOC_READ | _IOC_WRITE`
+///
+/// Reference: <https://elixir.bootlin.com/linux/v6.18/source/include/uapi/asm-generic/ioctl.h#L49-L67>
+#[derive(Clone, Copy, Debug, PartialEq, Eq, TryFromInt)]
+#[repr(u8)]
+pub enum IoctlDir {
+    None = 0,
+    Write = 1,
+    Read = 2,
+    ReadWrite = 3,
+}
+
+impl IoctlCmd {
+    pub(super) const fn new(cmd: u32) -> Self {
+        Self(cmd)
+    }
+
+    pub(super) const fn nr(self) -> u8 {
+        // Bits 0..=7
+        self.0 as u8
+    }
+
+    pub(super) const fn magic(self) -> u8 {
+        // Bits 8..=15
+        (self.0 >> 8) as u8
+    }
+
+    pub(super) const fn size(self) -> u16 {
+        // Bits 16..=29
+        ((self.0 >> 16) as u16) & 0x3FFF
+    }
+
+    pub(super) fn dir(self) -> IoctlDir {
+        // Bits 30..=31
+        IoctlDir::try_from((self.0 >> 30) as u8).unwrap()
+    }
+}
+
+pub trait DataSpec {
+    const SIZE: Option<u16>;
+    const DIR: IoctlDir;
+}
+
+pub trait PtrDataSpec: DataSpec {
+    type Pointee;
+}
