fb3441dac5a2a02183b795075f373301d67b6f3d
Tao Su <st498824@antgroup.com>
2025-12-11 06:09:45 +0000

    Export all SCML file paths via environment variable in Docker image

--- tools/sctrace.sh
+++ tools/sctrace.sh
@@ -9,8 +9,5 @@ SCRIPT_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 ASTERINAS_ROOT="$(dirname "$SCRIPT_DIR")"
 
-# Search all SCML files
-scml_files=$(find "$ASTERINAS_ROOT/book/src/kernel/linux-compatibility" -type f -name "*.scml" | tr '\n' ' ')
-
 # Run sctrace with all arguments passed to this script
-cargo run -q --manifest-path "$ASTERINAS_ROOT/tools/sctrace/Cargo.toml" -- $scml_files "$@"
+cargo run -q --manifest-path "$ASTERINAS_ROOT/tools/sctrace/Cargo.toml" -- "$@"
