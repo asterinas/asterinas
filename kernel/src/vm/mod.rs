ae3113194a9d741a6b98aa1f61f8157ac06bdb3d
Zhang Junyang <junyang@stu.pku.edu.cn>
2025-12-17 11:40:30 +0800

    Adjust the documentation of `aster-nix::vm`

--- kernel/src/vm/mod.rs
+++ kernel/src/vm/mod.rs
@@ -3,18 +3,11 @@
 //! Virtual memory (VM).
 //!
 //! There are two primary VM abstractions:
-//!  * Virtual Memory Address Regions (VMARs) a type of capability that manages
-//!    user address spaces.
-//!  * Virtual Memory Objects (VMOs) are are a type of capability that
-//!    represents a set of memory pages.
-//!
-//! The concepts of VMARs and VMOs are originally introduced by
-//! [Zircon](https://fuchsia.dev/fuchsia-src/reference/kernel_objects/vm_object).
-//! As capabilities, the two abstractions are aligned with our goal
-//! of everything-is-a-capability, although their specifications and
-//! implementations in C/C++ cannot apply directly to Asterinas.
-//! In Asterinas, VMARs and VMOs, as well as other capabilities, are implemented
-//! as zero-cost capabilities.
+//!  * The VMAR (used to be Virtual Memory Address Region, now an orphan
+//!    initialism) represents the entire virtual address space of a process;
+//!  * The VMO (Virtual Memory Object) is a set of logically contiguous memory
+//!    frames that can be mapped into one virtual address range. Frames in a
+//!    VMO can be non-contiguous in physical memory.
 
 use osdk_frame_allocator::FrameAllocator;
 use osdk_heap_allocator::{HeapAllocator, type_from_layout};
