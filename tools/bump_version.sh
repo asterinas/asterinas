87c4462f64aba7bdf6bea415f4a551c8f0f02a10
jiangjianfeng <jiangjianfeng.jjf@antgroup.com>
2025-12-25 11:05:43 +0000

    Update bump_version.sh

--- tools/bump_version.sh
+++ tools/bump_version.sh
@@ -145,25 +145,14 @@ update_all_docker_version_refs() {
 # Update project dependencies (Cargo.toml and Cargo.lock)
 update_project_dependencies() {
     # Update the versions in Cargo.toml
-    update_package_version ${OSTD_TEST_CARGO_TOML_PATH}
-    update_package_version ${OSTD_MACROS_CARGO_TOML_PATH}
-    update_package_version ${OSTD_CARGO_TOML_PATH}
-    update_package_version ${LINUX_BOOT_PARAMS_CARGO_TOML_PATH}
-    update_package_version ${LINUX_BZIMAGE_BUILDER_CARGO_TOML_PATH}
-    update_package_version ${LINUX_BZIMAGE_SETUP_CARGO_TOML_PATH}
+    update_package_version ${WORKSPACE_CARGO_TOML_PATH}
     update_package_version ${OSDK_CARGO_TOML_PATH}
-    update_package_version ${OSDK_TEST_RUNNER_CARGO_TOML_PATH}
-    update_package_version ${OSDK_FRAME_ALLOCATOR_CARGO_TOML_PATH}
-    update_package_version ${OSDK_HEAP_ALLOCATOR_CARGO_TOML_PATH}
     update_package_version ${SCTRACE_CARGO_TOML_PATH}
 
-    update_dep_version ${OSDK_TEST_RUNNER_CARGO_TOML_PATH} ostd
-    update_dep_version ${OSDK_FRAME_ALLOCATOR_CARGO_TOML_PATH} ostd
-    update_dep_version ${OSDK_HEAP_ALLOCATOR_CARGO_TOML_PATH} ostd
-    update_dep_version ${OSTD_CARGO_TOML_PATH} ostd-test
-    update_dep_version ${OSTD_CARGO_TOML_PATH} linux-boot-params
-    update_dep_version ${OSTD_CARGO_TOML_PATH} ostd-macros
-    update_dep_version ${LINUX_BZIMAGE_SETUP_CARGO_TOML_PATH} linux-boot-params
+    update_dep_version ${WORKSPACE_CARGO_TOML_PATH} linux-boot-params
+    update_dep_version ${WORKSPACE_CARGO_TOML_PATH} ostd
+    update_dep_version ${WORKSPACE_CARGO_TOML_PATH} ostd-test
+    update_dep_version ${WORKSPACE_CARGO_TOML_PATH} ostd-macros
     update_dep_version ${OSDK_CARGO_TOML_PATH} linux-bzimage-builder
 
     # Automatically bump Cargo.lock files
@@ -202,17 +191,9 @@ sync_project_version() {
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ASTER_SRC_DIR=${SCRIPT_DIR}/..
 BOOK_DIR=${ASTER_SRC_DIR}/book
-OSTD_CARGO_TOML_PATH=${ASTER_SRC_DIR}/ostd/Cargo.toml
-OSTD_TEST_CARGO_TOML_PATH=${ASTER_SRC_DIR}/ostd/libs/ostd-test/Cargo.toml
-OSTD_MACROS_CARGO_TOML_PATH=${ASTER_SRC_DIR}/ostd/libs/ostd-macros/Cargo.toml
-LINUX_BOOT_PARAMS_CARGO_TOML_PATH=${ASTER_SRC_DIR}/ostd/libs/linux-bzimage/boot-params/Cargo.toml
-LINUX_BZIMAGE_BUILDER_CARGO_TOML_PATH=${ASTER_SRC_DIR}/ostd/libs/linux-bzimage/builder/Cargo.toml
-LINUX_BZIMAGE_SETUP_CARGO_TOML_PATH=${ASTER_SRC_DIR}/ostd/libs/linux-bzimage/setup/Cargo.toml
+WORKSPACE_CARGO_TOML_PATH=${ASTER_SRC_DIR}/Cargo.toml
 OSDK_DIR=${ASTER_SRC_DIR}/osdk
 OSDK_CARGO_TOML_PATH=${OSDK_DIR}/Cargo.toml
-OSDK_TEST_RUNNER_CARGO_TOML_PATH=${ASTER_SRC_DIR}/osdk/deps/test-kernel/Cargo.toml
-OSDK_FRAME_ALLOCATOR_CARGO_TOML_PATH=${ASTER_SRC_DIR}/osdk/deps/frame-allocator/Cargo.toml
-OSDK_HEAP_ALLOCATOR_CARGO_TOML_PATH=${ASTER_SRC_DIR}/osdk/deps/heap-allocator/Cargo.toml
 SCTRACE_CARGO_TOML_PATH=${ASTER_SRC_DIR}/tools/sctrace/Cargo.toml
 VERSION_PATH=${ASTER_SRC_DIR}/VERSION
 DOCKER_IMAGE_VERSION_PATH=${ASTER_SRC_DIR}/DOCKER_IMAGE_VERSION
