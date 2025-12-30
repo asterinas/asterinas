eb83a38d127960c067b5b3db93ca048e0e9f18ee
Ruihan Li <lrh2000@pku.edu.cn>
2025-12-30 17:05:37 +0800

    Don't panic in LoongArch64 VirtIO MMIO probe

--- kernel/comps/virtio/src/transport/mmio/bus/arch/loongarch.rs
+++ kernel/comps/virtio/src/transport/mmio/bus/arch/loongarch.rs
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MPL-2.0
 
-// TODO: Add `MappedIrqLine` support for Loongarch.
+// TODO: Add `MappedIrqLine` support for LoongArch.
 pub(super) use ostd::irq::IrqLine as MappedIrqLine;
 
 pub(super) fn probe_for_device() {
-    unimplemented!()
+    // TODO: Probe virtio devices on the MMIO bus in LoongArch.
+    // Then, register them by calling `super::try_register_mmio_device`.
 }
