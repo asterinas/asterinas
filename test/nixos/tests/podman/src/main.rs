d1f58589cee5392c11fcfc1547fd462a7a1780ba
Chen Chengjun <chenchengjun.ccj@antgroup.com>
2026-01-09 03:54:58 +0000

    Add tests for podman -it

--- test/nixos/tests/podman/src/main.rs
+++ test/nixos/tests/podman/src/main.rs
@@ -32,3 +32,18 @@ fn alpine_container_basic(nixos_shell: &mut Session) -> Result<(), Error> {
 
     Ok(())
 }
+
+#[nixos_test]
+fn alpine_interactive_session(nixos_shell: &mut Session) -> Result<(), Error> {
+    let container_session_desc = SessionDesc::new()
+        .expect_prompt("/ #")
+        .cmd_to_enter("podman run -it docker.io/library/alpine")
+        .cmd_to_exit("exit");
+
+    nixos_shell.enter_session_and_run(container_session_desc, |alpine_shell| {
+        alpine_shell.run_cmd_and_expect("ls /etc/alpine-release", "/etc/alpine-release")?;
+        Ok(())
+    })?;
+
+    Ok(())
+}
