82936b64ef8366bab9b4aaaa613d5a2a8bd06033
Xinyi Yu <1809327837@qq.com>
2026-01-30 03:47:50 +0000

    add run.list and block.list

--- test/initramfs/nix/xfstests.nix
+++ test/initramfs/nix/xfstests.nix
@@ -1,9 +1,5 @@
 { lib, stdenvNoCC, pkgs }:
 
-let
-  whiteListPath = ./../src/fs/xfstests/white.list;
-  hasWhiteList = builtins.pathExists whiteListPath;
-in
 stdenvNoCC.mkDerivation {
   name = "xfstests-package";
 
@@ -29,6 +25,8 @@ ${pkgs.gnused}/bin:\
 ${pkgs.gawk}/bin:\
 ${pkgs.coreutils}/bin:\
 ${pkgs.findutils}/bin:\
+${pkgs.util-linux}/bin:\
+${pkgs.util-linux}/sbin:\
 ${pkgs.bc}/bin:\
 ${pkgs.kmod}/bin:\
 ${pkgs.kmod}/sbin:\
@@ -37,9 +35,46 @@ ${pkgs.xfsprogs}/sbin:\
 ${pkgs.e2fsprogs}/bin:\
 ${pkgs.e2fsprogs}/sbin:\
 /bin:\
-/usr/bin
+/usr/bin:\
+/sbin:\
+/usr/sbin
 cd /xfstests
-exec ./check "$@"
+RUNLIST_FILE=""
+ARGS=""
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -R|--runlist)
+      RUNLIST_FILE="$2"
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      ARGS="$ARGS \"$1\""
+      shift
+      ;;
+  esac
+done
+for arg in "$@"; do
+  ARGS="$ARGS \"$arg\""
+done
+if [ -n "$RUNLIST_FILE" ]; then
+  if [ ! -f "$RUNLIST_FILE" ]; then
+    echo "Run list file not found: $RUNLIST_FILE" >&2
+    exit 2
+  fi
+  while IFS= read -r test; do
+    case "$test" in
+      ""|\#*) continue ;;
+    esac
+    ARGS="$ARGS \"$test\""
+  done < "$RUNLIST_FILE"
+fi
+# shellcheck disable=SC2086
+eval ./check $ARGS
+echo "All xfstests passed."
 EOF
     chmod +x $out/xfstests/run-xfstests.sh
 
@@ -50,12 +85,11 @@ EOF
     # Copy local.config from source directory
     cp ${./../src/fs/xfstests/local.config} $out/xfstests/local.config
 
-    # copy skip.list into xfstests directory
-    cp ${./../src/fs/xfstests/skip.list} $out/xfstests/skip.list
+    # copy block.list into xfstests directory
+    cp ${./../src/fs/xfstests/block.list} $out/xfstests/block.list
 
-    # copy white.list into xfstests directory if it exists
-    ${lib.optionalString hasWhiteList ''
-      cp ${whiteListPath} $out/xfstests/white.list
-    ''}
+    # copy run.list into xfstests directory
+    cp ${./../src/fs/xfstests/run.list} $out/xfstests/run.list
+
   '';
 }
