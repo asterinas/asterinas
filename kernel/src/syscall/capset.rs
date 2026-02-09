4c09e076957fed36bd0d2094ec068c2eca465558
Ruihan Li <lrh2000@pku.edu.cn>
2026-02-09 23:58:37 +0800

    Fix `cap{get,set}` syscall behavior

--- kernel/src/syscall/capset.rs
+++ kernel/src/syscall/capset.rs
@@ -6,14 +6,11 @@ use super::SyscallReturn;
 use crate::{
     prelude::*,
     process::credentials::{
-        c_types::{LINUX_CAPABILITY_VERSION_3, cap_user_data_t, cap_user_header_t},
+        BOUNDING_CAPSET,
+        c_types::{CUserCapData, CUserCapHeader, LINUX_CAPABILITY_VERSION_3},
         capabilities::CapSet,
     },
 };
-
-fn make_kernel_cap(low: u32, high: u32) -> u64 {
-    ((low as u64) | ((high as u64) << 32)) & ((1u64 << (CapSet::most_significant_bit() + 1)) - 1)
-}
 
 pub fn sys_capset(
     cap_user_header_addr: Vaddr,
@@ -21,42 +18,58 @@ pub fn sys_capset(
     ctx: &Context,
 ) -> Result<SyscallReturn> {
     let user_space = ctx.user_space();
-    let cap_user_header: cap_user_header_t =
-        user_space.read_val::<cap_user_header_t>(cap_user_header_addr)?;
+    let cap_user_header = user_space.read_val::<CUserCapHeader>(cap_user_header_addr)?;
 
     if cap_user_header.version != LINUX_CAPABILITY_VERSION_3 {
-        return_errno_with_message!(Errno::EINVAL, "not supported (capability version is not 3)");
-    }
+        // Write the supported version back to userspace.
+        user_space.write_val(cap_user_header_addr, &LINUX_CAPABILITY_VERSION_3)?;
+        return_errno_with_message!(
+            Errno::EINVAL,
+            "capability versions other than v3 are not supported"
+        );
+    };
 
-    // The ability to set capabilities of any other process has been deprecated.
-    // See: https://elixir.bootlin.com/linux/v6.9.3/source/kernel/capability.c#L209 for more details.
+    // The ability to set capabilities of any other thread has been deprecated.
+    // Reference: The "With VFS capabilities support" section in
+    // <https://man7.org/linux/man-pages/man2/capset.2.html>.
     let header_pid = cap_user_header.pid;
-    if header_pid != 0 && header_pid != ctx.process.pid() {
-        return_errno_with_message!(Errno::EINVAL, "invalid pid");
+    if header_pid != 0 && header_pid != ctx.posix_thread.tid() {
+        return_errno_with_message!(
+            Errno::EPERM,
+            "setting other threads' capabilities is not allowed"
+        );
     }
 
-    // Check if the current process has CAP_SET_CAP capability
-    if !(ctx
-        .posix_thread
-        .credentials()
-        .permitted_capset()
-        .contains(CapSet::SETPCAP))
-    {
-        return_errno_with_message!(Errno::EPERM, "permission denied");
-    }
+    let caps_lo = user_space.read_val::<CUserCapData>(cap_user_data_addr)?;
+    let caps_hi =
+        user_space.read_val::<CUserCapData>(cap_user_data_addr + size_of::<CUserCapData>())?;
 
-    // Convert the cap(u32) to u64
-    let cap_user_data: cap_user_data_t =
-        user_space.read_val::<cap_user_data_t>(cap_user_data_addr)?;
-    let inheritable = make_kernel_cap(cap_user_data.inheritable, 0);
-    let permitted = make_kernel_cap(cap_user_data.permitted, 0);
-    let effective = make_kernel_cap(cap_user_data.effective, 0);
+    // Convert the 32-bit capability fields to the 64-bit capability value.
+    let permitted_capset = CapSet::from_lo_hi(caps_lo.permitted, caps_hi.permitted);
+    let effective_capset = CapSet::from_lo_hi(caps_lo.effective, caps_hi.effective);
+    let inheritable_capset = CapSet::from_lo_hi(caps_lo.inheritable, caps_hi.inheritable);
 
     let credentials = ctx.posix_thread.credentials_mut();
 
-    credentials.set_inheritable_capset(CapSet::from_bits_truncate(inheritable));
-    credentials.set_permitted_capset(CapSet::from_bits_truncate(permitted));
-    credentials.set_effective_capset(CapSet::from_bits_truncate(effective));
+    if !credentials.permitted_capset().contains(permitted_capset) {
+        return_errno_with_message!(Errno::EPERM, "adding permitted capabilities is not allowed");
+    }
+    if !permitted_capset.contains(effective_capset) {
+        return_errno_with_message!(Errno::EPERM, "effective capabilities are not permitted");
+    }
+    if !(credentials.inheritable_capset() | credentials.permitted_capset())
+        .contains(inheritable_capset)
+        && !credentials.effective_capset().contains(CapSet::SETPCAP)
+    {
+        return_errno_with_message!(Errno::EPERM, "inheritable capabilities are not permitted");
+    }
+    if !(credentials.inheritable_capset() | BOUNDING_CAPSET).contains(inheritable_capset) {
+        return_errno_with_message!(Errno::EPERM, "inheritable capabilities are not bounding");
+    }
+
+    credentials.set_permitted_capset(permitted_capset);
+    credentials.set_effective_capset(effective_capset);
+    credentials.set_inheritable_capset(inheritable_capset);
 
     Ok(SyscallReturn::Return(0))
 }
