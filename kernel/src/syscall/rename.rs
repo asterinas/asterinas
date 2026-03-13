bdb98bec6218a712ac808b2dcd9bccbc7be11e01
Xinyi Yu <1809327837@qq.com>
2026-03-13 07:01:48 +0000

    Fix rename subtree check for existing targets

--- kernel/src/syscall/rename.rs
+++ kernel/src/syscall/rename.rs
@@ -38,43 +38,37 @@ pub fn sys_renameat2(
     let path_resolver = fs_ref.resolver().read();
 
     let old_path_name = old_path_name.to_string_lossy();
-    let (old_dir_path, old_name) = {
+    let (old_parent_path, old_name) = {
         let (old_parent_path_name, old_name) = old_path_name.split_dirname_and_basename()?;
         let old_fs_path = FsPath::from_fd_and_path(old_dirfd, old_parent_path_name)?;
         (path_resolver.lookup(&old_fs_path)?, old_name)
     };
-    let old_path = path_resolver.lookup_at_path(&old_dir_path, old_name)?;
+    let old_path = path_resolver.lookup_at_path(&old_parent_path, old_name)?;
     if old_path.type_() != InodeType::Dir && old_path_name.ends_with('/') {
         return_errno_with_message!(Errno::ENOTDIR, "the old path is not a directory");
     }
 
     let new_path_name = new_path_name.to_string_lossy();
-    let (new_dir_path, new_name) = {
+    let (new_parent_path, new_name) = {
         if old_path.type_() != InodeType::Dir && new_path_name.ends_with('/') {
             return_errno_with_message!(Errno::EISDIR, "the new path is a directory");
         }
         let (new_parent_path_name, new_name) = new_path_name.split_dirname_and_basename()?;
-        let new_fs_path = FsPath::from_fd_and_path(new_dirfd, new_parent_path_name)?;
-        (path_resolver.lookup(&new_fs_path)?, new_name)
+        let new_parent_fs_path = FsPath::from_fd_and_path(new_dirfd, new_parent_path_name)?;
+        (
+            path_resolver.lookup(&new_parent_fs_path)?,
+            new_name.to_string(),
+        )
     };
 
-    // Check the absolute path
-    // FIXME: Using string prefix matching to check for path containment is incorrect.
-    // It doesn't handle path components like '..' or '.' properly.
-    let old_abs_path = path_resolver.make_abs_path(&old_path).into_string();
-    let new_abs_path = path_resolver.make_abs_path(&new_dir_path).into_string() + "/" + new_name;
-    if new_abs_path.starts_with(&old_abs_path) {
-        if new_abs_path.len() == old_abs_path.len() {
-            return Ok(SyscallReturn::Return(0));
-        } else {
-            return_errno_with_message!(
-                Errno::EINVAL,
-                "the new path contains a path prefix of the old path"
-            );
-        }
+    if old_path.type_() == InodeType::Dir && new_parent_path.is_equal_or_descendant_of(&old_path) {
+        return_errno_with_message!(
+            Errno::EINVAL,
+            "the new path is inside the old directory or its subtree"
+        );
     }
 
-    old_dir_path.rename(old_name, &new_dir_path, new_name)?;
+    old_parent_path.rename(old_name, &new_parent_path, &new_name)?;
 
     Ok(SyscallReturn::Return(0))
 }
