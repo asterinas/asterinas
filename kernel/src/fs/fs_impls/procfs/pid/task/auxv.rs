a78d180ffe4469fa1aee27fd6e5453f57709c18a
Wang Siyuan <wsy@stu.pku.edu.cn>
2026-03-30 08:11:31 +0000

    Add `/proc/pid/auxv`

---
+++ kernel/src/fs/fs_impls/procfs/pid/task/auxv.rs
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MPL-2.0
+
+use super::TidDirOps;
+use crate::{
+    fs::{
+        file::mkmod,
+        procfs::template::{FileOps, ProcFileBuilder},
+        vfs::inode::Inode,
+    },
+    prelude::*,
+    process::Process,
+};
+
+/// Represents the inode at `/proc/[pid]/task/[tid]/auxv` (and also `/proc/[pid]/auxv`).
+pub struct AuxvFileOps(Arc<Process>);
+
+impl AuxvFileOps {
+    pub fn new_inode(dir: &TidDirOps, parent: Weak<dyn Inode>) -> Arc<dyn Inode> {
+        let process_ref = dir.process_ref.clone();
+        // Reference: <https://elixir.bootlin.com/linux/v6.16.5/source/fs/proc/base.c#L3325>
+        ProcFileBuilder::new(Self(process_ref), mkmod!(u+r))
+            .parent(parent)
+            .build()
+            .unwrap()
+    }
+}
+
+impl FileOps for AuxvFileOps {
+    fn read_at(&self, offset: usize, writer: &mut VmWriter) -> Result<usize> {
+        let vmar_guard = self.0.lock_vmar();
+        let Some(init_stack_reader) = vmar_guard.init_stack_reader() else {
+            // According to Linux behavior, return an empty file
+            // if the process is a zombie process.
+            return Ok(0);
+        };
+
+        init_stack_reader.auxv(offset, writer)
+    }
+}
