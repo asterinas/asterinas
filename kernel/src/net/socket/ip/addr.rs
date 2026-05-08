705a895a99b6d89fb9176b5f9672973fcb349fd6
yyda <snowmantin@foxmail.com>
2026-05-08 16:33:28 +0800

    Add IPv6 address handling
    Add IPv6 address family and address utilities to support AF_INET6
    socket creation and IPv6 address manipulation.
    
    - Add IpAddressFamily::Ipv6 variant and From<IpAddress> conversion
    - Add Ipv6Address wrapper with conversion helpers
    - Add IpAddressFamily field to socket types for address family tracking

--- kernel/src/net/socket/ip/addr.rs
+++ kernel/src/net/socket/ip/addr.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use aster_bigtcp::wire::{IpAddress, IpEndpoint, Ipv4Address};
+use aster_bigtcp::wire::{IpAddress, IpEndpoint, Ipv4Address, Ipv6Address};
 
 use crate::{net::socket::util::SocketAddr, prelude::*};
 
@@ -10,6 +10,7 @@ impl TryFrom<SocketAddr> for IpEndpoint {
     fn try_from(value: SocketAddr) -> Result<Self> {
         match value {
             SocketAddr::IPv4(addr, port) => Ok(IpEndpoint::new(addr.into(), port)),
+            SocketAddr::IPv6(addr, port) => Ok(IpEndpoint::new(addr.into(), port)),
             _ => return_errno_with_message!(
                 Errno::EAFNOSUPPORT,
                 "the address is in an unsupported address family"
@@ -23,7 +24,7 @@ impl From<IpEndpoint> for SocketAddr {
         let port = endpoint.port;
         match endpoint.addr {
             IpAddress::Ipv4(addr) => SocketAddr::IPv4(addr, port),
-            // TODO: support IPv6
+            IpAddress::Ipv6(addr) => SocketAddr::IPv6(addr, port),
         }
     }
 }
@@ -35,3 +36,36 @@ impl From<IpEndpoint> for SocketAddr {
 /// unspecified endpoint helps with that.
 pub(super) const UNSPECIFIED_LOCAL_ENDPOINT: IpEndpoint =
     IpEndpoint::new(IpAddress::Ipv4(Ipv4Address::UNSPECIFIED), 0);
+
+/// IPv6 unspecified local endpoint.
+pub(super) const UNSPECIFIED_LOCAL_ENDPOINT_V6: IpEndpoint =
+    IpEndpoint::new(IpAddress::Ipv6(Ipv6Address::UNSPECIFIED), 0);
+
+/// Address family for IP sockets.
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum IpAddressFamily {
+    IPv4,
+    IPv6,
+}
+
+impl IpAddressFamily {
+    /// Returns the unspecified endpoint for this address family.
+    pub const fn unspecified_endpoint(&self) -> IpEndpoint {
+        match self {
+            IpAddressFamily::IPv4 => UNSPECIFIED_LOCAL_ENDPOINT,
+            IpAddressFamily::IPv6 => UNSPECIFIED_LOCAL_ENDPOINT_V6,
+        }
+    }
+}
+
+// Note: This does not handle IPv4-mapped IPv6 addresses. When `IPV6_V6ONLY` is set,
+// IPv4-mapped addresses are not permitted, so this function cannot be used to determine
+// whether such an address is acceptable.
+impl From<IpAddress> for IpAddressFamily {
+    fn from(addr: IpAddress) -> Self {
+        match addr {
+            IpAddress::Ipv4(_) => IpAddressFamily::IPv4,
+            IpAddress::Ipv6(_) => IpAddressFamily::IPv6,
+        }
+    }
+}
