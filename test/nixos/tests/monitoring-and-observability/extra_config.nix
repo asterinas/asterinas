0343550a27057eef4b53ad0cdd8ca2d3043a6968
Qingsong Chen <changxian.cqs@antgroup.com>
2026-05-12 02:32:33 +0000

    Add test for monitoring and observability applications

---
+++ test/nixos/tests/monitoring-and-observability/extra_config.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+let
+  prometheus_yml = pkgs.writeTextFile {
+    name = "prometheus.yml";
+    text = ''
+      global:
+        scrape_interval: 15s
+      scrape_configs:
+        - job_name: "prometheus"
+          static_configs:
+            - targets: [ "10.0.2.15:9090" ]
+    '';
+  };
+in {
+  environment.systemPackages = with pkgs; [ prometheus ];
+
+  system.activationScripts.testFixtures = ''
+    ln -sfT ${prometheus_yml} /tmp/prometheus.yml
+  '';
+}
