2f0d0a77cdb859be1e6cbb03d002bbc28fe3e58a
Qingsong Chen <changxian.cqs@antgroup.com>
2026-05-12 03:51:08 +0000

    Add test for AI and machine learning applications

---
+++ test/nixos/tests/ai-and-machine-learning/extra_config.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+let
+  test_pytorch = pkgs.writeTextFile {
+    name = "test_pytorch.py";
+    text = builtins.readFile
+      ../../../../book/src/distro/popular-applications/ai-and-machine-learning/test_pytorch.py;
+  };
+  test_tensorflow = pkgs.writeTextFile {
+    name = "test_tensorflow.py";
+    text = builtins.readFile
+      ../../../../book/src/distro/popular-applications/ai-and-machine-learning/test_tensorflow.py;
+  };
+in {
+  environment.systemPackages = with pkgs; [
+    (python3.withPackages (p: with p; [ torch tensorflow pytest ]))
+    ollama
+  ];
+
+  system.activationScripts.testFixtures = ''
+    ln -sfT ${test_pytorch} /tmp/test_pytorch.py
+    ln -sfT ${test_tensorflow} /tmp/test_tensorflow.py
+  '';
+}
