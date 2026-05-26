af8dc804c7a1752cd4f7ca768d5e560e78068e72
Jianfeng Jiang <jiangjianfeng.jjf@antgroup.com>
2026-05-26 11:03:35 +0000

    Fix proc comm to use thread names

--- test/initramfs/src/regression/process/execve/execve_comm_child.c
+++ test/initramfs/src/regression/process/execve/execve_comm_child.c
@@ -9,6 +9,22 @@
 
 #define EXPECTED_COMM_ENV "EXPECTED_COMM"
 
+static int check_proc_comm(const char *expected_comm)
+{
+	char expected_comm_with_newline[64];
+	char comm[64];
+	FILE *file;
+
+	snprintf(expected_comm_with_newline, sizeof(expected_comm_with_newline),
+		 "%s\n", expected_comm);
+
+	file = CHECK_WITH(fopen("/proc/self/comm", "r"), _ret != NULL);
+	CHECK_WITH(fgets(comm, sizeof(comm), file), _ret != NULL);
+	CHECK_WITH(fclose(file), _ret == 0);
+
+	return strcmp(comm, expected_comm_with_newline) != 0;
+}
+
 static int check_proc_stat(const char *expected_comm)
 {
 	char expected_prefix[64];
@@ -32,6 +48,7 @@ FN_TEST(exec_comm)
 	const char *expected_comm =
 		CHECK_WITH(getenv(EXPECTED_COMM_ENV), _ret != NULL);
 
+	TEST_RES(check_proc_comm(expected_comm), _ret == 0);
 	TEST_RES(check_proc_stat(expected_comm), _ret == 0);
 }
 END_TEST()
