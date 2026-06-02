e40d72a99aa4dd51c44fea695058cfd93a4dc92c
Xinyi Yu <1809327837@qq.com>
2026-06-02 03:54:16 +0000

    Integrate `tmpfs` into CI

--- test/initramfs/src/conformance/xfstests/template/config/build_config.mk
+++ test/initramfs/src/conformance/xfstests/template/config/build_config.mk
@@ -1,4 +1,6 @@
 # SPDX-License-Identifier: MPL-2.0
 
+# Whether block device images need to be created for test and scratch partitions.
 XFSTESTS_NEEDS_BLOCK_DEVICES := false
+# The mkfs command used to format block device images (empty if not applicable).
 XFSTESTS_MKFS :=
