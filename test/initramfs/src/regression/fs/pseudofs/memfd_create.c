91d52bef76ba3c76a6315540a95fda0c23f74fe3
li041 <lxh050013@outlook.com>
2026-06-29 05:03:57 +0000

    Test memfd fallocate seal checks

--- test/initramfs/src/regression/fs/pseudofs/memfd_create.c
+++ test/initramfs/src/regression/fs/pseudofs/memfd_create.c
@@ -2,11 +2,20 @@
 
 #define _GNU_SOURCE
 
+#include <fcntl.h>
 #include <string.h>
 #include <sys/mman.h>
 #include <unistd.h>
 
 #include "../../common/test.h"
+
+#ifndef FALLOC_FL_KEEP_SIZE
+#define FALLOC_FL_KEEP_SIZE 0x01
+#endif
+
+#ifndef FALLOC_FL_PUNCH_HOLE
+#define FALLOC_FL_PUNCH_HOLE 0x02
+#endif
 
 FN_TEST(name_too_long)
 {
@@ -20,6 +29,29 @@ FN_TEST(name_too_long)
 
 	name[249] = '\0';
 	fd = TEST_SUCC(memfd_create(name, MFD_CLOEXEC));
+	TEST_SUCC(close(fd));
+}
+END_TEST()
+
+FN_TEST(fallocate_seals)
+{
+	int fd;
+
+	fd = TEST_SUCC(memfd_create("test_memfd", MFD_ALLOW_SEALING));
+	TEST_SUCC(ftruncate(fd, 4096));
+	TEST_SUCC(fcntl(fd, F_ADD_SEALS, F_SEAL_WRITE));
+	TEST_SUCC(fallocate(fd, 0, 0, 4096));
+	TEST_ERRNO(fallocate(fd, FALLOC_FL_KEEP_SIZE | FALLOC_FL_PUNCH_HOLE, 0,
+			     4096),
+		   EPERM);
+	TEST_SUCC(close(fd));
+
+	fd = TEST_SUCC(memfd_create("test_memfd", MFD_ALLOW_SEALING));
+	TEST_SUCC(ftruncate(fd, 4096));
+	TEST_SUCC(fcntl(fd, F_ADD_SEALS, F_SEAL_GROW));
+	TEST_SUCC(fallocate(fd, 0, 0, 4096));
+	TEST_ERRNO(fallocate(fd, 0, 0, 8192), EPERM);
+	TEST_ERRNO(fallocate(fd, FALLOC_FL_KEEP_SIZE, 0, 8192), EPERM);
 	TEST_SUCC(close(fd));
 }
 END_TEST()
