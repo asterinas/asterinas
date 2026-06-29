94379b8da3668e17c75180f07799b17a49549a71
co63oc <4617245+co63oc@users.noreply.github.com>
2026-06-29 10:28:09 +0800

    Fix StatfsTest.InternalDevShm

--- kernel/src/fs/fs_impls/ramfs/fs.rs
+++ kernel/src/fs/fs_impls/ramfs/fs.rs
@@ -66,7 +66,7 @@ impl RamFs {
 
     // TODO: Remove this tmpfs-specific constructor once `TmpFs` no longer
     // aliases `RamFs`.
-    pub(in crate::fs) fn new_tmpfs() -> Arc<Self> {
+    pub fn new_tmpfs() -> Arc<Self> {
         let anon_device_id = AnonDeviceId::acquire().expect("no device ID is available for tmpfs");
         let sb = {
             let mut super_block =
