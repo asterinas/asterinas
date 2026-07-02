003568118b12dd6e2825e3f3b2188a6b60dd2bab
WaterWhisperer <waterwhisperer24@qq.com>
2026-07-02 19:43:45 +0800

    Fix chroot capability check

--- kernel/src/syscall/chroot.rs
+++ kernel/src/syscall/chroot.rs
@@ -4,6 +4,8 @@ use super::SyscallReturn;
 use crate::{
     fs::{file::InodeType, vfs::path::FsPath},
     prelude::*,
+    process::credentials::capabilities::CapSet,
+    security::lsm::hooks as lsm_hooks,
     syscall::constants::MAX_FILENAME_LEN,
 };
 
@@ -25,6 +27,13 @@ pub fn sys_chroot(path_ptr: Vaddr, ctx: &Context) -> Result<SyscallReturn> {
     if path.type_() != InodeType::Dir {
         return_errno_with_message!(Errno::ENOTDIR, "must be directory");
     }
+
+    lsm_hooks::on_capable(lsm_hooks::CapableContext::new(
+        ctx.thread_local.borrow_user_ns().as_ref(),
+        ctx.posix_thread,
+        CapSet::SYS_CHROOT,
+    ))?;
+
     path_resolver.set_root(path);
     Ok(SyscallReturn::Return(0))
 }
