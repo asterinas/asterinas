3bfb3626eb0e0de36946782e9777390d66e69735
Ruihan Li <lrh2000@pku.edu.cn>
2026-07-03 13:00:22 +0800

    Support virtio-mmio bus in AArch64

--- kernel/core/comps/virtio/src/transport/mmio/bus/arch/arm.rs
+++ kernel/core/comps/virtio/src/transport/mmio/bus/arch/arm.rs
@@ -1,14 +1,38 @@
 // SPDX-License-Identifier: MPL-2.0
 
-// TODO: Add `MappedIrqLine` support for ARM.
-pub(super) use ostd::irq::IrqLine as MappedIrqLine;
+use fdt_util::{AcquireIoMems, AcquireIrqLines};
+use ostd::arch::boot::DEVICE_TREE;
+pub(super) use ostd::arch::irq::MappedIrqLine;
 
-use crate::transport::mmio::bus::{MmioBus, common_device::MmioCommonDevice};
+use crate::transport::mmio::{
+    MMIO_BUS, bus::common_device::MmioCommonDevice, layout::VirtioMmioLayout,
+};
 
 pub(super) fn probe_for_device() {
-    // TODO: Probe virtio devices on the MMIO bus in ARM.
-    // The following methods will be used to probe the device.
-    let _ = super::validate_mmio_device;
-    let _ = MmioCommonDevice::new;
-    let _ = MmioBus::register_mmio_device;
+    // The device tree parsing logic here assumed a Linux-compatible device
+    // tree.
+    // Reference: <https://www.kernel.org/doc/Documentation/devicetree/bindings/virtio/mmio.txt>.
+    let device_tree = DEVICE_TREE.get().unwrap();
+    let mmio_nodes = device_tree.all_nodes().filter(|node| {
+        node.compatible().is_some_and(|compatibles| {
+            compatibles
+                .all()
+                .any(|compatible| compatible == "virtio,mmio")
+        })
+    });
+    mmio_nodes.for_each(|node| {
+        let Some([io_mem]) = node.acquire_io_mems([size_of::<VirtioMmioLayout>()]) else {
+            return;
+        };
+        if super::validate_mmio_device(&io_mem).is_err() {
+            return;
+        }
+
+        let Some([irq_line]) = node.acquire_irq_lines() else {
+            return;
+        };
+
+        let device = MmioCommonDevice::new(io_mem, irq_line);
+        MMIO_BUS.lock().register_mmio_device(device);
+    });
 }
