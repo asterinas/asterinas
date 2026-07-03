0ef8e3d597a02a4094e0e8fd6fe568fa481d00cc
Bet4 <0xbet4@gmail.com>
2026-07-03 11:13:28 +0800

    Update LTP to 20260529
    The old runltp script has been replaced with kirk. So we install and use kirk instead.

--- test/initramfs/nix/conformance/ltp.nix
+++ test/initramfs/nix/conformance/ltp.nix
@@ -1,12 +1,22 @@
-{ stdenv, fetchFromGitHub, hostPlatform, libcap, pkgsBuildBuild, }:
+{ stdenv, fetchFromGitHub, hostPlatform, libcap, pkgsBuildBuild, python3Minimal,
+}:
 stdenv.mkDerivation rec {
   pname = "ltp";
-  version = "20250930";
+  version = "20260529";
   src = fetchFromGitHub {
     owner = "linux-test-project";
     repo = "ltp";
     rev = "${version}";
-    hash = "sha256-vmsC4QRM4U1MoRjLbRsodX4jAolWeifaP9zetwIbWl4";
+    hash = "sha256-h4cIK0sDbyGiGwvba3jkJ+W28oNzvQAfGu1RbGAFljA=";
+  };
+  # Kirk is the official LTP tests executor.
+  kirkSrc = fetchFromGitHub {
+    owner = "linux-test-project";
+    repo = "kirk";
+    # Pinned to version v4.1.0, which is the exact version of the kirk
+    # submodule specified by the LTP 20260529 release.
+    rev = "v4.1.0";
+    hash = "sha256-W/6sxdqNACs0yI+g8BLYFdqPzJXA1VHYbUN/YL7kuJE=";
   };
 
   # Clear `CFLAGS` and `DEBUG_CFLAGS` to prevent `-g` from being automatically added.
@@ -37,7 +47,6 @@ stdenv.mkDerivation rec {
     make -C testcases/kernel
     make -C testcases/lib
     make -C runtest
-    make -C pan
 
     runHook postBuild
   '';
@@ -47,9 +56,11 @@ stdenv.mkDerivation rec {
     make -C testcases/kernel install
     make -C testcases/lib install
     make -C runtest install
-    make -C pan install
 
-    install -m 00755 $src/runltp $out/runltp
+    cp -r ${kirkSrc}/libkirk $out/libkirk
+    install -m 00755 ${kirkSrc}/kirk $out/kirk
+    substituteInPlace $out/kirk \
+      --replace-fail '#!/usr/bin/env python3' '#!${python3Minimal}/bin/python3'
     install -m 00444 $src/VERSION $out/Version
     install -m 00755 $src/ver_linux $out/ver_linux
     install -m 00755 $src/IDcheck.sh $out/IDcheck.sh
