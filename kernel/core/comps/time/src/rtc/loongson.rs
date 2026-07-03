686eff7ad6c1e55050553a5a1814e0ea824e27b3
Ruihan Li <lrh2000@pku.edu.cn>
2026-07-04 00:03:28 +0800

    Refactor RTC code with `fdt-util`

--- kernel/core/comps/time/src/rtc/loongson.rs
+++ kernel/core/comps/time/src/rtc/loongson.rs
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
 
+use fdt_util::AcquireIoMems;
 use ostd::{arch::boot::DEVICE_TREE, io::IoMem, mm::VmIoOnce};
 
 use crate::{SystemTime, rtc::Driver};
@@ -10,35 +11,23 @@ pub struct RtcLoongson {
 
 impl Driver for RtcLoongson {
     fn try_new() -> Option<Self> {
-        let chosen = DEVICE_TREE.get().unwrap().find_node("/rtc").unwrap();
-        if let Some(compatible) = chosen.compatible()
-            && compatible.all().any(|c| c == "loongson,ls7a-rtc")
-        {
-            let region = chosen.reg().unwrap().next().unwrap();
-            let io_mem = IoMem::acquire(
-                region.starting_address as usize
-                    ..region.starting_address as usize + region.size.unwrap(),
-            )
-            .unwrap();
+        const FDT_COMPATIBLE: &str = "loongson,ls7a-rtc";
 
-            const SYS_RTCCTRL: usize = 0x40;
-            const SYS_TOYTRIM: usize = 0x20;
-            // Initialize the RTC unit
-            // Reference: <https://loongson.github.io/LoongArch-Documentation/Loongson-7A1000-usermanual-EN.html#rtc>
-            io_mem.write_once(SYS_TOYTRIM, &0x0u32).unwrap();
-            io_mem.write_once(SYS_RTCCTRL, &0x2900u32).unwrap();
+        let [io_mem] = DEVICE_TREE
+            .get()
+            .unwrap()
+            .find_compatible(&[FDT_COMPATIBLE])?
+            .acquire_io_mems([SYS_MAX])?;
 
-            Some(Self { io_mem })
-        } else {
-            None
-        }
+        // Initialize the RTC unit
+        // Reference: <https://loongson.github.io/LoongArch-Documentation/Loongson-7A1000-usermanual-EN.html#rtc>
+        io_mem.write_once(SYS_TOYTRIM, &0x0u32).unwrap();
+        io_mem.write_once(SYS_RTCCTRL, &0x2900u32).unwrap();
+
+        Some(Self { io_mem })
     }
 
     fn read_rtc(&self) -> SystemTime {
-        const SYS_TOYREAD0: usize = 0x2c;
-        const SYS_TOYREAD1: usize = 0x30;
-        const SYS_RTCREAD: usize = 0x68;
-
         // Read the Time of Year (TOY) counter and the RTC timer counter
         // Reference: <https://loongson.github.io/LoongArch-Documentation/Loongson-7A1000-usermanual-EN.html#rtc>
         let sys_toyread0: u32 = self.io_mem.read_once(SYS_TOYREAD0).unwrap();
@@ -64,3 +53,12 @@ impl Driver for RtcLoongson {
         }
     }
 }
+
+const SYS_TOYTRIM: usize = 0x20;
+const SYS_TOYREAD0: usize = 0x2c;
+const SYS_TOYREAD1: usize = 0x30;
+
+const SYS_RTCCTRL: usize = 0x40;
+const SYS_RTCREAD: usize = 0x68;
+
+const SYS_MAX: usize = 0x78;
