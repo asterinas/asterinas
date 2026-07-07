162bc5eb746303807be800155766ea5d669a6391
Bet4 <0xbet4@gmail.com>
2026-07-07 20:04:31 +0800

    Support TCGETS2 and TCSETS2 TTY and PTY ioctls
    Support TCGETS2 and TCSETS2 by adding the Linux termios2 ABI layout and storing CTermios2 directly in the TTY line discipline.
    
    This lets userspace that prefers termios2, such as systemd getty, read and update terminal settings without changing the existing TCSETSW/TCSETSF behavior.
    
    Document the newly supported ioctl commands in the SCML coverage file and enable the corresponding gVisor TermiosFault conformance test.

--- kernel/src/device/tty/ioctl_defs.rs
+++ kernel/src/device/tty/ioctl_defs.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use super::termio::{CTermios, CWinSize};
+use super::termio::{CTermios, CTermios2, CWinSize};
 use crate::util::ioctl::{InData, OutData, ioc};
 
 // Reference: <https://elixir.bootlin.com/linux/v6.18/source/include/uapi/asm-generic/ioctls.h>
@@ -9,6 +9,9 @@ pub type GetTermios      = ioc!(TCGETS,     0x5401,     OutData<CTermios>);
 pub type SetTermios      = ioc!(TCSETS,     0x5402,     InData<CTermios>);
 pub type SetTermiosWait  = ioc!(TCSETSW,    0x5403,     InData<CTermios>);
 pub type SetTermiosFlush = ioc!(TCSETSF,    0x5404,     InData<CTermios>);
+
+pub type GetTermios2     = ioc!(TCGETS2,    b'T', 0x2A, OutData<CTermios2>);
+pub type SetTermios2     = ioc!(TCSETS2,    b'T', 0x2B, InData<CTermios2>);
 
 pub type GetWinSize      = ioc!(TIOCGWINSZ, 0x5413,     OutData<CWinSize>);
 pub type SetWinSize      = ioc!(TIOCSWINSZ, 0x5414,     InData<CWinSize>);
