171b26bffaa9089b43819c94c96adb273b9288e1
Ray Lee <hburaylee@gmail.com>
2026-07-20 11:57:12 +0800

    Rename qemu-direct boot method to vmm-direct
    The `qemu-direct` name implies the boot method is tied to QEMU, but the
    kernel image is in fact supplied directly to the VMM that runs the guest.
    Rename it to `vmm-direct` so the name describes how the image is loaded
    rather than which VMM is used.
    
    Signed-off-by: Ray Lee <hburaylee@gmail.com>

--- osdk/src/config/scheme/boot.rs
+++ osdk/src/config/scheme/boot.rs
@@ -25,10 +25,10 @@ pub enum BootMethod {
     GrubRescueIso,
     /// Boot the kernel by making a Qcow2 image with Grub as the bootloader.
     GrubQcow2,
-    /// Use the [QEMU direct boot](https://qemu-project.gitlab.io/qemu/system/linuxboot.html)
-    /// to boot the kernel with QEMU's built-in Seabios and Coreboot utilities.
+    /// Boot the kernel by supplying the kernel image directly to the VMM,
+    /// without a GRUB boot device.
     #[default]
-    QemuDirect,
+    VmmDirect,
 }
 
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
@@ -65,7 +65,7 @@ impl BootScheme {
         Boot {
             kcmdline,
             initramfs: self.initramfs,
-            method: self.method.unwrap_or(BootMethod::QemuDirect),
+            method: self.method.unwrap_or(BootMethod::VmmDirect),
         }
     }
 }
