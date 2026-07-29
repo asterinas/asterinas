8ab3b220c695db93533801876bd67198b866dea4
Tao Su <st498824@antgroup.com>
2026-07-29 05:44:25 +0000

    Honor open flags for PTY peers

--- kernel/src/util/ioctl/mod.rs
+++ kernel/src/util/ioctl/mod.rs
@@ -309,8 +309,8 @@ macro_rules! impl_get_by_val_for {
     };
 }
 
-// We can add more types as needed, e.g., `u32`, `i8`.
-impl_get_by_val_for! { i32 }
+// We can add more types as needed, e.g., `i8`.
+impl_get_by_val_for! { i32 u32 }
 
 impl DataSpec for InData<[u8]> {
     const SIZE: Option<u16> = None;
