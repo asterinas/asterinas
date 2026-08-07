e3b4a12d7e34c26a0988c5bb4e6c7b921f76e132
FallingLeavez <77148269+FallingLeavez@users.noreply.github.com>
2026-08-07 22:32:02 +0800

    Refactor inter-processor calls to return pending IPIs

--- ostd/src/smp.rs
+++ ostd/src/smp.rs
@@ -4,8 +4,13 @@
 //!
 //! This module provides a way to execute code on other processors via inter-
 //! processor interrupts.
+//!
+//! Callers issue work with [`inter_processor_call`]. Remote calls are queued for
+//! interrupt-context execution and return a [`PendingIpis`] handle that can be
+//! waited on when the caller needs completion.
 
 use alloc::{boxed::Box, collections::VecDeque};
+use core::sync::atomic::{AtomicBool, Ordering};
 
 use spin::Once;
 
@@ -19,21 +24,73 @@ use crate::{
 
 /// Executes a function on other processors.
 ///
-/// The provided function `f` will be executed on all target processors
+/// The provided function `call_fn` will be executed on all target processors
 /// specified by `targets`. It can also be executed on the current processor.
 /// The function should be short and non-blocking, as it will be executed in
 /// interrupt context with interrupts disabled.
 ///
-/// This function does not block until all the target processors acknowledges
-/// the interrupt. So if any of the target processors disables IRQs for too
-/// long that the controller cannot queue them, the function will not be
-/// executed.
+/// The function `call_fn` will be executed asynchronously on the target
+/// processors. However, if called on the current processor, it will be
+/// synchronous.
 ///
-/// The function `f` will be executed asynchronously on the target processors.
-/// However if called on the current processor, it will be synchronous.
-pub fn inter_processor_call(targets: &CpuSet, f: fn()) {
+/// The returned [`PendingIpis`] can be used to wait until all remote target
+/// processors have handled IPIs for this call.
+///
+/// # Panics
+///
+/// This function will panic if a hardware error occurs while sending an IPI to
+/// a remote processor.
+pub fn inter_processor_call(targets: &CpuSet, call_fn: fn()) -> PendingIpis {
     let ipi_sender = IPI_SENDER.get().unwrap();
-    ipi_sender.inter_processor_call(targets, f);
+    ipi_sender.inter_processor_call(targets, call_fn)
+}
+
+/// Pending remote inter-processor calls.
+pub struct PendingIpis {
+    cpus: CpuSet,
+}
+
+impl PendingIpis {
+    pub(crate) fn new_empty() -> Self {
+        Self {
+            cpus: CpuSet::new_empty(),
+        }
+    }
+
+    fn add(&mut self, cpu_id: crate::cpu::CpuId) {
+        self.cpus.add(cpu_id);
+    }
+
+    pub(crate) fn extend(&mut self, other: &Self) {
+        for cpu_id in other.cpus.iter() {
+            self.add(cpu_id);
+        }
+    }
+
+    /// Waits until all pending remote processors have handled their IPIs.
+    ///
+    /// # Panics
+    ///
+    /// This method panics if local IRQs are disabled. Waiting for remote IPIs
+    /// with local IRQs disabled can deadlock if one of the remote processors is
+    /// also waiting for this processor to handle an IPI.
+    pub fn wait(&self) {
+        assert!(
+            crate::arch::irq::is_local_enabled(),
+            "waiting for remote inter-processor calls with IRQs disabled"
+        );
+
+        for cpu_id in self.cpus.iter() {
+            // Wait until there are no pending IPIs on the target CPU.
+            //
+            // Note that if new IPIs arrive to that CPU in the meantime, we
+            // will also wait for them. This is fine because there usually
+            // aren't too many IPIs in common cases.
+            while HAS_PENDING_IPIS.get_on_cpu(cpu_id).load(Ordering::Acquire) {
+                core::hint::spin_loop();
+            }
+        }
+    }
 }
 
 /// A sender that carries necessary information to send inter-processor interrupts.
@@ -53,34 +110,44 @@ impl IpiSender {
     /// See [`inter_processor_call`] for details. The purpose of exporting this
     /// method is to enable callers to check whether [`IPI_SENDER`] has been
     /// initialized.
-    pub(crate) fn inter_processor_call(&self, targets: &CpuSet, f: fn()) {
+    pub(crate) fn inter_processor_call(&self, targets: &CpuSet, call_fn: fn()) -> PendingIpis {
         let irq_guard = irq::disable_local();
         let this_cpu_id = irq_guard.current_cpu();
 
         let mut call_on_self = false;
+        let mut pending_ipis = PendingIpis::new_empty();
         for cpu_id in targets.iter() {
             if cpu_id == this_cpu_id {
                 call_on_self = true;
                 continue;
             }
-            CALL_QUEUES.get_on_cpu(cpu_id).lock().push_back(f);
+            let mut call_queue = CALL_QUEUES.get_on_cpu(cpu_id).lock();
+            call_queue.push_back(call_fn);
+            // Set the pending flag before dropping the lock to avoid races.
+            HAS_PENDING_IPIS
+                .get_on_cpu(cpu_id)
+                .store(true, Ordering::Release);
+            pending_ipis.add(cpu_id);
         }
         for cpu_id in targets.iter() {
             if cpu_id == this_cpu_id {
                 continue;
             }
             let hw_cpu_id = self.hw_cpu_ids[cpu_id.as_usize()];
-            crate::arch::irq::send_ipi(hw_cpu_id, &irq_guard as _);
+            crate::arch::irq::send_ipi(hw_cpu_id, &irq_guard as _)
+                .expect("failed to send inter-processor interrupt");
         }
         if call_on_self {
             // Execute the function synchronously.
-            f();
+            call_fn();
         }
+        pending_ipis
     }
 }
 
 cpu_local! {
     static CALL_QUEUES: SpinLock<VecDeque<fn()>> = SpinLock::new(VecDeque::new());
+    static HAS_PENDING_IPIS: AtomicBool = AtomicBool::new(false);
 }
 
 /// Handles inter-processor calls.
@@ -94,14 +161,18 @@ pub(crate) unsafe fn do_inter_processor_call(_trapframe: &TrapFrame) {
     let this_cpu_id = crate::cpu::CpuId::current_racy();
 
     let mut queue = CALL_QUEUES.get_on_cpu(this_cpu_id).lock();
-    while let Some(f) = queue.pop_front() {
+    while let Some(call_fn) = queue.pop_front() {
         crate::debug!(
             "Performing inter-processor call to {:#?} on CPU {:#?}",
-            f,
+            call_fn,
             this_cpu_id,
         );
-        f();
+        call_fn();
     }
+    // Clear the pending flag before dropping the lock to avoid races.
+    HAS_PENDING_IPIS
+        .get_on_cpu(this_cpu_id)
+        .store(false, Ordering::Release);
 }
 
 pub(super) fn init() {
@@ -109,4 +180,85 @@ pub(super) fn init() {
         let hw_cpu_ids = crate::boot::smp::construct_hw_cpu_id_mapping();
         IpiSender { hw_cpu_ids }
     });
+}
+
+#[cfg(ktest)]
+mod test {
+    use core::sync::atomic::{AtomicUsize, Ordering};
+
+    use crate::{
+        cpu::{self, PinCurrentCpu},
+        prelude::ktest,
+        task,
+    };
+
+    static IPI_CALL_COUNT: AtomicUsize = AtomicUsize::new(0);
+
+    fn count_ipi_call() {
+        IPI_CALL_COUNT.fetch_add(1, Ordering::Relaxed);
+    }
+
+    #[ktest]
+    fn pending_ipis_waits_for_all_cpus() {
+        let before = IPI_CALL_COUNT.load(Ordering::Relaxed);
+
+        super::inter_processor_call(&cpu::CpuSet::new_full(), count_ipi_call).wait();
+
+        assert_eq!(
+            IPI_CALL_COUNT.load(Ordering::Relaxed) - before,
+            cpu::num_cpus()
+        );
+    }
+
+    #[ktest]
+    fn inter_processor_call_runs_on_current_cpu() {
+        let preempt_guard = task::disable_preempt();
+        let before = IPI_CALL_COUNT.load(Ordering::Relaxed);
+
+        super::inter_processor_call(
+            &cpu::CpuSet::from(preempt_guard.current_cpu()),
+            count_ipi_call,
+        )
+        .wait();
+
+        assert_eq!(IPI_CALL_COUNT.load(Ordering::Relaxed) - before, 1);
+    }
+
+    #[ktest]
+    fn pending_ipis_waits_for_remote_cpu() {
+        if cpu::num_cpus() < 2 {
+            return;
+        }
+
+        let preempt_guard = task::disable_preempt();
+        let target_cpu = cpu::all_cpus()
+            .find(|cpu_id| *cpu_id != preempt_guard.current_cpu())
+            .unwrap();
+        let before = IPI_CALL_COUNT.load(Ordering::Relaxed);
+
+        super::inter_processor_call(&cpu::CpuSet::from(target_cpu), count_ipi_call).wait();
+
+        assert_eq!(IPI_CALL_COUNT.load(Ordering::Relaxed) - before, 1);
+    }
+
+    #[ktest]
+    fn pending_ipis_can_be_extended() {
+        if cpu::num_cpus() < 2 {
+            return;
+        }
+
+        let preempt_guard = task::disable_preempt();
+        let target_cpu = cpu::all_cpus()
+            .find(|cpu_id| *cpu_id != preempt_guard.current_cpu())
+            .unwrap();
+        let target_cpus = cpu::CpuSet::from(target_cpu);
+        let before = IPI_CALL_COUNT.load(Ordering::Relaxed);
+
+        let mut pending_ipis = super::PendingIpis::new_empty();
+        pending_ipis.extend(&super::inter_processor_call(&target_cpus, count_ipi_call));
+        pending_ipis.extend(&super::inter_processor_call(&target_cpus, count_ipi_call));
+        pending_ipis.wait();
+
+        assert_eq!(IPI_CALL_COUNT.load(Ordering::Relaxed) - before, 2);
+    }
 }
