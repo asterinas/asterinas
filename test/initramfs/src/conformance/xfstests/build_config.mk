e01871f0c9fd73052a77cc3009c5d608861cff25
li041 <lxh050013@outlook.com>
2026-08-17 10:56:23 +0000

    Integrate virtio-fs into `xfstests` runs

--- test/initramfs/src/conformance/xfstests/build_config.mk
+++ test/initramfs/src/conformance/xfstests/build_config.mk
@@ -3,5 +3,7 @@
 ifeq ($(ENABLE_CONFORMANCE_TEST), true)
 ifeq ($(CONFORMANCE_TEST_SUITE), xfstests)
 include $(dir $(lastword $(MAKEFILE_LIST)))$(XFSTESTS_FS_TYPE)/config/build_config.mk
+XFSTESTS_TEST_DEV ?= /dev/vdd
+XFSTESTS_SCRATCH_DEV ?= /dev/vde
 endif
 endif
