3ce05e9a48cbcb3d1254e92bde1c910ae5f13449
Chen Chengjun <chenchengjun.ccj@antgroup.com>
2026-09-02 02:21:23 +0000

    Introduce an atomic symlink creation API

--- kernel/core/src/fs/vfs/fs_apis/inode.rs
+++ kernel/core/src/fs/vfs/fs_apis/inode.rs
@@ -420,6 +420,10 @@ pub(crate) trait Inode: Any + FileOps + Send + Sync {
         Err(Error::new(Errno::ENOTDIR))
     }
 
+    fn create_symlink(&self, name: &str, target: &str, mode: InodeMode) -> Result<Arc<dyn Inode>> {
+        Err(Error::new(Errno::EOPNOTSUPP))
+    }
+
     fn create_tmpfile(
         &self,
         mode: InodeMode,
@@ -469,10 +473,6 @@ pub(crate) trait Inode: Any + FileOps + Send + Sync {
     }
 
     fn read_link(&self) -> Result<SymbolicLink> {
-        Err(Error::new(Errno::EISDIR))
-    }
-
-    fn write_link(&self, target: &str) -> Result<()> {
         Err(Error::new(Errno::EISDIR))
     }
 
