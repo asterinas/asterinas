f68c11d0f27fef9449320380bfd5dc9a16e6a079
li041 <lxh050013@outlook.com>
2026-09-02 02:13:17 +0000

    Add cached ext2 fio benchmarks

--- test/initramfs/src/benchmark/fio/ext2_seq_read_bw_no_iommu/run.sh
+++ test/initramfs/src/benchmark/fio/ext2_seq_read_bw_no_iommu/run.sh
@@ -4,9 +4,9 @@
 
 set -e
 
-echo "*** Running the FIO sequential read test (Ext2) ***"
+echo "*** Running the FIO buffered sequential read test (Ext2) ***"
 
 /benchmark/bin/fio -rw=read -filename=/ext2/fio-test -name=seqread \
--size=1G -bs=1M \
--ioengine=sync -direct=1 -numjobs=1 -fsync_on_close=1 \
--time_based=1 -ramp_time=60 -runtime=100
+    -size=1G -bs=1M \
+    -ioengine=sync -direct=0 -numjobs=1 -fsync_on_close=1 \
+    -time_based=1 -ramp_time=60 -runtime=100
