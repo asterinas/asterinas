a9ba6d9ebe101d630e111e90d511d726e4a424c3
Linermao <LinermaoGemail@gmail.com>
2026-09-04 02:52:44 +0000

    Add simpledrm as the first DRM device

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -13,6 +13,7 @@
 // to ensure that its component registration and initialization code are linked into
 // the kernel, because the assembler does not otherwise reference symbols from that crate.
 extern crate aster_drm as _;
+extern crate aster_simpledrm as _;
 extern crate aster_uart as _;
 
 #[ostd::main]
