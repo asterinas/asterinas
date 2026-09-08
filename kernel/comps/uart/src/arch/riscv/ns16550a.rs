5cdf5eb3f52d2e7206f50d088969a7e985622662
Ruihan Li <lrh2000@pku.edu.cn>
2026-09-08 23:57:42 +0800

    Refactor UART with `fdt-util`

--- kernel/comps/uart/src/arch/riscv/ns16550a.rs
+++ kernel/comps/uart/src/arch/riscv/ns16550a.rs
@@ -3,11 +3,11 @@
 use alloc::string::ToString;
 
 use fdt::node::FdtNode;
+use fdt_util::{AcquireIoMems, AcquireIrqLines};
 use ostd::{
-    arch::irq::{IRQ_CHIP, InterruptSourceInFdt, MappedIrqLine},
+    arch::irq::MappedIrqLine,
     console::uart_ns16650a::{Ns16550aAccess, Ns16550aRegister, Ns16550aUart},
     io::IoMem,
-    irq::IrqLine,
     mm::VmIoOnce,
     sync::SpinLock,
 };
@@ -34,53 +34,12 @@ impl Ns16550aAccess for SerialAccess {
 static IRQ_LINE: Once<MappedIrqLine> = Once::new();
 
 pub(super) fn init(fdt_node: FdtNode) {
-    let Some(reg) = fdt_node.reg().and_then(|mut regs| regs.next()) else {
-        ostd::info!("Failed to read 'reg' property from NS16550A node");
-        return;
-    };
-    let Some(reg_size) = reg.size else {
-        ostd::info!("Incomplete 'reg' property found in NS16550A node");
-        return;
-    };
-
-    let reg_addr = reg.starting_address as usize;
-    let Ok(io_mem) = IoMem::acquire(reg_addr..reg_addr + reg_size) else {
-        ostd::info!("I/O memory is not available for NS16550A");
-        return;
-    };
-
-    let Some(intr_parent) = fdt_node
-        .property("interrupt-parent")
-        .and_then(|prop| prop.as_usize())
+    let Some([io_mem]) =
+        fdt_node.acquire_io_mems([const { Ns16550aRegister::MAX as u16 as usize + 1 }])
     else {
-        ostd::info!("Failed to read 'interrupt-parent' property from NS16550A node");
         return;
     };
-    let intr_args = if let Some(prop) = fdt_node.property("interrupts")
-        && let Ok(args) = prop
-            .value
-            .as_chunks::<{ size_of::<u32>() }>()
-            .0
-            .iter()
-            .map(|chunk| u32::from_be_bytes(*chunk))
-            .next_chunk()
-    {
-        args
-    } else {
-        ostd::info!("Failed to read 'interrupts' property from NS16550A node");
-        return;
-    };
-
-    let Ok(mut irq_line) = IrqLine::alloc().and_then(|irq_line| {
-        IRQ_CHIP.get().unwrap().map_fdt_pin_to(
-            InterruptSourceInFdt {
-                interrupt_parent: intr_parent as u32,
-                arguments: intr_args,
-            },
-            irq_line,
-        )
-    }) else {
-        ostd::info!("IRQ line is not available for NS16550A");
+    let Some([mut irq_line]) = fdt_node.acquire_irq_lines() else {
         return;
     };
 
