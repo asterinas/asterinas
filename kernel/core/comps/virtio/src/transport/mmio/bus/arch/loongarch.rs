07e1b6df2c944c90aff77847063d5f1ed70709a2
Ruihan Li <lrh2000@pku.edu.cn>
2026-09-09 01:40:40 +0800

    Refactor VirtIO-MMIO with `fdt-util`

--- kernel/core/comps/virtio/src/transport/mmio/bus/arch/loongarch.rs
+++ kernel/core/comps/virtio/src/transport/mmio/bus/arch/loongarch.rs
@@ -3,7 +3,12 @@
 // TODO: Add `MappedIrqLine` support for LoongArch.
 pub(super) use ostd::irq::IrqLine as MappedIrqLine;
 
+use crate::transport::mmio::bus::{MmioBus, common_device::MmioCommonDevice};
+
 pub(super) fn probe_for_device() {
     // TODO: Probe virtio devices on the MMIO bus in LoongArch.
-    // Then, register them by calling `super::try_register_mmio_device`.
+    // The following methods will be used to probe the device.
+    let _ = super::validate_mmio_device;
+    let _ = MmioCommonDevice::new;
+    let _ = MmioBus::register_mmio_device;
 }
