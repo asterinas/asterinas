1f7b2bc485864559cdd73983a2d799d3abf4da6c
harry <ambition0316@126.com>
2026-09-09 21:29:13 +0800

    Add guest physical memory management with EPT

--- ostd/src/arch/x86/vm/mod.rs
+++ ostd/src/arch/x86/vm/mod.rs
@@ -3,12 +3,17 @@
 //! Hardware virtualization support for x86.
 
 mod context;
+pub(crate) mod ept;
 mod types;
 pub(crate) mod vmx;
 
 /// Initializes hardware-virtualization state on the current CPU.
 pub(super) fn init() {
+    use crate::arch::cpu::extension::{IsaExtensions, has_extensions};
     vmx::init_feature_control();
+    if has_extensions(IsaExtensions::VMX) {
+        vmx::invept::check_invept_support();
+    }
 }
 
 pub use self::{
