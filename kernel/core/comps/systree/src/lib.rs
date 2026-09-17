79da6a94d2460c4e761b2988453698680784a05c
Tao Su <st498824@antgroup.com>
2026-09-17 07:13:15 +0000

    Add `relative_path` for `SysTree` symlink targets

--- kernel/core/comps/systree/src/lib.rs
+++ kernel/core/comps/systree/src/lib.rs
@@ -46,7 +46,7 @@ pub use self::{
     utils::{
         _InheritSysBranchNode, _InheritSysLeafNode, _InheritSysSymlinkNode,
         AttrLessBranchNodeFields, BranchNodeFields, EmptyNode, NormalNodeFields, ObjFields,
-        SymlinkNodeFields, is_valid_name,
+        SymlinkNodeFields, is_valid_name, relative_path,
     },
 };
 use crate::tree::RootNode;
