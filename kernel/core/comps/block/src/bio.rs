f1612aa852f37af4e494890a82f73988aebd0561
zjp <jiping_zhou@foxmail.com>
2026-09-21 16:43:05 +0800

    Remove `pub` for items private to current module

--- kernel/core/comps/block/src/bio.rs
+++ kernel/core/comps/block/src/bio.rs
@@ -293,15 +293,15 @@ struct BioMetadata {
 }
 
 impl BioMetadata {
-    pub fn type_(&self) -> BioType {
+    fn type_(&self) -> BioType {
         self.type_
     }
 
-    pub fn sid_range(&self) -> &Range<Sid> {
+    fn sid_range(&self) -> &Range<Sid> {
         &self.sid_range
     }
 
-    pub fn status(&self) -> BioStatus {
+    fn status(&self) -> BioStatus {
         BioStatus::try_from(self.status.load(Ordering::Acquire)).unwrap()
     }
 }
@@ -515,7 +515,7 @@ impl BioSegmentPool {
     /// managed blocks is currently set to `POOL_DEFAULT_NBLOCKS`.
     ///
     /// The new pool will be allocated and mapped for later allocation.
-    pub fn new(direction: BioDirection) -> Self {
+    fn new(direction: BioDirection) -> Self {
         Self {
             arena_pool: DmaArenaPool::new(POOL_DEFAULT_NBLOCKS).unwrap(),
             direction,
@@ -537,7 +537,7 @@ impl BioSegmentPool {
     ///
     /// If the `offset_within_first_block` exceeds the block size, or the `len`
     /// exceeds the total length, this method will panic.
-    pub fn alloc(
+    fn alloc(
         &self,
         nblocks: usize,
         offset_within_first_block: usize,
