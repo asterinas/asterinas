9b1a725f3825ffb1fa3d5302aedcd1879cfe29e6
Cautreoxit <huk@stu.pku.edu.cn>
2026-09-24 08:30:05 +0000

    Add FIO NVMe bandwidth benchmarks

--- test/initramfs/nix/initramfs.nix
+++ test/initramfs/nix/initramfs.nix
@@ -44,7 +44,7 @@ stdenvNoCC.mkDerivation {
   name = "initramfs";
   buildCommand = ''
     mkdir -p $out/{dev,etc,root,usr,opt,tmp,var,proc,sys}
-    mkdir -p $out/{benchmark,test,ext2,exfat}
+    mkdir -p $out/{benchmark,test,ext2,exfat,nvme}
     mkdir -p $out/usr/{bin,sbin,lib,lib64,local}
     ln -sfn usr/bin $out/bin
     ln -sfn usr/sbin $out/sbin
